44a8e2d9094cbf7a2233cc772295ae9323555ef0
Bojidar <bojidar1308@abv.bg>
2018-01-20 13:17:42 +0200

    bojidar

---
+++ Bojidar Markov/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bojidar_Markov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Email: bojidar1308@abv.bg");
+        }
+    }
+}
