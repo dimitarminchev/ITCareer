d716f4149810ef41a6e6cdf8a4f24c581f4b85f7
cakasa <cakas@abv.bg>
2018-01-20 12:13:28 +0200

    Bozhidar's attempt

---
+++ Hello Bozhidar/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Hello_Bozhidar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heya! I'm Bozhidar");
+            Console.WriteLine("My e-mail is: cakas@abv.bg");
+        }
+    }
+}
