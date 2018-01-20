789fb61cee0b97b8fbe4704165efcb1fdc0d586f
Christian Neykov <kikokak62@gmail.com>
2018-01-20 13:29:25 +0200

    update

---
+++ 2. HelloSliven/Christian Neykov/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Christian_Neykov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name:Christian Neykov");
+            Console.WriteLine("Email:kikokak62@gmail.com");
+        }
+    }
+}
