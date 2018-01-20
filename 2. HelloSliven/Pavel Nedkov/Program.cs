ce5f5c756471f8ba8d63f7fddd63c24962bd00ef
martain911 <p.petrov1996@abv.bg>
2018-01-20 13:21:19 +0200

    Dimitar Minchev

---
+++ 2. HelloSliven/Pavel Nedkov/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pavel_Nedkov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pavkata190@abv.bg");
+        }
+    }
+}
