cbf6e3a7b397f9380a77e16e329d58ca115197e3
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-01-21 10:32:17 +0200

    Задача 1

--- 2. Programirane/3. Numbering_Systems/Task 1/Program.cs
+++ 2. Programirane/3. Numbering_Systems/Task 1/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace Task_1
 {
+    /*
+    - Преобразувайте 1234 (10) в двоична и шестнадесетична бройна системи. 
+    - Преобразувайте 1100101 (2) в десетична и шестнадесетична бройна системи. 
+    - Преобразувайте ABC (16) в десетично и двоична бройна системи.
+    */
     class Program
     {
         static void Main(string[] args)
         {
+            // Десетично > Двоично            
+            int number = 1234;
+            Console.Write("{0} (10) = ", number);
+            string binary = "";
+            while (number > 0)
+            {
+                int reminder = number % 2;
+                number /= 2;
+                binary += reminder;
+            }
+            for(int i=binary.Length - 1;i>=0;i--)
+            Console.Write(binary[i]);
+            Console.WriteLine(" (2)");
         }
     }
 }
