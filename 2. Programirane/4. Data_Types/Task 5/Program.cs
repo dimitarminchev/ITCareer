f3d035f60be49e166adf576c199bf63eada88363
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-01-27 11:38:16 +0200

    Решение: Божидар Андонов

--- 2. Programirane/4. Data_Types/Task 5/Program.cs
+++ 2. Programirane/4. Data_Types/Task 5/Program.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace Task_5
 {
+/* 5. Десетично към шестнадесетично и двоично
+Напишете програма, която преобразува десетично число в шестнадесетично и двоично число и го извежда.
+*/
     class Program
     {
+        // Решение: Божидар Андонов
         static void Main(string[] args)
         {
+            var a = int.Parse(Console.ReadLine());
+            string b = Convert.ToString(a, 16).ToUpper();
+            string c = Convert.ToString(a, 2).ToUpper();
+            Console.WriteLine(b);
+            Console.WriteLine(c);
         }
     }
 }
