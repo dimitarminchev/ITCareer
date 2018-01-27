13947ad68e8776ca694fe56c3052ff778731a25e
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-01-27 11:21:29 +0200

    Update

--- 2. Programirane/4. Data_Types/Task 2/Program.cs
+++ 2. Programirane/4. Data_Types/Task 2/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Task_2
 {
 /* 2. Цели числа
@@ -17,6 +12,20 @@ namespace Task_2
         // Решение: Божидар Андонов
         static void Main(string[] args)
         {
+            sbyte num1 = -100;
+            byte num2 = 128;
+            short num3 = -3540;
+            ushort num4 = 64876;
+            uint num5 = 2147483648;
+            int num6 = -1141583228;
+            long num7 = -1223372036854775808;
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+            Console.WriteLine(num4);
+            Console.WriteLine(num5);
+            Console.WriteLine(num6);
+            Console.WriteLine(num7);
         }
     }
 }
--- 2. Programirane/4. Data_Types/Task 4/Program.cs
+++ 2. Programirane/4. Data_Types/Task 4/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Task_4
 {
+/* 4. Размяна на стойности на променливи
+Декларирайте две целочислени променливи a и b и им присвоете 
+стойности 5 и 10 след това разменете техните стойности чрез някаква програмна логика. 
+Изведете стойностите на променливите преди и след размяната, както е показано:
+*/
     class Program
     {
+        // Решение: Живко Колев
         static void Main(string[] args)
         {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            var c = 0;
+            Console.WriteLine("Before");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("After");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
         }
     }
 }
