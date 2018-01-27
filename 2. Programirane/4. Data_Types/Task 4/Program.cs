c8b8ebfad65a888315e8fe12c97f117770fb9611
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-01-27 11:24:56 +0200

    Решение: Живко Колев

--- 2. Programirane/4. Data_Types/Task 4/Program.cs
+++ 2. Programirane/4. Data_Types/Task 4/Program.cs
@@ -14,12 +14,15 @@ namespace Task_4
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             var c = 0;
+
             Console.WriteLine("Before");
             Console.WriteLine(a);
             Console.WriteLine(b);
+
             c = a;
             a = b;
             b = c;
+
             Console.WriteLine("After");
             Console.WriteLine(a);
             Console.WriteLine(b);
