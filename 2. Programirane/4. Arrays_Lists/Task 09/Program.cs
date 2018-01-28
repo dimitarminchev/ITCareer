4476a38add606463f20c3ec7efeabe8742d05d17
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-01-28 13:58:53 +0200

    Решение: Димитър Минчев

---
+++ 2. Programirane/4. Arrays_Lists/Task 09/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_09
+{
+    class Program
+    {
+/* 9. Въвеждане на списък от конзолата
+Въведете списък от цели числа и го изведете в конзолата:
+*/
+        // Решение: Димитър Минчев
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int index = 0; index < list.Count; index++)
+            {
+                Console.WriteLine("list[{0}] = {1}", index, list[index]);
+            }
+        }
+    }
+}
