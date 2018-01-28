2f0820c6ad019c25ff1857cd2875a9be27d2fe4c
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-01-28 10:38:38 +0200

    28.01.2018

---
+++ 2. Programirane/4. Arrays_Lists/Task 01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+/* 1. Статистика на масив
+Напишете програма, която получава масив от цели числа (разделени  с интервал) и извежда най-малкия елемент, 
+най-големия елемент, сумата на елементите и средната им стойност.
+*/
+    class Program
+    {
+        // Решение: 
+        static void Main(string[] args)
+        {
+        }
+    }
+}
