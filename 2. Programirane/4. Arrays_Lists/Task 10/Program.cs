7e2eeedcda6c6e241201cbc30eb9dc94665485da
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-01-28 14:01:08 +0200

    Решение: Димитър Минчев

---
+++ 2. Programirane/4. Arrays_Lists/Task 10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+/* 10. Въвеждане на списък от конзолата чрез 1 ред
+Въведете списък от цели числа и го изведете в конзолата. Елементите на списъка ще получите от единствен ред, разделени с интервали.
+- Първо създайте нов C# конзолен проект и задайте име "ListInputOutputLine".
+- Въведете списъка от числа от един ред, чрез следния код:
+*/
+    class Program
+    {
+        // Решение: Димитър Минчев
+        static void Main(string[] args)
+        {
+            // Въвеждаме поредица от цели числа в списък
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            // Отпечатване на списък от цели числа
+            for (int index = 0; index < nums.Count; index++)
+            Console.WriteLine("nums[{0}] = {1}", index, nums[index]);
+            // Console.WriteLine(String.Join("; ", nums));
+        }
+    }
+}
