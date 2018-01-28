4c21b05cf8b529e5fc354dad1aa6b30e52395148
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-01-28 15:28:23 +0200

    Решение: Митко Недялков

--- 2. Programirane/4. Arrays_Lists/Task 17/Program.cs
+++ 2. Programirane/4. Arrays_Lists/Task 17/Program.cs
@@ -14,45 +14,12 @@ namespace Task_17
         // Решение: Митко Недялков
         static void Main(string[] args)
         {
-            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
-
-            var row1Part1 = new int[numbers.Length / 4];
-            var row1Part2 = new int[numbers.Length / 4];
-            var row2 = new int[numbers.Length / 2];
-
-            for (int i = 0; i < numbers.Length / 4; i++)
+            var nums = Console.ReadLine().Split(' ');
+            for (int i = 0; i < nums.Count(); i++)
             {
-                row1Part2[i] = numbers[numbers.Length - 1 - i];
+                nums[i] = String.Join("", nums[i].Reverse());
             }
-
-            for (int i = 0; i < numbers.Length / 4 ; i++)
-            {
-                row1Part1[i] = numbers[i];
-            }
-
-            row1Part1 = row1Part1.Reverse().ToArray();
-
-            var row1 = new int[row1Part1.Length + row1Part2.Length];
-
-            for (int i = 0; i < row1.Length / 2; i++)
-            {
-                row1[i] = row1Part1[i];
-            }
-
-            for (int i = 0; i < row1.Length / 2; i++)
-            {
-                row1[i + row1Part1.Length] = row1Part2[i];
-            }
-
-            for (int i = numbers.Length / 4; i < numbers.Length / 2 + numbers.Length / 4; i++)
-            {
-                row2[i- numbers.Length / 4] = numbers[i];
-            }
-
-            for(int i = 0; i < row1.Length; i++)
-            {
-                Console.Write("{0} ", row1[i] + row2[i]);
-            }
+            Console.WriteLine(nums.Select(int.Parse).Sum());
         }
     }
 }
