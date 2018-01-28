e4c24b255bdf1bb0226b0e7342fb25fde371feae
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-01-28 10:50:18 +0200

    Решение: Владимир Владимиров

---
+++ 2. Programirane/4. Arrays_Lists/Task 02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
