e6e8514ed374febb66256d7e02f9261b6cb0137c
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-03 14:50:37 +0200

    Решение: Петко Люцканов и Божидар Андонов

---
+++ 2. Programirane/5. Methods_Debuging/Task 10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+/* 10. Почивни дни между две дати
+Имате задачата да откриете и поправите грешките във вече написан програмен код, като използвате дебъгера на Visual Studio. За целта трябва да проследите изпълнението на програмата, за да откриете тези редове от кода ѝ, които пораждат неправилен или неочакван резултат.
+Решение: Петко Люцканов и Божидар Андонов
+*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            DateTime endDate = DateTime.ParseExact(Console.ReadLine(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            var holidaysCount = 0;
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) { holidaysCount++; }
+            }
+            Console.WriteLine(holidaysCount);
+        }
+    }
+}
