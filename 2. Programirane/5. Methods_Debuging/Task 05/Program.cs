f39e078f5438769284311775adde40d0453f5e01
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-03 11:45:04 +0200

    Решение: Владимир Владимиров

---
+++ 2. Programirane/5. Methods_Debuging/Task 05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_05
+{
+/* 5. Конвертор за температури
+Създайте метод, който конвертира температура от Фаренхайт в Целзий. Форматирайте резултата до втория десетичен знак.
+Използвайте формулата: (fahrenheit - 32) * 5 / 9.
+Решение: Владимир Владимиров
+*/
+    class Program
+    {
+        static double For(double F)
+        {
+            return (F - 32) * 5 / 9;
+        }
+
+        static void Main(string[] args)
+        {
+            double n;
+            n = For(double.Parse(Console.ReadLine()));
+            Console.Write("Celsius=");
+            Console.WriteLine("{0:f2}", n);
+        }
+    }
+}
+}
