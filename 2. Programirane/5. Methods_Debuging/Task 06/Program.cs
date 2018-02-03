4e2a1962ccf339a071970afcb619b103492545f8
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-03 11:47:35 +0200

    Решение: Валентин Хаджиминов

---
+++ 2. Programirane/5. Methods_Debuging/Task 06/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_06
+{
+/* 6. Пресмятане на лице на триъгълник
+Създайте метод, който изчислява и връща лицето на триъгълник по дадени основа и височина.
+Решение: Валентин Хаджиминов
+*/
+    class Program
+    {
+        static double TriangleArea(double width, double height)
+        {
+            return width * height / 2;
+        }
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = TriangleArea(width, height);
+            Console.WriteLine(area);
+        }
+    }
+}
