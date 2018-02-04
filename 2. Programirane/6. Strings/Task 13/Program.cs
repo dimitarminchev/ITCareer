c876dcf1fc65962e0f07b8c721ee830164139fa1
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-04 14:51:27 +0200

    Решение: Петко Люцканов

---
+++ 2. Programirane/6. Strings/Task 13/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13
+{
+/* 13. Скривалището
+Вие сте детектив от Скотланд Ярд и трябва да намерите скривалището на много опасна група от престъпници. Вие ще получите карта под формата на низ и след това ще получите улики от разузнаването.
+Решение: Петко Люцканов
+*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
