ea77d7ced7902e2f4aeda2c5320d41445e707e4d
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-04 14:49:47 +0200

    Решение: Павел Недков

---
+++ 2. Programirane/6. Strings/Task 12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+/* 12. Само букви
+Напишете програма, която въвежда низ съобщение като вход и замества всички числа с буквата непосредствено след числото.
+Решение: Павел Недков
+*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
