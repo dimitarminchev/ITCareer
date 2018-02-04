a79422ca744a2fb3caa220cdbdb7a1a2caf5af0a
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-04 15:50:38 +0200

    Решение: Павел Недков

--- 2. Programirane/6. Strings/Task 12/Program.cs
+++ 2. Programirane/6. Strings/Task 12/Program.cs
@@ -14,6 +14,23 @@ namespace Task_12
     {
         static void Main(string[] args)
         {
+            string line = Console.ReadLine();
+            string last = "", result = "";
+            for (int i = line.Length - 1; i >= 0; i--)
+            {
+                var c = (int)line[i];
+                if (c >= 48 && c <= 57)
+                {
+                    if (last != "") result += last;
+                    else result += line[i].ToString();
+                }
+                else
+                {
+                    last = line[i].ToString();
+                    result += line[i].ToString();
+                }
+            }
+            Console.WriteLine(string.Join("", result.Reverse()));
         }
     }
 }
