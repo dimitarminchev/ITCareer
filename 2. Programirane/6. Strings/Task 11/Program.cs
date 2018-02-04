bafb21e49bd08246f874479c799f21591024a41a
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-04 15:55:59 +0200

    Решение: Божидар Андонов и Митко Недялков

--- 2. Programirane/6. Strings/Task 11/Program.cs
+++ 2. Programirane/6. Strings/Task 11/Program.cs
@@ -10,10 +10,32 @@ namespace Task_11
 Даден е низ от случайни символи, както и шаблон от случайни символи. Вие трябва да "отърсите" (премахнете) всички граничните случаи на този шаблон, с други думи, първото съвпадение (срещане) и последното съвпадение на шаблона в низа.
 Когато успешно премахнете дадено съвпадение , премахнете от шаблона символа, който съответства на индекса, равен на дължината на шаблона / 2. След това продължавате да премахвате от краищата на низа съвпаденията с новия шаблон, докато шаблона стане празен или докато основния низ стане по-къс от шаблона.
 В случай, че сте намерили най-малко две съвпадения, успешно сте ги премахнали , извеждате "Shaked it" на конзолата. В противен случай извеждате “No shake.”, остатъкът от основния низ, и завършвате програмата. Вижте примерите за повече информация.
-Решение: Божидар Андонов
+Решение: Божидар Андонов и Митко Недялков
 */
     class Program
     {
-        
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            var shaken = true;
+            string trimming = Console.ReadLine();
+            string finalword = "";
+            while (shaken)
+            {
+                var temp = finalword;
+                var parts = finalword.Split(new string[] { trimming }, StringSplitOptions.RemoveEmptyEntries);
+                finalword = String.Join("", parts);
+                if (temp == finalword) shaken = false;
+                if (shaken == true)
+                {
+                    if (parts.Length > 2)
+                        Console.WriteLine("Shaked it.");
+                    else
+                        Console.WriteLine("No Shake.");
+                    trimming = trimming.Remove((int)Math.Truncate(trimming.Length / 2d), 1);
+                }
+            }
+            Console.WriteLine("No Shake.\n" + finalword);
+        }
     }
 }
