caa62b916d4d7a741b5b6425d992d18f0ea5e6a2
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-24 18:54:26 +0200

    24.02.2018

--- 2. Programirane/9. Exam_Preparation/Task 04/Program.cs
+++ 2. Programirane/9. Exam_Preparation/Task 04/Program.cs
@@ -6,54 +6,51 @@ using System.Threading.Tasks;
 
 namespace Task_04
 {
-    // 04. CODE: Phoenix Oscar Romeo November
+    // 04. CODE: Phoenix Oscar Romeo November (Result: 100/100)
     class Program
     {
         static void Main(string[] args)
         {
+            // Речник
             var dict = new Dictionary<String, List<String>>();
+
+            // Обработка на входа
             string line = Console.ReadLine();
             while (line != "Blaze it!")
             {
                 var Parts = line.Split('>');
-                var Creature = (Parts[0].Trim('-')).Trim(' '); // Creature
-                var SquadMate = Parts[1].Trim(' '); // SquadMate 
-
-                // New Creature
-                if (!dict.ContainsKey(Creature))
+                var Creature = Parts[0].Trim('-').Trim(' '); 
+                var SquadMate = Parts[1].Trim(' ');
+                // Условия
+                if (Creature != SquadMate)
                 {
-                    var mates = new List<String>();
-                    mates.Add(SquadMate);
-                    dict.Add(Creature, mates);
+                    if (!dict.ContainsKey(Creature)) dict.Add(Creature, new List<String>() { SquadMate });
+                    else if(!dict[Creature].Contains(SquadMate)) dict[Creature].Add(SquadMate);
                 }
-                // Existing Creature
-                else 
-                {
-                    var mates = dict[Creature];
-                    mates.Add(SquadMate);
-                }
-
-                // Next Line
+                // Следващ ред
                 line = Console.ReadLine();
             }
 
-/*
-            // TODO: Remove Duplicates
-            var noDupl = new Dictionary<String, List<String>>();
+            // Премахваме дублираните
+            var nope = new Dictionary<String, List<String>>(); 
             foreach (var creature in dict)
             {
-                List<String> NewMates = new List<String>();
-                var exclude = dict.Keys.Where(x => x != creature.Key).ToList();
+                var list = new List<String>();
                 foreach (var mate in creature.Value)
-                  foreach(var item in exclude)
-                        if(mate != item && !NewMates.Contains(mate))
-                            NewMates.Add(mate);
-                // No Duplicates
-                noDupl.Add(creature.Key, NewMates);
+                {
+                    if (dict.ContainsKey(mate))
+                        if (dict[mate].Contains(creature.Key))
+                            continue;
+                    list.Add(mate);
+                }
+                nope.Add(creature.Key, list);
             }
-*/
-            // Print
-            foreach (var item in dict)
+
+            // Сортиране 
+            var sorted = nope.OrderByDescending(x => x.Value.Count);
+
+            // Отпечатване на резултата
+            foreach (var item in sorted)
             Console.WriteLine("{0} : {1}", item.Key, item.Value.Count);
         }
     }
