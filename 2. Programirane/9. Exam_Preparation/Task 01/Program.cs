90c7e8d9c07ce07024f7f382c8b89bcfb08e6912
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-24 10:56:06 +0200

    24.02.2018

---
+++ 2. Programirane/9. Exam_Preparation/Task 01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    // 01. Resurrection
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                long totalLength = int.Parse(Console.ReadLine());
+                decimal totalWidth = decimal.Parse(Console.ReadLine());
+                long wingLength = int.Parse(Console.ReadLine());
+                decimal totalYears = (totalLength * totalLength) * (totalWidth + 2 * wingLength);
+                Console.WriteLine(totalYears);
+                n--;
+            }
+        }
+    }
+}
