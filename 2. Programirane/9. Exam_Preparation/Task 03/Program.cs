33bbc99eefd949e4cfe1dc8b34b92e4b991eac41
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-24 12:34:25 +0200

    24.02.2018

---
+++ 2. Programirane/9. Exam_Preparation/Task 03/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03
+{
+    // 03. Phoenix Grid (60/100)
+    class Program
+    {
+        // Проверка за палиндром
+        static bool Palindrome(string palindrome)
+        {
+            bool TrueOrFalse = false;
+            for (int i = 0; i <= palindrome.Length / 2; i++)
+            {
+                if (palindrome[i] != palindrome[palindrome.Length - i - 1])
+                {
+                    TrueOrFalse = false;
+                    break;
+                }
+                else TrueOrFalse = true;
+            }
+            return TrueOrFalse;
+        }
+
+        // Проверка за валиден вход
+        static bool PhoenixGrid(String line)
+        {
+            try
+            {
+                List<string> tripples = new List<string>();
+                for (int i = 0; i < line.Length; i += 4)
+                {
+                    tripples.Add(line.Substring(i, 3));
+                }
+                String text = String.Join("", tripples);
+                if (text.Length % 3 != 0) return false;
+                return Palindrome(text);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        // Глаавна функция
+        static void Main(string[] args)
+        {
+            String line = Console.ReadLine();
+            while (line != "ReadMe")
+            {
+                if (PhoenixGrid(line)) Console.WriteLine("YES");
+                else Console.WriteLine("NO");
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
