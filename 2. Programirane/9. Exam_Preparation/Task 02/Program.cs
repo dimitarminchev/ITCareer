5c91366cbeeaae44ee10e519f489fd9d860cb15a
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-25 09:28:28 +0200

    25.02.2018

--- 2. Programirane/9. Exam_Preparation/Task 02/Program.cs
+++ 2. Programirane/9. Exam_Preparation/Task 02/Program.cs
@@ -8,28 +8,17 @@ namespace Task_02
     {
         static void Main(string[] args)
         {
-            // input
             var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
             int start = int.Parse(Console.ReadLine());
-            int pos = start;
-            int minus = 1;
-
-            // print
-            //Console.WriteLine("Initial index: {0}", start);
-            //Console.WriteLine("Initial state:");
-            //Console.WriteLine(String.Join(" ", nums));
-
-            // process
+            int pos = start, minus = 1;
             string line = Console.ReadLine();
             do
             {
                 var parts = line.Split(' ');
                 String command = parts[0];
-                int steps = int.Parse(parts[1]);
-                //Console.WriteLine("Go {0} {1} steps:", command, steps);
+                int steps = int.Parse(parts[1]);               
                 switch (command)
                 {
-                    // left
                     case "left":
                     {
                          while(steps>0)
@@ -42,11 +31,9 @@ namespace Task_02
                                 }
                                 nums[pos] -= minus;
                                 steps--;
-                                //Console.WriteLine(String.Join(" ", nums));
                          }
                          break;
                     }
-                    // right
                     case "right":
                     {
                             while (steps > 0)
@@ -59,7 +46,6 @@ namespace Task_02
                                 }
                                 nums[pos] -= minus;
                                 steps--;
-                                //Console.WriteLine(String.Join(" ", nums));
                             }
                             break;
                     }
@@ -67,9 +53,6 @@ namespace Task_02
                 line = Console.ReadLine();
             }
             while(line != "Supernova");
-
-            // print
-            //Console.WriteLine("Final state:");
             Console.WriteLine(String.Join(" ", nums));
         }
     }
---
+++ 2. Programirane/9. Exam_Preparation/Task 05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_05
+{
+    // 05. Anonymous Downsite (Result: 90/100)
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            // Входни данни
+            int n = int.Parse(Console.ReadLine());
+            int key = int.Parse(Console.ReadLine());
+
+            // Сметки
+            decimal totalLoss = 0;
+            double securityToken = Math.Pow(key, n);
+
+            // Обработка
+            for (int i=0;i<n;i++)
+            {
+                var parts = Console.ReadLine().Split(' ');
+                String siteName = parts[0];
+                decimal siteVisits = decimal.Parse(parts[1]);
+                decimal siteCommercialPricePerVisit = decimal.Parse(parts[2]);
+                decimal loss = siteVisits * siteCommercialPricePerVisit;
+                totalLoss += loss;
+                Console.WriteLine("{0}", siteName);
+            }
+
+            // Отпечатване на резултата
+            Console.WriteLine("Total Loss: {0:f20}", totalLoss);
+            Console.WriteLine("Security Token: {0}", securityToken);
+        }
+    }
+}
