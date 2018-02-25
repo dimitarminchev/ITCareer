62851d76bc869b5e1fdab296f8fa6e0468d0244b
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-02-25 10:19:03 +0200

    25.02.2018

--- 2. Programirane/9. Exam_Preparation/Task 06/Program.cs
+++ 2. Programirane/9. Exam_Preparation/Task 06/Program.cs
@@ -8,42 +8,33 @@ namespace Task_06
     {
         static void Main(string[] args)
         {
-            String[] dummy = Console.ReadLine().Split(' ');
-            String[] command;  
-            
+            String[] command, dummy = Console.ReadLine().Split(' ');
             while(true)
             {
                 command = Console.ReadLine().Split(' ');
                 switch (command[0])
                 {
-                    // Сливане
                     case "merge":
                     {
                             int startIndex = int.Parse(command[1]) < 0 ? 0 : int.Parse(command[1]);
                             int endIndex = int.Parse(command[2]) > dummy.Length - 1 ? dummy.Length - 1 : int.Parse(command[2]);
-
                             String[] left = dummy.Take(startIndex).ToArray();
                             String[] middle = new String[1] { String.Join("", dummy.Skip(startIndex).Take(endIndex - startIndex).ToArray()) };
                             String[] right = dummy.Skip(endIndex).Take(dummy.Length - endIndex).ToArray();
                             dummy = left.Union(middle).Union(right).ToArray();
-
                             break;
                     }
-                    // Разделяне
                     case "divide":
                     {
                             int index = int.Parse(command[1]);
                             float partitions = float.Parse(command[2]);
                             int parts = (int)Math.Ceiling(dummy[index].Length / partitions);
-
                             String[] left = dummy.Take(index).ToArray();
                             String[] middle = Enumerable.Range(0, dummy[index].Length / parts).Select(i => dummy[index].Substring(i * parts, parts)).ToArray();
                             String[] right = dummy.Skip(index+1).Take(dummy.Length - index).ToArray();
                             dummy = left.Union(middle).Union(right).ToArray();
-
                             break;
                     }
-                    // Край
                     case "3:1":
                     {
                             Console.WriteLine(String.Join(" ", dummy));
