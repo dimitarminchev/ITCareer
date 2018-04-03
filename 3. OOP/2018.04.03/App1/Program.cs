da0ab13079c9aea9ddbd904e955f0710cbb6f73f
dimitarminchev <dimitar.minchev@gmail.com>
2018-04-03 11:50:24 +0300

    03.04.2018

---
+++ 3. OOP/2018.04.03/App1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App1
+{
+    class Program
+    {
+        // Главен метод
+        static void Main(string[] args)
+        {
+            // Създавам обект инстанция на клас
+            Dice dice = new Dice(6);
+
+            // Хвърляме зара
+            Console.WriteLine("Dice Roll = {0}", dice.Roll());
+        }
+    }
+}
---
+++ 3. OOP/2018.04.03/App2/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    /// <summary>
+    /// Човек
+    /// </summary>
+    class Person
+    {
+        // Име 
+        public string Name { get; set; }
+
+        // Възраст
+        public int Age { get; set; }
+
+        // Представяне
+        public void IntroduceYourself()
+        {
+            Console.WriteLine("I am {0} and my age is {1}.", Name, Age);
+        }
+
+    }
+}
