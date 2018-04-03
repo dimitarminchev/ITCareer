9c9c89b1d7095b30c92f74db974c58373dd013c1
dimitarminchev <dimitar.minchev@gmail.com>
2018-04-03 15:40:55 +0300

    03.04.2018

---
+++ 3. OOP/2018.04.03/App5/Family.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App5
+{
+    /// <summary>
+    /// Фамилия
+    /// </summary>
+    class Family
+    {
+        // Списък
+        private List<Person> members = new List<Person>();
+
+        // Добавяне 
+        public void AddMember(Person member)
+        {
+            members.Add(member);
+        }
+
+        // Най-възрастен
+        public Person GetOldestMember()
+        {
+            var list = members.OrderByDescending(a => a.Age).ToList();
+            return list[0];
+        }
+    }
+}
--- 3. OOP/2018.04.03/README.md
+++ 3. OOP/2018.04.03/README.md
@@ -2,3 +2,5 @@
 - App1 = Dice.cs 
 - App2 = Person.cs
 - App3 = BankAccount.cs + Person.cs 
+- App4 = Employee.cs
+- App5 = Family.cs + Person.cs
