78e6bba79955df7264ba31947e5d66a6a5087a98
dimitarminchev <dimitar.minchev@gmail.com>
2018-04-03 12:14:46 +0300

    03.04.2018

---
+++ 3. OOP/2018.04.03/App3/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3
+{
+    /// <summary>
+    /// Банкова сметка
+    /// </summary>
+    class BankAccount
+    {
+        // Иденфикатор
+        private int id;
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        // Баланс
+        private double balance;
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+    }
+
+}
