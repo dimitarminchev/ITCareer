40a1884de4e135fd64f44170c4d16bf983ad4431
dimitarminchev <dimitar.minchev@gmail.com>
2018-04-04 09:36:53 +0300

    04.04.2018

---
+++ 3. OOP/2018.04.04/App1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        // Главен метод
+        static void Main(string[] args)
+        {
+        }
+    }
+}
