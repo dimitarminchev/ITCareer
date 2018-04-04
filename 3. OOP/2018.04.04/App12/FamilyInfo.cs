3f308357f4fd6a5a2c8794b03190495c88d849ae
dimitarminchev <dimitar.minchev@gmail.com>
2018-04-04 16:59:59 +0300

    Решение: Митко

---
+++ 3. OOP/2018.04.04/App12/FamilyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class FamilyInfo
+{
+    public List<Person> Parents { get; set; }
+
+    public List<Person> Children { get; set; }
+
+    public FamilyInfo()
+    {
+        this.Parents = new List<Person>();
+        this.Children = new List<Person>();
+    }
+}
