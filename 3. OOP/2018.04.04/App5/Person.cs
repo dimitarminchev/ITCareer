5cc9e8ec68d45648eab1b7def2af30e032ab3312
dimitarminchev <dimitar.minchev@gmail.com>
2018-04-04 12:45:44 +0300

    Решение: Ангел Ангелов и Валентин Хаджиминов

---
+++ 3. OOP/2018.04.04/App5/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App5
+{
+    class Person
+    {
+        private string name;
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        private int age;
+        public int Age
+        {
+            set { age = value; }
+            get { return age; }
+        }
+        private List<BankAccount> accounts;
+
+        public Person()
+        {
+            this.name = "No name";
+            this.age = 1;
+        }
+        public Person(int age)
+        {
+            this.name = "No name";
+            this.age = age;
+        }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            this.accounts = new List<BankAccount>();
+        }
+
+        public Person(string name, int age, List<BankAccount> accounts)
+        {
+            this.name = name;
+            this.age = age;
+            this.accounts = accounts;
+        }
+
+        public double GetBalance()
+        {
+            return accounts.Sum(x => x.Balance);
+        }
+    }
+    
+}
