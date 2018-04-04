f820053434033d6fcaf477b4192ef71f7b05dcbf
dimitarminchev <dimitar.minchev@gmail.com>
2018-04-04 16:01:00 +0300

    Решение: Ангел

--- 3. OOP/2018.04.04/App7/Car.cs
+++ 3. OOP/2018.04.04/App7/Car.cs
@@ -8,5 +8,40 @@ namespace App7
 {
     class Car
     {
+        // Name
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // Fuel
+        private float fuel;
+        public float Fuel
+        {
+            get { return fuel; }
+            set { fuel = value; }
+        }
+
+        // perkm
+        private float perkm;
+        public float Perkm
+        {
+            get { return perkm; }
+            set { perkm = value; }
+        }
+
+        // The Magic
+        public void TheMagic(float km)
+        {
+            if (km * this.perkm > this.fuel) Console.WriteLine("Insufficient fuel for the drive");
+            else
+            {
+                this.fuel -= (km * this.perkm);
+                Console.WriteLine("{0} {1:f2} {2}", this.name, this.fuel, km);
+            }
+
+        }
     }
 }
--- 3. OOP/2018.04.04/App7/Program.cs
+++ 3. OOP/2018.04.04/App7/Program.cs
@@ -10,6 +10,49 @@ namespace App7
     {
         static void Main(string[] args)
         {
+            // Car
+            int n = int.Parse(Console.ReadLine());
+            List<Car> cars = new List<Car>();
+            for (int i = 0; i < n; i++)
+            {
+                var fuk = Console.ReadLine().Split();
+                cars.Add
+                (
+                    new Car()
+                    {
+                        Name = fuk[0],
+                        Fuel = float.Parse(fuk[1]),
+                        Perkm = float.Parse(fuk[2])
+                    }
+                );
+            }
+
+            // Commands
+            var dik = new Dictionary<string, int>();
+            var line = "";
+            do
+            {
+                line = Console.ReadLine();
+                if (line != "End")
+                {
+                    var split = line.Split();
+                    if (dik.ContainsKey(split[1]))
+                    {
+                        var km = dik[split[1]] + int.Parse(split[2]);
+                        dik[split[1]] = km;
+
+                    }
+                    else dik.Add(split[1], int.Parse(split[2]));
+                }
+            }
+            while (line != "End");
+
+            // Process
+            foreach (var item in dik)
+            {
+                var currentCar = cars.Where(a => a.Name == item.Key).First();
+                currentCar.TheMagic(item.Value);
+            }
         }
     }
 }
