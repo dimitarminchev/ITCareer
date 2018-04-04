931bee208ae84da6e5a62b17e7713d90fdb836e5
dimitarminchev <dimitar.minchev@gmail.com>
2018-04-04 10:47:06 +0300

    04.04.2018

--- 3. OOP/2018.04.04/App1/Program.cs
+++ 3. OOP/2018.04.04/App1/Program.cs
@@ -11,6 +11,14 @@ namespace App1
         // Главен метод
         static void Main(string[] args)
         {
+            BankAccount acc = new BankAccount()
+            {
+                Id = 12,
+                Balance = 123.45
+            };
+            acc.Deposit(23);
+            acc.Withdraw(100);
+            Console.WriteLine(acc.ToString());
         }
     }
 }
