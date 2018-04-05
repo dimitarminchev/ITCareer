67c1f4fb7c2271555a6b43aeb89d81d8f1242133
dimitarminchev <dimitar.minchev@gmail.com>
2018-04-05 13:58:44 +0300

    Решение: Божидар Андонов

---
+++ 3. OOP/2018.04.05/App6/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App6
+{
+    class Product
+    {
+        private string name;
+        private double price;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ 3. OOP/2018.04.05/App6/Shopping.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App6
+{
+    class Shopping
+    {
+        static void Main(string[] args)
+        {
+            string[] inputPeople = Console.ReadLine().Split(';').ToArray();
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < inputPeople.Length; i++)
+            {
+                string[] person = inputPeople[i].Split('=').ToArray();
+                people.Add(new Person(person[0], int.Parse(person[1])));
+            }
+
+            string[] inputProducts = Console.ReadLine().Split(';').ToArray();
+            List<Product> products = new List<Product>();
+            for (int i = 0; i < inputProducts.Length; i++)
+            {
+                string[] product = inputProducts[i].Split('=').ToArray();
+                products.Add(new Product(product[0], double.Parse(product[1])));
+            }
+
+            string[] command = Console.ReadLine().Split(' ').ToArray();
+            while (command[0] != "END")
+            {
+                string nameOfPerson = command[0];
+                string nameOfProduct = command[1];
+                for (int i = 0; i < people.Count; i++)
+                {
+                    if (people[i].Name == nameOfPerson)
+                        people[i].Buy(nameOfProduct, products);
+                }
+                command = Console.ReadLine().Split(' ').ToArray();
+            }
+
+            foreach (var person in people)
+            {
+                if (person.Products.Count == 0)
+                    Console.WriteLine($"{person.Name} - Nothing bought");
+                else
+                    Console.WriteLine($"{person.Name} - {string.Join(", ", person.Products)}");
+            }
+        }
+    }
+}
