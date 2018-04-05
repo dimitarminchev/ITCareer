b34f4e9b82b8566bd175651306c8554778c44d4f
dimitarminchev <dimitar.minchev@gmail.com>
2018-04-05 15:22:45 +0300

    05.04.2018

---
+++ 3. OOP/2018.04.05/App8/Player.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App8
+{
+    class Player
+    {
+        // Конструктор
+        public Player(string name, int du, int sp, int dr, int p, int sh)
+        {
+            this.Name = name;
+            this.Durablility = du;
+            this.Sprint = sp;
+            this.Dribble = dr;
+            this.Passing = p;
+            this.Shooting = sh;
+        }
+
+        // Име
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+
+                if (value.Length == 0 || String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("A name should not be empty");
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
+
+        // Райтинг на играч
+        public double Rating()
+        {
+            return (durability+sprint+dribble+passing+shooting)/5.0;
+        }
+
+        // Издръжливост, Спринт, дрибъл, подавания и стрелба. 
+        private int durability;
+        private int sprint;
+        private int dribble;
+        private int passing;
+        private int shooting;
+
+        // Методи
+        public int Durablility
+        {
+            get { return durability; }
+            set
+            {
+                if (value > 100 || value < 0)
+                {
+                    Console.WriteLine("Durability should be between 0 and 100");
+                }
+                else
+                {
+                    durability = value;
+                }
+            }
+        }
+        public int Sprint
+        {
+            get { return sprint; }
+            set
+            {
+                if (value > 100 || value < 0)
+                {
+                    Console.WriteLine("Sprint should be between 0 and 100");
+                }
+                else
+                {
+                    sprint = value;
+                }
+            }
+        }
+        public int Dribble
+        {
+            get { return dribble; }
+            set
+            {
+                if (value > 100 || value < 0)
+                {
+                    Console.WriteLine("Dribble should be between 0 and 100");
+                }
+                else
+                {
+                    dribble = value;
+                }
+            }
+        }
+        public int Passing
+        {
+            get { return passing; }
+            set
+            {
+                if (value > 100 || value < 0)
+                {
+                    Console.WriteLine("Passing should be between 0 and 100");
+                }
+                else
+                {
+                    passing = value;
+                }
+            }
+        }
+        public int Shooting
+        {
+            get { return shooting; }
+            set
+            {
+                if (value > 100 || value < 0)
+                {
+                    Console.WriteLine("Shooting should be between 0 and 100");
+                }
+                else
+                {
+                    shooting = value;
+                }
+            }
+        }
+    }
+}
---
+++ 3. OOP/2018.04.05/App8/Team.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App8
+{
+    class Team
+    {
+        // Конструктор
+        public Team(string name)
+        {
+            this.Name = name;
+            this.Players = new List<Player>();
+        }
+
+        // Играчи
+        private List<Player> players;
+        public List<Player> Players
+        {
+            get { return players;  }
+            set { players = value; }
+        }
+
+        // Име
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length == 0 || String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("A name should not be empty");
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
+
+        // Рейтинг
+        public int Rating()
+        {
+            double sum = this.Players.Sum(x => x.Rating());
+            int count = this.Players.Count();
+            return (int)Math.Ceiling(sum / count);
+        }
+    }
+}
