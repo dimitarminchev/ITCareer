60adc8bd24c4c8cb79d02e2a8ea23b85c7934d24
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-06-30 18:26:13 +0300

    30.06.2018

---
+++ 4. ASD/30.06.2018/Problem5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem5
+{
+    class Program
+    {
+        /* Problem 5.	Намиране на най-малко число 
+           Определете сложността (максимания брой стъпки) на програма, 
+           която чете от конзолата последователност от цели числа на един ред, 
+           разделени с интервал. Намерете най-малкото от тях и го изведете.
+        */
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int min = int.MaxValue;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] < min)
+                {
+                    min = nums[i];
+                }
+            }
+            Console.WriteLine(min); 
+            // Total: O(N)
+        }
+    }
+}
