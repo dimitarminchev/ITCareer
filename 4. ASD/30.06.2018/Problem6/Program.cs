fcf3533deb607b5f3acb57c498d977452d4b101d
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-06-30 18:41:08 +0300

    30.06.2018

---
+++ 4. ASD/30.06.2018/Problem6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem6
+{
+    class Program
+    {
+        /* Problem 6. Наредени двойки
+            Определете сложността (максимания брой стъпки) на програма, 
+            която чете от конзолата последователност от две цели числа m и n .
+            Да се изведат всички въможни наредени двойки цели числа  ( p, q ) 
+            които се менят съответно за p [1..m],  q в [1..n]
+         */
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.WriteLine(i+" "+j);
+                }
+            }
+            // Total: O(N^2)
+        }
+    }
+}
