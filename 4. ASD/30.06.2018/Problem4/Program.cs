0e68d798d32480d40dce7975d1b3d6c10f08aa76
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-06-30 16:40:04 +0300

    30.06.2018

---
+++ 4. ASD/30.06.2018/Problem4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // N
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            // 3*N = O(N)
+            Console.WriteLine($"Sum={nums.Sum()}; Average={Math.Round(nums.Average()),2}");
+
+            // Total: O(N)
+        }
+    }
+}
