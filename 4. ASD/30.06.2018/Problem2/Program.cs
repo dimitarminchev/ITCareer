0e3d18256ee5fac4a6e7a5284acccc8dff1542e1
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-06-30 16:22:58 +0300

    30.06.2018

---
+++ 4. ASD/30.06.2018/Problem2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // O(N)
+            List<int> startNumbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> finalNumbers = new List<int>();
+            int numberToInsert = int.Parse(Console.ReadLine());
+
+            // O(N^2)
+            for (int i = 0; i < startNumbers.Count; i++)
+            {
+                if(startNumbers[i] == numberToInsert)
+                {
+                    finalNumbers = startNumbers;
+                    startNumbers.Insert(i, numberToInsert); 
+                }                   
+            }
+
+            // 2 * O(N)
+            Console.WriteLine(String.Join(" ", finalNumbers));
+            Console.WriteLine(String.Join(" ", startNumbers));
+
+            // Total: O(N) + O(N^2) + 2*O(N) = O(N^2)
+        }
+    }
+}
