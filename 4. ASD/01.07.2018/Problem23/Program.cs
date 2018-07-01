72ae80bd693458e7f77a2815d46d6fc77b987a36
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-01 14:04:24 +0300

    01.07.2018

---
+++ 4. ASD/01.07.2018/Problem23/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem23
+{
+    class Program
+    {
+        /* Problem 23. CircularQueue */
+        static void Main(string[] args)
+        {
+            // Имплементирайте кръгова опашка
+        }
+    }
+}
---
+++ 4. ASD/01.07.2018/Problem24/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem24
+{
+    class Program
+    {
+        /* Problem 24. ArrayStack */
+        static void Main(string[] args)
+        {
+            // Статична имплементация на стек
+        }
+    }
+}
