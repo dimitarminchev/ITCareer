0cf7c7227af8fb26c9f945d1f68d584d0015d952
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-01 09:50:57 +0300

    01.07.2018

---
+++ 4. ASD/01.07.2018/Demo1/ArrayList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    public class ArrayList<T>
+    {
+        private const int INITIAL_SIZE = 2;
+        private T[] items;
+
+        // Брой
+        private int length;
+        public int Length
+        {
+            get { return this.length; }
+            private set { this.length = value; }
+        }
+
+        // Капацитет 
+        private int capacity;
+        public int Capacity
+        {
+            get { return this.capacity; }
+            private set { this.capacity = value; }
+        }
+
+        // Конструктор
+        public ArrayList(int size = INITIAL_SIZE)
+        {
+            this.capacity = size;
+            this.length = 0;
+            this.items = new T[size];
+        }
+
+        // Итератор
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > length)
+                throw new IndexOutOfRangeException();
+                return this.items[index];
+            }
+            set
+            {
+                if (index < 0 || index > length)
+                throw new IndexOutOfRangeException();
+                this.items[index] = value;
+                this.length++;
+            }
+        }
+/*
+        public void Add(T item) { }
+        public T Get(int index) { }
+        public void Set(int index, T item) { }
+        public T RemoveAt(int index) { }
+*/
+    }
+}
---
+++ 4. ASD/01.07.2018/Demo1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        /* Разтеглив Масив */
+        static void Main(string[] args)
+        {
+            ArrayList<string> list = new ArrayList<string>();
+
+            Console.WriteLine("Length = {0}",list.Length);
+            Console.WriteLine("Capacity = {0}", list.Capacity);
+
+            list[0] = "Иванчо";
+            list[1] = "Марийка";
+        }
+    }
+}
