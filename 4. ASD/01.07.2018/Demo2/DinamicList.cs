165191b9f1578d82d21055854684afe30ff6c8a5
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-01 10:57:55 +0300

    01.07.2018

---
+++ 4. ASD/01.07.2018/Demo2/DinamicList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2
+{
+    public class DinamicList
+    {
+        // Елемент
+        public class Node
+        {
+            // Елемент от списъка
+            private object element;
+            public object Element
+            {
+                get { return element;  }
+                set { element = value; }
+            }
+            // Следващ елемент от списъка
+            private Node next;
+            public object Next
+            {
+                get { return element; }
+                set { element = value; }
+            }
+            //TODO: Добавете свойства за Element и Next с публични get и set
+            public Node(object element, Node prevNode)
+            {
+                this.element = element;
+                prevNode.next = this;
+            }
+            public Node(object element)
+            {
+                this.element = element;
+                next = null;
+            }
+        }
+        // Списък
+        private Node head;
+        private Node tail;
+        private int count;
+        public DynamicList() {…}
+        public void Add(object item) { … }
+        public object Remove(int index) { … }
+        public int Remove(object item) { … }
+        public int IndexOf(object item) { … }
+        public bool Contains(object item) { … }
+        public object this[int index] { …}
+
+    }
+}
---
+++ 4. ASD/01.07.2018/Demo2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2
+{
+    class Program
+    {
+        /* Свързан списък */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
