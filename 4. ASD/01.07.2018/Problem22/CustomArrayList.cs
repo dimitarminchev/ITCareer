b6493a99b0849b6a2d59924e53968ae96564a722
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-01 18:30:25 +0300

    01.07.2018

--- 4. ASD/01.07.2018/Problem22/CustomArrayList.cs
+++ 4. ASD/01.07.2018/Problem22/CustomArrayList.cs
@@ -8,6 +8,126 @@ namespace Problem22
 {
     class CustomArrayList
     {
-        // ...
+        private const int INITIAL_CAPACITY = 4;
+        private object[] arr; 
+
+        // Брой
+        private int count;
+        public int Count
+        {
+            get { return this.count; }
+            private set { this.count = value; }
+        }
+
+        // Капацитет 
+        private int capacity;
+        public int Capacity
+        {
+            get { return this.capacity; }
+            private set { this.capacity = value; }
+        }
+
+        // Конструктор
+        public CustomArrayList(int size = INITIAL_CAPACITY)
+        {
+            arr = new object[size];
+            capacity = size;
+            count = 0;
+        }
+
+        // Итератор 
+        private object this[int index]
+        {
+            get
+            {
+                OutOfRange(index);
+                return this.arr[index];
+            }
+            set
+            {
+                OutOfRange(index);
+                this.arr[index] = value;
+                this.count++;
+            }
+        }
+
+        // Добавяне
+        public void Add(object item)
+        {
+            if (capacity == count)
+            {
+                capacity *= 2; // Разтягане
+                object[] temp = arr;
+                arr = new object[capacity];
+                for (int i = 0; i < temp.Length; i++) arr[i] = temp[i];
+                arr[count] = item;
+            }
+            else arr[count] = item;
+            count++;
+        }
+
+        // Връща елемент 
+        public object Get(int index)
+        {
+            OutOfRange(index);
+            return arr[index];
+        }
+
+        // Премахване по индекс
+        public object Remove(int index)
+        {
+            OutOfRange(index);
+            var temp = arr[index];
+            arr = arr.Take(index).Concat(arr.Skip(index + 1)).ToArray();
+            count--;
+            return temp;
+        }
+
+        // TODO: Премахване по обект
+        public int Remove(object item)
+        {
+            return 42;
+        }
+
+        // Добавяне // TO TEST
+        public void Insert(int index, object item)
+        {
+            OutOfRange(index);
+
+            // v1
+            //var t1 = arr.Take(index).ToArray();
+            //var t2 = new object[1] { item };
+            //var t3 = arr.Skip(index).ToArray();
+            //arr = t1.Concat(t2.Concat(t3)).ToArray();
+
+            // v2
+            arr = arr.Take(index).Concat((new object[1] { item }).Concat(arr.Skip(index))).ToArray();
+
+        }
+
+        public int IndexOf(object item)
+        {
+            return 42;
+        }
+
+        // Изистване 
+        public void Clear()
+        {
+            while (count > 0) Remove(0);
+            return;
+        }
+
+        public bool Contains(object item)
+        {
+            return true;
+        }
+
+
+        // Проверка дали сме в границите
+        private void OutOfRange(int index)
+        {
+            if (index < 0 || index > count)
+            throw new IndexOutOfRangeException();
+        }
     }
 }
--- 4. ASD/01.07.2018/Problem22/Program.cs
+++ 4. ASD/01.07.2018/Problem22/Program.cs
@@ -12,6 +12,23 @@ namespace Problem22
         static void Main(string[] args)
         {
             CustomArrayList list = new CustomArrayList();
+
+            Console.WriteLine("Count = {0}", list.Count);
+            Console.WriteLine("Capacity = {0}", list.Capacity);
+
+            list.Add(100); // 0
+            list.Add(200); // 1
+            list.Add(300); // 2
+
+            list.Clear(); // zero
+
+            list.Add(400); // 0 
+            list.Add(500); // 1
+
+            list.Insert(0, 69696969);
+
+            Console.WriteLine("Count = {0}", list.Count);
+            Console.WriteLine("Capacity = {0}", list.Capacity);
         }
     }
 }
