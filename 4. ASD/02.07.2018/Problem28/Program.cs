0acbc10306c5c7842f0c4dce159683375edb9569
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-02 11:17:11 +0300

    02.07.2018

---
+++ 4. ASD/02.07.2018/Problem28/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem28
+{
+    class Program
+    {
+        /* Problem 28. LinkedStack  */
+        static void Main(string[] args)
+        {
+            LinkedStack stack = new LinkedStack();
+        }
+    }
+}
