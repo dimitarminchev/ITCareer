518a0dd645dc7a199501a5dd762b3dbdc22356f7
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-02 11:14:31 +0300

    02.07.2018

---
+++ 4. ASD/02.07.2018/Problem27/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem27
+{
+    class Program
+    {
+        /* Problem 27. LinkedQueue */
+        static void Main(string[] args)
+        {
+            LinkedQueue q = new LinkedQueue();
+        }
+    }
+}
