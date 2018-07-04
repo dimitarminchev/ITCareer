9f4c1c73b88188e1a96ed6c2a48e118289dd6be9
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-04 07:30:44 +0300

    03.07.2018

---
+++ 4. ASD/03.07.2018/Problem31/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3.1. Упражнениe върху върху линейни структури от данни
+        }
+    }
+}
