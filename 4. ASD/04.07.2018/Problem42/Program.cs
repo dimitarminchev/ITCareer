7316fea835b63ab14b6113faac82c8f730a3ca71
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-04 17:05:05 +0300

    04.07.2018

--- 4. ASD/04.07.2018/Problem42/Program.cs
+++ 4. ASD/04.07.2018/Problem42/Program.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Problem42 {
-    class Program {
-        static void Main(string[] args) {
+namespace Problem42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
             int number = int.Parse(Console.ReadLine());
-            if (number == 4) {
+
+            // 4. Уравновесен масив
+            if (number == 4)
+            {
                 int n = int.Parse(Console.ReadLine());
                 var input = Console.ReadLine().Split(' ').Select(int.Parse);
                 var arr1 = input.Take(n / 2).ToArray();
@@ -17,11 +23,15 @@ namespace Problem42 {
                 Sorting.SortByDescending(arr2);
                 Console.WriteLine(string.Join(" ", arr1) + " " + string.Join(" ", arr2));
             }
-            else if (number == 5) {
+
+            // 5. Сортиране по произволна колона
+            if (number == 5)
+            {
                 var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
                 var d2arr = new int[input[0]][];
                 var sort = new int[input[1]];
-                for (int r = 0; r < input[0]; r++) {
+                for (int r = 0; r < input[0]; r++)
+                {
                     d2arr[r] = new int[input[1]];
                     var arr = Console.ReadLine().Split(' ');
                     for (int c = 0; c < input[1]; c++)
