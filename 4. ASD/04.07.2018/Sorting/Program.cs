6bc7227a91a6881c8c04beee3dc0732f51900edc
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-04 11:23:01 +0300

    04.07.2018

---
+++ 4. ASD/04.07.2018/Sorting/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class Program
+    {
+        // Измерване на време на работа на алгоритъма
+        private static Stopwatch timer = new Stopwatch();
+
+        static void Main(string[] args)
+        {
+            const int SIZE = 100000;
+            int[] numbers = new int[SIZE];
+            for (int i = 0; i < SIZE; i++) numbers[i] = i;
+            // Console.WriteLine(string.Join(",", numbers));
+
+            // 1. Shifting
+            timer.Start();
+            Sorting.Shifting(numbers);
+            timer.Stop();
+            //Console.WriteLine("Shifting...\n{0}",string.Join(",", numbers));          
+            Console.WriteLine("Time = {0} ms\n", timer.Elapsed.TotalMilliseconds);
+
+            // 2. SelectionSort = O(N^2)
+            timer.Start();
+            Sorting.SelectionSort(numbers);
+            timer.Stop();
+            //Console.WriteLine("SelectionSort...\n{0}", string.Join(",", numbers));
+            Console.WriteLine("Time = {0} ms\n", timer.Elapsed.TotalMilliseconds);
+
+            // 3. Shifting
+            timer.Start();
+            Sorting.Shifting(numbers);
+            timer.Stop();
+            //Console.WriteLine("Shifting...\n{0}", string.Join(",", numbers));
+            Console.WriteLine("Time = {0} ms\n", timer.Elapsed.TotalMilliseconds);
+
+        }
+    }
+}
---
+++ 4. ASD/04.07.2018/Sorting/Sorting.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    static class Sorting
+    {
+        // 1. Размяна на елементи
+        public static void Swap<T>(T[] array, int first, int second)
+        {
+            T temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+
+        // 2. Сравнение на два елемента
+        public static bool Less(IComparable first, IComparable second)
+        {
+            return first.CompareTo(second) < 0;
+        }
+
+        // 3. Проверка дали структурата е сортирана = O(N)
+        public static bool IsSorted<T> (T[] array) where T : IComparable
+        {
+            for (int i = 1; i < array.Length; i++)
+            if (!Less(array[i-1],array[i])) return false;
+            return true;
+        }
+
+        // 4. Разбъркване на елементите = O(N)
+        private static Random random = new Random();
+        public static void Shifting<T>(T[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int r = i + random.Next(0, array.Length - i);
+                Swap(array, i, r);
+            }
+        }
+
+        // 5. Сортиране по метода на пряката селеция = O(N^2)
+        public static void SelectionSort<T> (T[] array) where T : IComparable
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                    if (Less(array[j], array[min]))
+                        min = j;
+                Swap(array, i, min);
+            }
+        }
+
+    }
+}
