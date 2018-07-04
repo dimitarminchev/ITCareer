04273494b4a33aae23b187709822c783ce4e9616
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-04 09:57:30 +0300

    04.07.2018

---
+++ 4. ASD/04.07.2018/Recursion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        // Измерване на време на работа на алгоритъма
+        private static Stopwatch timer = new Stopwatch();
+
+        // Рекурсия
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            // 1. Факториел
+            Console.WriteLine("Factoriel ...");
+            timer.Start();
+            Console.WriteLine("{0}", Recursion.Fak(n));
+            timer.Stop();
+            Console.WriteLine("Time = {0} ms\n", timer.Elapsed.TotalMilliseconds);
+
+            // 2. Фибоначи
+            Console.WriteLine("Fibonacci ...");
+            timer.Start();
+            Console.WriteLine("{0}", Recursion.Fib(n));
+            timer.Stop();
+            Console.WriteLine("Time = {0} ms\n", timer.Elapsed.TotalMilliseconds);
+            // for (int i = 0; i < n; i++) Console.Write("{0}, ", Recursion.Fib(i+1));
+
+            // 3. Графика
+            Console.WriteLine("Draw ...");
+            timer.Start();
+            Recursion.Draw(n);
+            timer.Stop();
+            Console.WriteLine("Time = {0} ms\n", timer.Elapsed.TotalMilliseconds);
+        }
+    }
+}
---
+++ 4. ASD/04.07.2018/Recursion/Recursion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    static class Recursion
+    {
+        // 1. Факториел
+        public static ulong Fak(int n)
+        {
+            if (n <= 1) return 1;
+            else return (ulong)n * Fak(n - 1);
+        }
+
+        // 2. Фибоначи
+        public static ulong Fib(int n)
+        {
+            if (n <= 2)  return 1;
+            else return Fib(n - 2) + Fib(n - 1);
+        }
+
+        // 3. Графика
+        public static void Draw(int n)
+        {
+            // 1. Условие за изход от рекурсията
+            if (n == 0) return;
+
+            // 2. Прав ход на рекурсията
+            Console.WriteLine(new string('*',n));
+
+            // 3. Рекурсивно извикване
+            Draw(n - 1);
+
+            // 4. Обратен ход на рекурсията
+            Console.WriteLine(new string('#', n));
+        }
+        
+    }
+}
