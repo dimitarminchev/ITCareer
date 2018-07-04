c9eb547a33a211930bbbc4d856fc81cdff80d22d
destbg <niki4616@gmail.coom>
2018-07-04 16:55:04 +0300

    04.07.2018

--- 4. ASD/04.07.2018/Problem42/Program.cs
+++ 4. ASD/04.07.2018/Problem42/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Problem42
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Problem42 {
+    class Program {
+        static void Main(string[] args) {
+            int number = int.Parse(Console.ReadLine());
+            if (number == 4) {
+                int n = int.Parse(Console.ReadLine());
+                var input = Console.ReadLine().Split(' ').Select(int.Parse);
+                var arr1 = input.Take(n / 2).ToArray();
+                var arr2 = input.Skip(n / 2).ToArray();
+                Sorting.SortBy(arr1);
+                Sorting.SortByDescending(arr2);
+                Console.WriteLine(string.Join(" ", arr1) + " " + string.Join(" ", arr2));
+            }
+            else if (number == 5) {
+                var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var d2arr = new int[input[0]][];
+                var sort = new int[input[1]];
+                for (int r = 0; r < input[0]; r++) {
+                    d2arr[r] = new int[input[1]];
+                    var arr = Console.ReadLine().Split(' ');
+                    for (int c = 0; c < input[1]; c++)
+                        d2arr[r][c] = int.Parse(arr[c]);
+                }
+                Sorting.Sort2D(d2arr, input[2] - 1);
+                for (int r = 0; r < input[0]; r++)
+                    Console.WriteLine(string.Join(" ", d2arr[r]));
+            }
         }
     }
 }
---
+++ 4. ASD/04.07.2018/Problem42/Sorting.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem42 {
+    class Sorting {
+        public static void SortBy<T>(T[] array) where T : IComparable<T> {
+            for (int i = 0; i < array.Length; i++) {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                    if (Less(array[j], array[min]))
+                        min = j;
+                Swap(array, i, min);
+            }
+        }
+
+        public static void SortByDescending<T>(T[] arr) where T : IComparable<T> {
+            for (int i = 0; i < array.Length; i++) {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                    if (!Less(array[j], array[min]))
+                        min = j;
+                Swap(array, i, min);
+            }
+        }
+
+        private static void Swap<T>(T[] arr, int i, int min) where T : IComparable<T> {
+            var num = arr[min];
+            arr[min] = arr[i];
+            arr[i] = num;
+        }
+
+        private static bool Less<T>(T t1, T t2) where T : IComparable<T> {
+            return t1.CompareTo(t2) < 0;
+        }
+
+        public static void Sort2D<T>(T[][] arr, int pos) where T : IComparable<T> {
+            for (int i = 0; i < arr.Length; i++) {
+                int min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                    if (Less(arr[j][pos], arr[min][pos]))
+                        min = j;
+                Swap2D(arr, i, min);
+            }
+        }
+
+        private static void Swap2D<T>(T[][] arr, int i, int min) where T : IComparable<T> {
+            var num = arr[min];
+            arr[min] = arr[i];
+            arr[i] = num;
+        }
+    }
+}
