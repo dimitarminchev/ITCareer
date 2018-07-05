5b5908130ff2d18ebe790d52f7410dbc50bf1a68
destbg <niki4616@gmail.coom>
2018-07-05 10:50:37 +0300

    TrainsSkeleton

---
+++ 4. ASD/TrainsSkeleton/Train.cs
@@ -0,0 +1,20 @@
+﻿namespace TrainsSkeleton {
+    internal class Train {
+        private readonly int number;
+        private readonly string name;
+
+        internal Train(int number, string name, string type, int cars) {
+            this.number = number;
+            this.name = name;
+            Type = type;
+            Cars = cars;
+        }
+
+        public string Type { get; }
+        public int Cars { get; }
+
+        public override string ToString() {
+            return $"{number} {name} {Type} {Cars}";
+        }
+    }
+}
