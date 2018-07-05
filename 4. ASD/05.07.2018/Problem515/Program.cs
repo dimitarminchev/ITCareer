3f851b0060895b9eb3856aaebfe7970c80d4cfc7
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-05 12:21:25 +0300

    05.07.2018

--- 4. ASD/05.07.2018/Problem515/Program.cs
+++ 4. ASD/05.07.2018/Problem515/Program.cs
@@ -9,9 +9,36 @@ namespace Problem515
     class Program
     {
         // Problem 515. Игли
+        static void Needles(int c, int n, int[] array, int[] toInsert)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                int index = c;
+                for (int k = c - 1; k >= 0; k--)
+                {
+                    if (array[k] == 0 || array[k] >= toInsert[i])
+                    {
+                        index--;
+                    }
+                    else
+                    {
+                        Console.Write(index + " ");
+                        break;
+                    }
+                    if (index == 0) Console.Write(index + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            // TODO
+            int[] cnInput = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] startArray = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] toInsert = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int c = cnInput[0];
+            int n = cnInput[1];
+            Needles(c, n, startArray, toInsert);
         }
     }
 }
