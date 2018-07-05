e7113e79aa04b7bcb01ae6690ecfaec271dfe977
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-05 09:43:47 +0300

    05.07.2018

--- 4. ASD/05.07.2018/Demo1/Program.cs
+++ 4. ASD/05.07.2018/Demo1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,36 @@ namespace Demo1
 {
     class Program
     {
+        // Измерване на време на работа на алгоритъма
+        private static void MesureTime(Action method)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            method(); 
+            timer.Stop();
+            Console.WriteLine("Time = {0} ms", timer.Elapsed.TotalMilliseconds);
+        }
+
         // Алгоритми за търсене
         static void Main(string[] args)
         {
+            int[] array = new int[] { 5, -5, 4, -4, 3, -3, 2, -2, 1, -1 };
+            int index = 0, key = 2;
+
+            // 1. LinearSearch = O(N)
+            Console.WriteLine("1. LinearSearch = O(N)");
+            MesureTime(() =>  index = Search.LinearSearch(array, key));
+            if (index == -1) Console.WriteLine("Not Found\n");
+            else Console.WriteLine("Index = {0}\n", index);
+
+
+            // 2. RecursiveBinarySearch = O(log(N))
+            Console.WriteLine("2. RecursiveBinarySearch = O(log(N))");
+            MesureTime(() => index = Search.RecursiveBinarySearch(array, key, 0, array.Count()));
+            if (index == -1) Console.WriteLine("Not Found\n");
+            else Console.WriteLine("Index = {0}\n", index);
+
+            // 3. BinarySearch = O(log(N))
         }
     }
 }
---
+++ 4. ASD/05.07.2018/Demo1/Search.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Search
+    {
+        // 1. LinearSearch = O(N)
+        public static int LinearSearch<T> (T[] array, T key) where T : IComparable
+        {
+            for(int index = 0; index < array.Count(); index++)
+            {
+                if (array[index].CompareTo(key) == 0)
+                {
+                    return index;
+                }
+            }
+            return -1; 
+        }
+
+        // 2. RecursiveBinarySearch = O(log(N))
+        public static int RecursiveBinarySearch(int[] array, int key, int start, int end)
+        {
+            if (end < start) return -1; 
+            int mid = (start + end) / 2;
+            if (array[mid] < key) return RecursiveBinarySearch(array, key, start, mid - 1);
+            else if (array[mid] > key) return RecursiveBinarySearch(array, key, mid + 1, end);
+            else return mid; 
+        }
+
+        // 3. BinarySearch = O(log(N))
+    }
+}
