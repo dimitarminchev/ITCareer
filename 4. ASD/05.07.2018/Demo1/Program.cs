c58299835cf0d5668c7e8e9242954389afef115c
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-05 08:58:37 +0300

    05.07.2018

---
+++ 4. ASD/05.07.2018/Demo1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        // Алгоритми за търсене
+        static void Main(string[] args)
+        {
+        }
+    }
+}
