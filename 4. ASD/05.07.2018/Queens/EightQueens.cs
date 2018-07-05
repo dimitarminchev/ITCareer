56c3e87b176c1f37719d849bdd16d76c73db1297
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-05 18:05:44 +0300

    07.05.2018

--- 4. ASD/05.07.2018/Queens/EightQueens.cs
+++ 4. ASD/05.07.2018/Queens/EightQueens.cs
@@ -8,17 +8,15 @@ namespace Queens
 {
     class EightQueens
     {
+        static int solutionFound = 0;
         const int Size = 8;
-        static bool[,] chassboard = new bool[Size, Size];
+        static bool[,] scoreboard = new bool[Size, Size];
 
         // Атакувани позиции
         static HashSet<int> attackedRows = new HashSet<int>();
         static HashSet<int> attackedColumns = new HashSet<int>();
         static HashSet<int> attackedLeftDiagonals = new HashSet<int>();
         static HashSet<int> attackedRightDiagonals= new HashSet<int>();
-
-        // leftDiag = col - row
-        // rightDiag = col + row
 
         // BackTraking Algorithm
         public static void PutQueens(int row)
@@ -38,7 +36,7 @@ namespace Queens
             }
         }
 
-        // може ли да поставим царица
+        // Може ли да поставим царица?
         static bool CanPlaceQueen(int row, int col)
         {
             var occupied = attackedRows.Contains(row) ||
@@ -46,6 +44,42 @@ namespace Queens
                            attackedLeftDiagonals.Contains(col - row) ||
                            attackedRightDiagonals.Contains(row + col);
             return !occupied;
+        }
+
+        // Маркиране на полета, които царицата атакува
+        static void MarkAllAttackedPositions(int row, int col)
+        {
+            attackedRows.Add(row); // Ред 
+            attackedColumns.Add(col); // Колона
+            attackedLeftDiagonals.Add(col - row); // Ляв диагонал
+            attackedRightDiagonals.Add(row + col); // Десн диагонал
+            scoreboard[row, col] = true;
+        }
+
+        // Демаркиране на полета, които царицата атакува
+        static void UnarkAllAttackedPositions(int row, int col)
+        {
+            attackedRows.Remove(row); // Ред 
+            attackedColumns.Remove(col); // Колона
+            attackedLeftDiagonals.Remove(col - row); // Ляв диагонал
+            attackedRightDiagonals.Remove(row + col); // Десн диагонал
+            scoreboard[row, col] = false;
+        }
+
+        // Отпечатване на решението
+        static void PrintSolution()
+        {
+            for (int row = 0; row < Size; row++)
+            {
+                for (int col = 0; col < Size; col++)
+                {
+                    if (scoreboard[row, col]) Console.Write('*');
+                    else Console.Write('-');
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            solutionFound++;
         }
     }
 }
