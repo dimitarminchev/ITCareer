b28d48b95bd0a99fde9a41a3f0d96bd68c03a044
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-07-07 08:49:40 +0300

    07.07.2018

---
+++ 4. ASD/07.07.2018/Words/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words
+{
+    class Program
+    {
+        // 6.3. Думи
+        static void Main(string[] args)
+        {
+            string inputLine = Console.ReadLine();
+
+            Recursion rec = new Recursion();
+            rec.InputSet = rec.MakeCharArray(inputLine);
+            rec.CalcPermutation(0);
+
+            Console.WriteLine(rec.PermutationCount);
+        }
+    }
+}
