160207ede31c8d17eae836a6847bc581b2bc9cb8
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-01 13:48:03 +0300

    01.09.2018

--- 5. Object Oriented Programming/2018.09.01/Axe/Axe.cs
+++ 5. Object Oriented Programming/2018.09.01/Axe/Axe.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Axe
+namespace Axes
 {
     // Axe durability drop with 5 
     public class Axe
---
+++ 5. Object Oriented Programming/2018.09.01/AxeTests/AxeTests.cs
@@ -0,0 +1,44 @@
+﻿using Axes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AxeTests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        [Test] // Тест за издържливостта на бравата след атака
+        public void AxeLosesDurabilityAfterAttack()
+        {
+            // Arrange
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+
+            // Act
+            axe.Attack(dummy);
+
+            // Assets
+            Assert.AreEqual(1, axe.DurabilityPoints, "Axe Durability does not change after attack.");
+        }
+
+        [Test] // Тест дали счупена бравата да атакакува
+        public void BrokenAxeCantAttack()
+        {
+            // Arrange
+            Axe axe = new Axe(2,2);
+            Dummy dummy = new Dummy(20,20);
+
+            // Act
+            axe.Attack(dummy);
+            axe.Attack(dummy);
+
+            // Assets
+            var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+            Assert.That(ex.Message, Is.EqualTo("Axe is broken."));
+        }
+    }
+}
