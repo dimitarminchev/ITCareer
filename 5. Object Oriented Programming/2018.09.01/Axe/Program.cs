68dcc24160bb3ae8d3c6cafdc4c48bca2ae29628
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-01 13:22:52 +0300

    01.09.2018

---
+++ 5. Object Oriented Programming/2018.09.01/Axe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Axe
+{
+    class Program
+    {
+        // Пример за компонентно тестване
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unit Test Sample");
+        }
+    }
+}
