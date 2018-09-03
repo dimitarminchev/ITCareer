824aa28adae9b1cbcca3d4b519efbd9e72e2bece
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-03 10:30:24 +0300

    03.09.2018

---
+++ 5. Object Oriented Programming/2018.09.03/App1/OddEven.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    // Интефейса изглежда като клас, но не имплементира методите 
+    public interface IOddEven
+    {
+        // Чисто абстрактни методи (без имплементация)
+        bool IsOdd(int number);
+        bool IsEven(int number);
+    }
+
+    // Наследяващ клас
+    public class OddEven : IOddEven
+    {
+        // Имплементации на абстрактните методите от интерфейса
+        public bool IsOdd(int number)
+        {
+            return number % 2 != 0 ? true : false;
+        }
+        public bool IsEven(int number)
+        {
+            return number % 2 == 0 ? true : false;
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.03/App1Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("App1Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("App1Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a3f9bc54-5a5b-415f-8e03-60517a782f3b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
