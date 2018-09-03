fc8e8b5fe57d29e7ba442de02b6294b91b422bd9
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-03 11:15:16 +0300

    03.09.2018

---
+++ 5. Object Oriented Programming/2018.09.03/App2Tests/UnitTest2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using App2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace App2Tests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void HeroGainsExperienceAfterAttackIfTargetDies()
+        {
+            // Arrange
+            Mock<ITarget> fakeTarget = new Mock<ITarget>();
+            fakeTarget.Setup(p => p.Health).Returns(0);
+            fakeTarget.Setup(p => p.GiveExperience()).Returns(20);
+            fakeTarget.Setup(p => p.IsDead()).Returns(true);
+            Mock<IWeapon> fakeWeapon = new Mock<IWeapon>();
+            Hero hero = new Hero("Pesho", fakeWeapon.Object);
+
+            // Akt
+            hero.Attack(fakeTarget.Object);
+
+            // Assert
+            Assert.AreEqual(20, hero.Experience);
+        }
+
+    }
+}
