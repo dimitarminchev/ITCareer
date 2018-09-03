fa67fb1fce1b02e54a1dbc5418b503e226f89345
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-03 11:00:13 +0300

    03.09.2018

---
+++ 5. Object Oriented Programming/2018.09.03/App2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2
+{
+    class Program
+    {
+        // Пример за компонентни тестване 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unit Test Sample");
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.03/App2Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using App2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App2Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string HeroName = "Pesho";
+
+        [TestMethod]
+        public void HeroGainsExperienceAfterAttackIfTargetDies()
+        {
+            // Arrange
+            ITarget fakeTarget = new FakeTarget();
+            IWeapon fakeWeapon = new FakeWeapon();
+            Hero hero = new Hero(HeroName, fakeWeapon);
+
+            // Act
+            var xp = hero.Experience;
+            hero.Attack(fakeTarget);
+
+            // Assert
+            Assert.IsTrue(hero.Experience > xp);
+        }
+
+    }
+}
