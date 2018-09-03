e9123d4b1f1537b0e80e39cf5d3f2310f16f95e5
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-03 11:30:53 +0300

    03.09.2018

---
+++ 5. Object Oriented Programming/2018.09.03/SimplePerson/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePerson
+{
+    // Човечето
+    public class Person
+    {
+        // Поле: Име
+        private string name;
+        // реализираме свойство на полето name
+        public String Name
+        { 
+            get { return name; }
+            set { name = value; }
+        }
+
+        // Поле: Възраст
+        private int age;
+        // реализираме свойство на полето age
+        public int Age
+        { 
+            get { return age; }
+            set { age = value; }
+        }
+
+        // Най-неввероятния начин да се представиш
+        public void IntroduceYourself()
+        {
+            Console.WriteLine("Здравейте! Аз съм {0} и съм на {1} години.", name, age);
+        }
+    }
+
+}
---
+++ 5. Object Oriented Programming/2018.09.03/SimplePerson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Създаваме обект инстанция на класа "Човечето"
+            Person firstPerson = new Person();
+
+            // Задаваме свойствата на обекта
+            firstPerson.Name = "Гошо";
+            firstPerson.Age = 15;
+
+            // Изпълняваме метод от класа
+            firstPerson.IntroduceYourself();
+        }
+    }
+}
