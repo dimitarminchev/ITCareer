fab2602a5fbf85910acbf08566a0be2f4a6e7aac
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-04 11:29:00 +0300

    04.09.2018

---
+++ 5. Object Oriented Programming/2018.09.04/31_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_1
+{
+    class Program
+    {
+        // Problem 1. Кутия с T
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+            Console.WriteLine(box.Remove());
+            box.Add(4);
+            box.Add(5);
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
