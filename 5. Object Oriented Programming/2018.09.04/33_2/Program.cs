9ce88d1d6fed26aabb31715d4fc6473bdefcde8d
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-04 13:54:40 +0300

    04.09.2018

---
+++ 5. Object Oriented Programming/2018.09.04/33_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> lis = new CustomList<int>();
+            lis.Add(1);
+            lis.Add(2);
+            lis.Add(3);
+            Console.WriteLine(lis.CountGreaterThan(1));
+            Console.WriteLine(lis.Min());
+            Console.WriteLine(lis.Max());
+            lis.Swap(1, 2);
+            Console.WriteLine(lis.ToString());
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.04/33_2_Tests/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _33_2;
+
+namespace _33_2_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TetsAddingThreeElements()
+        {
+            // Arrange
+            CustomList<int> lis = new CustomList<int>();
+
+            // Act
+            lis.Add(1);
+            lis.Add(2);
+            lis.Add(3);
+
+            // Assert
+            Assert.IsTrue(lis.Count == 3);
+        }
+        [TestMethod]
+        public void TestSwapingTwoElements()
+        {
+            // Arrange
+            CustomList<int> lis = new CustomList<int>();
+
+            // Act
+            lis.Add(1);
+            lis.Add(2);
+            lis.Swap(0, 1);
+
+            // Assert
+            Assert.IsTrue(lis.ToString() == "2 1 ");
+        }
+    }
+}
