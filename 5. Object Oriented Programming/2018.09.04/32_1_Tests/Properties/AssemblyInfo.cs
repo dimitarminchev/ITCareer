02d2aad1a3e6abfb1e1cac119302d4180394836d
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-04 14:44:21 +0300

    04.09.2018

---
+++ 5. Object Oriented Programming/2018.09.04/32_1_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("32_1_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("32_1_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b520cfa-0155-43e4-a09c-a6e472f7b005")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 5. Object Oriented Programming/2018.09.04/33_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> lis = new CustomList<int>();
+            lis.Add(33);
+            lis.Add(22);
+            lis.Add(11);
+            lis.Sort();
+            Console.WriteLine(lis.ToString());
+        }
+    }
+}
