d7bd118a3e87551f82744bf538657d1fe39dd51d
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-04 12:26:41 +0300

    04.09.2018

---
+++ 5. Object Oriented Programming/2018.09.04/31_2_Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using _31_2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _31_2_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTheBox()
+        {
+            // Arrange
+            Box<int> box = new Box<int>();
+
+            // Act
+            box.Add(123);
+
+            // Assert
+            Assert.IsTrue("System.Int32: 123" == box.ToString());
+        }
+    }
+}
