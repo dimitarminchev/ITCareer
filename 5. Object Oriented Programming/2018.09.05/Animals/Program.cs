c89d36524a8cee1e694d3f7e86ec10052bcfb5a3
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-05 11:41:43 +0300

    05.09.2018

---
+++ 5. Object Oriented Programming/2018.09.05/Animals/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            Cat cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.05/AnimalsTests/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Animals;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnimalsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AnimalEat()
+        {
+            // Arrange
+            Type animal = typeof(Animal); // Get type pointer
+            // Act
+            var method = animal.GetMethod("Eat");
+            // Assert
+            Assert.IsTrue(method.ReturnType == typeof(void));
+        }
+
+        [TestMethod]
+        public void DogTest()
+        {
+            // Arrange
+            Type animal = typeof(Dog); // Get type pointer
+            // Act
+            var method = animal.GetMethod("Bark");
+            // Assert
+            Assert.IsNotNull(method);
+        }
+
+        [TestMethod]
+        public void EatParameters()
+        {
+            // Arrange
+            Type animal = typeof(Animal); // Get type pointer
+            // Act
+            var method = animal.GetMethod("Eat");
+            // Assert
+            Assert.IsTrue(method.GetParameters().Length == 0);
+        }
+
+        [TestMethod]
+        public void EatReturnType()
+        {
+            // Arrange
+            Type animal = typeof(Animal); // Get type pointer
+            // Act
+            var method = animal.GetMethod("Eat");
+            // Assert
+            Assert.IsTrue(method.ReturnType == typeof(void));
+        }
+
+        [TestMethod]
+        public void BarkParameters()
+        {
+            // Arrange
+            Type animal = typeof(Dog); // Get type pointer
+            // Act
+            var method = animal.GetMethod("Bark");
+            // Assert
+            Assert.IsTrue(method.GetParameters().Length == 0);
+        }
+
+        [TestMethod]
+        public void BarkReturnType()
+        {
+            // Arrange
+            Type animal = typeof(Dog); // Get type pointer
+            // Act
+            var method = animal.GetMethod("Bark");
+            // Assert
+            Assert.IsTrue(method.ReturnType == typeof(void));
+        }
+
+        [TestMethod]
+        public void Inheritance()
+        {
+            // Arrange
+            Type animal = typeof(Dog); // Get type pointer
+            // Act & Assert
+            Assert.IsTrue(animal.BaseType == typeof(Animal));
+        }
+    }
+}
