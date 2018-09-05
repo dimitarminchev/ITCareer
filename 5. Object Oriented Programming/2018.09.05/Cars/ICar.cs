28e46b3bf32cb4dbb92a1e552690588d9d2c21b4
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-05 13:54:26 +0300

    05.09.2018

---
+++ 5. Object Oriented Programming/2018.09.05/Cars/ICar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public interface ICar
+    {
+        string Model { get; }
+        string Color { get; }
+        string Start();
+        string Stop();
+    }
+
+    public interface IElectricCar
+    {
+        int Batteries { get; }
+    }
+}
