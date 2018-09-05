ca09d5674867abf76a9123fa8a0c7070a83b70ca
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-05 13:29:05 +0300

    05.09.2018

---
+++ 5. Object Oriented Programming/2018.09.05/ColorFiguresTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ColorFiguresTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ColorFiguresTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6643d426-e678-4a5f-98fe-37982f514da0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 5. Object Oriented Programming/2018.09.05/ColorFiguresTests/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using ColorFigures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ColorFiguresTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TriangleGetAreaMethodExists()
+        {
+            // Arrange
+            Type obj = typeof(Triangle); // Get type pointer
+            // Act
+            var method = obj.GetMethod("GetArea");
+            // Assert
+            Assert.IsNotNull(method);
+        }
+
+        [TestMethod]
+        public void TriangleGetAreaMethodParameters()
+        {
+            // Arrange
+            Type obj = typeof(Triangle); // Get type pointer
+            // Act
+            var method = obj.GetMethod("GetArea");
+            // Assert
+            Assert.IsTrue(method.GetParameters().Length == 0);
+        }
+
+        [TestMethod]
+        public void TriangleGetAreaMethodReturnType()
+        {
+            // Arrange
+            Type obj = typeof(Triangle); // Get type pointer
+            // Act
+            var method = obj.GetMethod("GetArea");
+            // Assert
+            Assert.IsTrue(method.ReturnType == typeof(double));
+        }
+
+        [TestMethod]
+        public void TriangleGetNameMethodExists()
+        {
+            // Arrange
+            Type obj = typeof(Triangle); // Get type pointer
+            // Act
+            var method = obj.GetMethod("GetName");
+            // Assert
+            Assert.IsNotNull(method);
+        }
+
+        [TestMethod]
+        public void TriangleGetNameMethodParameters()
+        {
+            // Arrange
+            Type obj = typeof(Triangle); // Get type pointer
+            // Act
+            var method = obj.GetMethod("GetName");
+            // Assert
+            Assert.IsTrue(method.GetParameters().Length == 0);
+        }
+
+        [TestMethod]
+        public void TriangleGetNameMethodReturnType()
+        {
+            // Arrange
+            Type obj = typeof(Triangle); // Get type pointer
+            // Act
+            var method = obj.GetMethod("GetName");
+            // Assert
+            Assert.IsTrue(method.ReturnType == typeof(string));
+        }
+    }
+}
