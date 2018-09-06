e174cefe1c6e0e15d394cd966332995ab0a94b39
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-06 11:51:31 +0300

    06.09.2018

---
+++ 5. Object Oriented Programming/2018.09.06/452/IBirthable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _452
+{
+    interface IBirthable
+    {
+        string Birthdate { get; }
+    }
+}
