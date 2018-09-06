894d7d01d296156917f53f3fbfe648d819f975c1
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-06 15:01:22 +0300

    06.09.2018

---
+++ 5. Object Oriented Programming/2018.09.06/Polymorphism/541/Cat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _541
+{
+    public class Cat : IAnimal
+    {
+        public string MakeNoise()
+        {
+            // Console.WriteLine("Meow!");
+            return "Meow!";
+        }
+
+        public string MakeTrick()
+        {
+            // Console.WriteLine("No trick for you! I'm too lazy!");
+            return "No trick for you! I'm too lazy!";
+        }
+
+        public void Perform()
+        {
+            Console.WriteLine(MakeNoise());
+            Console.WriteLine(MakeTrick());
+        }
+    }
+}
--- 5. Object Oriented Programming/2018.09.06/Polymorphism/541/Program.cs
+++ 5. Object Oriented Programming/2018.09.06/Polymorphism/541/Program.cs
@@ -10,6 +10,13 @@ namespace _541
     {
         static void Main(string[] args)
         {
+            // Dog
+            Trainer dog = new Trainer(new Dog());
+            dog.Make();
+
+            // Cat
+            Trainer cat = new Trainer(new Cat());
+            cat.Make();
         }
     }
 }
