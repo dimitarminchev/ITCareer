fc0a12111c8894b8c44d7c74c6d7207417b2973f
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-06 11:42:58 +0300

    06.09.2018

--- 5. Object Oriented Programming/2018.09.06/451/Program.cs
+++ 5. Object Oriented Programming/2018.09.06/451/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,15 @@ namespace _451
     {
         static void Main(string[] args)
         {
+            Type personInterface = typeof(Citizen).GetInterface("IPerson");
+            PropertyInfo[] properties = personInterface.GetProperties();
+            Console.WriteLine(properties.Length);
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            IPerson person = new Citizen(name, age);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+
         }
     }
 }
