73fc1eb1bcc4f005582ead88a128e46f3d59d380
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-06 11:55:33 +0300

    06.09.2018

--- 5. Object Oriented Programming/2018.09.06/453/Program.cs
+++ 5. Object Oriented Programming/2018.09.06/453/Program.cs
@@ -6,10 +6,53 @@ using System.Threading.Tasks;
 
 namespace _453
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+
+        public static void WriteLine(object text)
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+        public static void Main()
+        {
+            var sAc = new SitesAndCalls();
+            ICall call = sAc;
+            call.Call(Console.ReadLine().Split(' '));
+            IBrowse browse = sAc;
+            browse.Browse(Console.ReadLine().Split(' '));
+        }
+    }
+
+    interface ICall
+    {
+        void Call(string[] numbers);
+    }
+
+    interface IBrowse
+    {
+        void Browse(string[] sites);
+    }
+
+    class SitesAndCalls : ICall, IBrowse
+    {
+        public void Browse(string[] sites)
+        {
+            for (int i = 0; i < sites.Length; i++)
+                if (sites[i].Any(a => char.IsNumber(a)))
+                    Program.WriteLine("Invalid URL!");
+                else
+                    Program.WriteLine("Browsing: " + sites[i]);
+        }
+
+        public void Call(string[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+                if (ulong.TryParse(numbers[i], out ulong useless))
+                    Program.WriteLine("Calling... " + numbers[i]);
+                else
+                    Program.WriteLine("Invalid number!");
         }
     }
 }
