7d505f605aaba7dcd8269b722eb2f9de5752f541
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-07 13:58:58 +0300

    07.09.2018

---
+++ 5. Object Oriented Programming/2018.09.07/616/Lake.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _616
+{
+    public class Lake : IEnumerable<int>
+    {
+        private List<int> rocks;
+
+        public Lake(params int[] rocks)
+        {
+            this.rocks = new List<int>(rocks);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            if (this.rocks.Count % 2 == 0)
+            {
+                // odd
+                for (int i = 0; i < this.rocks.Count; i += 2)
+                    yield return this.rocks[i];
+
+                // even
+                for (int i = this.rocks.Count - 1; i >= 0; i -= 2)
+                    yield return this.rocks[i];
+            }
+            else
+            {
+                // odd
+                for (int i = 0; i <= this.rocks.Count - 1; i += 2)
+                    yield return this.rocks[i];
+
+                // even
+                for (int i = this.rocks.Count - 2; i >= 0; i -= 2)
+                    yield return this.rocks[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.07/616/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _616
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            var rocks = Console.ReadLine()
+                        .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse).ToArray();
+            // The Lake
+            Lake lake = new Lake(rocks);
+
+            // Output
+            foreach (var item in lake)
+                Console.Write($"{item} ");
+
+        }
+    }
+}
