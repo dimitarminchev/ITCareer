3f53780e91db55e5b4896c186962148df2be374c
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-07 10:53:22 +0300

    07.09.2018

---
+++ 5. Object Oriented Programming/2018.09.07/611/Library.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _611
+{
+    public class Library
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+    }
+
+}
