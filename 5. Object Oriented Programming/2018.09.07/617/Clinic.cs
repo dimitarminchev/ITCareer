e01514caee21d48422ed492b836796dce52e36df
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-07 15:54:36 +0300

    07.09.2018

---
+++ 5. Object Oriented Programming/2018.09.07/617/Clinic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _617
+{
+    public class Clinic : Pet
+    {
+        private string name;
+        private int[] rooms;
+
+        private List<Pet> pets;
+
+        public void Create(string name, int rooms)
+        {
+            this.name = name;
+            this.rooms = new int[rooms];
+        }
+
+        public bool Add(Pet pet)
+        {
+            if (this.pets.Contains(pet)) return false;
+            this.pets.Add(pet);
+            return true;
+        }
+
+        public bool Release()
+        {
+            return true;
+        }
+        // public bool HasEmptyRooms ()
+        // public void Print()
+        // public void Print(int room)
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.07/617/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _617
+{
+    class Program
+    {
+        // TODO
+        static void Main(string[] args)
+        {
+            List<Clinic> clinic = new List<Clinic>();
+        }
+    }
+}
