dcc04b0d126313aafa3b029d5a8569d08a4d61cd
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-07 11:04:21 +0300

    07.09.2018

---
+++ 5. Object Oriented Programming/2018.09.07/612/Library.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _612
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+    public class LibraryIterator : IEnumerator<Book>
+    {
+        private readonly List<Book> books;
+        private int currentIndex;
+        public LibraryIterator(IEnumerable<Book> books)
+        {
+            this.Reset();
+            this.books = new List<Book>(books);
+        }
+        public void Dispose() { }
+        public bool MoveNext()  => ++this.currentIndex < this.books.Count;
+        public void Reset() => this.currentIndex = -1;
+        public Book Current => this.books[this.currentIndex];
+        object IEnumerator.Current => this.Current;
+    }
+
+
+}
---
+++ 5. Object Oriented Programming/2018.09.07/612/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _612
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+            Book bookTwo = new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace");
+            Book bookThree = new Book("The Foundation", 1951);
+
+            Library library = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (var book in library)
+            {
+                Console.WriteLine(book.Title);
+            }
+
+        }
+    }
+}
