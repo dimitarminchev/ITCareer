f72004943eb93c6ecea8e9d2a7f564a47ab99ee9
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-07 12:14:58 +0300

    07.09.2018

---
+++ 5. Object Oriented Programming/2018.09.07/614/ListyIterator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _614
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private int index = 0;
+        private List<T> listy;
+
+        public ListyIterator(params T[] listy)
+        {
+            this.Create(listy);
+        }
+
+        public void Create(params T[] listy)
+        {
+            this.listy = new List<T>(listy);
+        }
+
+        public bool Move()
+        {
+            if (!HasNext()) return false;
+            else
+            {
+                this.index = this.index + 1;
+                return true;
+            }
+        }
+
+        public string Print()
+        {
+            if (this.listy.Count == 0)
+            {
+                throw new Exception("Invalid Operation");
+            }
+            return this.listy[this.index].ToString();
+        }
+
+        public bool HasNext()
+        {
+            if (this.index + 1 > this.listy.Count - 1) return false;
+            else  return true;
+        }
+
+        // New
+        public string PrintAll()
+        {
+            string str = null;
+            foreach (var item in this.listy)
+            str = str + item.ToString() + " ";
+            return str;
+        }
+
+        // IEnumerable<T>
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.listy.Count; i++)
+            yield return this.listy[i];
+
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.07/614/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _614
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> list = new ListyIterator<string>();
+            var line = Console.ReadLine().Split().ToArray();
+            while (line[0] != "END")
+            {
+                switch (line[0])
+                {
+                    case "Create":
+                        list.Create(line.Skip(1).ToArray());
+                        break;
+                    case "HasNext":
+                        Helper.WriteLine(list.HasNext());
+                        break;
+                    case "Move":
+                        Helper.WriteLine(list.Move());
+                        break;
+                    case "Print": 
+                        Helper.WriteLine(list.Print());
+                        break;
+                    case "PrintAll":
+                        Helper.WriteLine(list.PrintAll());
+                        break;
+                }
+                line = Console.ReadLine().Split().ToArray();
+            }
+        }
+    }
+}
