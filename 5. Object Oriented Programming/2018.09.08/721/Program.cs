17502e55260d77c2001a143c11742c422e437fed
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-08 15:09:46 +0300

    09.09.2018

---
+++ 5. Object Oriented Programming/2018.09.08/721/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _721
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            numbers = numbers.Where(n => n % 2 == 0).OrderBy(n => n).ToArray();
+            string result = string.Join(", ", numbers);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.08/722/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _722
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => int.Parse(n))
+                .ToArray();
+
+            int count = numbers.Count();
+            int sum = numbers.Sum();
+
+            Console.WriteLine($"{count}\n{sum}");
+        }
+    }
+}
