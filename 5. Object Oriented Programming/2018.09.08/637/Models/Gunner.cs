e49ade898ecd60ca37985901e9af464e01482052
Bozhidar Andonov <cakas@abv.bg>
2018-09-08 11:51:32 +0300

    6.3.7

---
+++ 5. Object Oriented Programming/2018.09.08/637/Models/Gunner.cs
@@ -0,0 +1,18 @@
+﻿using _03BarracksFactory.Models.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03BarracksFactory.Models.Units
+{
+    class Gunner:Unit
+    {
+        private const int DefaultHealth = 20;
+        private const int Attack = 20;
+        public Gunner() : base(DefaultHealth, Attack)
+        {
+        }
+    }
+}
