9e28e60d1f0d96ec127abd90e53fd4bdc2c5faf0
Bozhidar Andonov <cakas@abv.bg>
2018-09-08 12:14:17 +0300

    6.3.8

---
+++ 5. Object Oriented Programming/2018.09.08/638/AppEntryPoint.cs
@@ -0,0 +1,18 @@
+﻿namespace _638
+{
+
+    using _03BarracksFactory.Contracts;
+    using _03BarracksFactory.Core;
+    using _03BarracksFactory.Core.Factories;
+    using _03BarracksFactory.Data;
+    class AppEntryPoint
+    {
+        static void Main(string[] args)
+        {
+            IRepository repository = new UnitRepository();
+            IUnitFactory unitFactory = new UnitFactory();
+            IRunnable engine = new Engine(repository, unitFactory);
+            engine.Run();
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.08/638/Core/Commands/Command.cs
@@ -0,0 +1,44 @@
+﻿using _03BarracksFactory.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._3._7.Core.Commands
+{
+    abstract class Command : IExecutable
+    {
+        private string[] data;
+
+        protected string[] Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        private IRepository repository;
+
+        protected IRepository Repository
+        {
+            get { return repository; }
+            set { repository = value; }
+        }
+
+        private IUnitFactory unitFactory;
+
+        protected IUnitFactory UnitFactory
+        {
+            get { return unitFactory; }
+            set { unitFactory = value; }
+        }
+
+        public Command(string[] data, IRepository repository, IUnitFactory unitFactory)
+        {
+            this.Data = data;
+            this.Repository = repository;
+            this.UnitFactory = unitFactory;
+        }
+        public abstract string Execute();
+    }
+}
