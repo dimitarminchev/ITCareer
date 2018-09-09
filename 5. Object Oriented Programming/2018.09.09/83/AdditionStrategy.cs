bdd78a03bb3ce92d32c09a099aa5d286a5da1584
Bozhidar Andonov <cakas@abv.bg>
2018-09-09 14:36:49 +0300

    83

---
+++ 5. Object Oriented Programming/2018.09.09/83/AdditionStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace _83
+{
+	public class AdditionStrategy : IStrategy
+    {
+        public int Calculate(int firstOperand, int secondOperand)
+        {
+            return firstOperand + secondOperand;
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.09/83/SubtractionStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace _83
+{
+	public class SubtractionStrategy : IStrategy
+    {
+        public int Calculate(int firstOperand, int secondOperand)
+        {
+            return firstOperand - secondOperand;
+        }
+    }
+}
