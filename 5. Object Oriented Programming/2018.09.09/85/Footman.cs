bffe670708516b9243ac4d81912efdcd5e8fe34b
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-09 15:00:56 +0300

    09.09.2018

---
+++ 5. Object Oriented Programming/2018.09.09/85/Footman.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _85
+{
+    public class Footman 
+    {
+        // life
+        private int life = 2;
+        public int Life
+        {
+            get { return this.life; }
+        }
+
+        // property
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        // constructor
+        public Footman(string name)
+        {
+            this.Name = name;
+        }
+
+        // event
+        public void KingIsUnderAttack(object sender, EventArgs e)
+        {
+            this.life--;
+            Helper.WriteLine($"Footman {this.Name} is panicking!");
+        }
+    }
+}
