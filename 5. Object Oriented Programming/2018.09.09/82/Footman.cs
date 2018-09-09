251a02b3c29ac019117a1cd2dadd5de6d895e51b
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-09 14:32:43 +0300

    09.09.2018

---
+++ 5. Object Oriented Programming/2018.09.09/82/Footman.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _82
+{
+    public class Footman 
+    {
+        // property
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        // constructor
+        public Footman(string name)
+        {
+            this.Name = name;
+        }
+
+        // event
+        public void KingIsUnderAttack(object sender, EventArgs e)
+        {
+            Helper.WriteLine($"Footman {this.Name} is panicking!");
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.09/82/RoyalGuard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _82
+{
+    public class RoyalGuard 
+    {
+        // property
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        // constructor
+        public RoyalGuard(string name)
+        {
+            this.Name = name;
+        }
+
+        // event
+        public void KingIsUnderAttack(object sender, EventArgs e)
+        {
+            Helper.WriteLine($"Royal Guard {this.Name} is defending!");
+        }
+    }
+}
