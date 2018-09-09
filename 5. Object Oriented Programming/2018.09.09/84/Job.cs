db936d9b92dfec4a1dd2e1f0acfe2d1a70f84644
Bozhidar Andonov <cakas@abv.bg>
2018-09-09 15:44:53 +0300

    84

---
+++ 5. Object Oriented Programming/2018.09.09/84/Job.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _84
+{
+    public class Job
+    {
+
+        public event JobDoneEventHandler JobIsDone;
+
+        public string Name { get; private set; }
+        public int HoursRequired { get; private set; }
+        public BaseEmployee Employee { get; private set; }
+        public Job(string name, int hoursRequired, BaseEmployee employee)
+        {
+            this.Name = name;
+            this.HoursRequired = hoursRequired;
+            this.Employee = employee;
+        }
+
+        public void Update()
+        {
+            this.HoursRequired -= Employee.WorkingHours;
+            if (this.HoursRequired <= 0)
+                if(JobIsDone!=null)
+                    JobIsDone(this, new JobEventArgs(this));
+        }
+
+        public void OnJobDone(object sender, JobEventArgs e)
+        {
+            Helper.WriteLine($"Job {this.Name} done!");
+        }
+        public override string ToString()
+        {
+            return $"Job: {this.Name} Hours Remaining: {HoursRequired}";
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.09/84/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _84
+{
+    public delegate void JobDoneEventHandler(object sender, JobEventArgs e);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<BaseEmployee> employees = new List<BaseEmployee>();
+            JobList jobs = new JobList();
+            
+            string[] command = Console.ReadLine().Split().ToArray();
+            while(command[0]!="End")
+            {
+                switch (command[0])
+                {
+                    case "StandartEmployee":
+                        employees.Add(new StandartEmpoyee(command[1]));
+                        break;
+                    case "PartTimeEmployee":
+                        employees.Add(new PartTimeEmployee(command[1]));
+                        break;
+                    case "Job":
+                        Job realJob = new Job(command[1], int.Parse(command[2]), employees.Where(x => x.Name == command[3]).First());
+                        realJob.JobIsDone += realJob.OnJobDone;
+                        realJob.JobIsDone += jobs.OnJobDone; 
+                        jobs.Add(realJob);
+                        break;
+                    case "Status":
+                        foreach (var job in jobs) Helper.WriteLine(job);
+                        break;
+                    case "Pass":
+                        List<Job> jobsToUpdate = new List<Job>(jobs);
+                        foreach (var job in jobsToUpdate)
+                        {
+                            job.Update();
+                        }
+                        break;
+                    default:
+                        Helper.WriteLine("Invalid command!");
+                        break;
+                }
+
+                command = Console.ReadLine().Split().ToArray();
+            }
+
+
+        }
+    }
+}
