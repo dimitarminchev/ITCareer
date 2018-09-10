7ed465afb630d7025b091144105ac311c054c4a0
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-10 14:51:19 +0300

    10.09.2018

--- 5. Object Oriented Programming/2018.09.10/1012/Program.cs
+++ 5. Object Oriented Programming/2018.09.10/1012/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace _1012
     {
         static void Main(string[] args)
         {
+            StreamReader reader = new StreamReader("somefile.txt");
+            StreamWriter writer = new StreamWriter("oderfile.txt");
+            using (reader)
+            {
+                using (writer)
+                {
+                    int lineNumber = 1;
+                    string line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        writer.Write("Line" + ":" + line);
+                        writer.WriteLine();
+                        lineNumber++;
+                        line = reader.ReadLine();
+
+                    }
+
+                }
+
+            }
         }
     }
 }
