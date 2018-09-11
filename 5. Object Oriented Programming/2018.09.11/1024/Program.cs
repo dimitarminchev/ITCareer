887a35b78d0100d9e987844aa7f02d3b001a8ee8
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-11 12:13:46 +0300

    11.09.2018

--- 5. Object Oriented Programming/2018.09.11/1024/Program.cs
+++ 5. Object Oriented Programming/2018.09.11/1024/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,17 @@ namespace _1024
 {
     class Program
     {
+        // TODO: Fix The Output
         static void Main(string[] args)
         {
+            var dir = Environment.CurrentDirectory;
+            DirectoryInfo d = new DirectoryInfo($"{dir}\\..\\..\\..\\");
+            FileInfo[] Files = d.GetFiles("*.*"); 
+            foreach (FileInfo file in Files)
+            {
+                Console.WriteLine("Name: {0}, Size: {1} KB, Ext: {2}",
+                                  file.Name, file.Length / 1024, file.Extension);
+            }
         }
     }
 }
