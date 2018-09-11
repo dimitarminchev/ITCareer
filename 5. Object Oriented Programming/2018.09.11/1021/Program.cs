bcfe201a66da6967f4b90454e4bbeff6851c9511
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-11 10:31:36 +0300

    11.09.2018

---
+++ 5. Object Oriented Programming/2018.09.11/1021/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1021
+{
+    class Program
+    {
+        // Copy
+        public static bool Copy(string _source, string _destination)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(new FileStream(_source, FileMode.Open)))
+                using (StreamWriter writer = new StreamWriter(new FileStream(_destination, FileMode.Create)))
+                {
+                    writer.Write(reader.ReadToEnd());
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        // Main
+        static void Main(string[] args)
+        {
+            if (Copy("LoremIpsum.txt", "LoremIpsum2.txt"))
+            {
+                Console.WriteLine("File Copy Successful");
+            }
+            else
+            {
+                Console.WriteLine("File Copy Error");
+            }
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2018.09.11/1023/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
