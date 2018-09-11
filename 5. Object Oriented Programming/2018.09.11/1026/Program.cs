b3b3ee4d8fc6e0e119865d65872a6db50bc156c2
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-09-11 14:14:26 +0300

    11.09.2018

--- 5. Object Oriented Programming/2018.09.11/1026/Program.cs
+++ 5. Object Oriented Programming/2018.09.11/1026/Program.cs
@@ -2,13 +2,40 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _1026
 {
     class Program
     {
-        
+        // TODO
+        static void Main(string[] args)
+        {
+            // Transmission Control Protocol Server
+            TcpListener server = new TcpListener(IPAddress.Loopback, 8081);
+            server.Start();
+            while (true)
+            {
+                // Transmission Control Protocol Client
+                using (TcpClient client = server.AcceptTcpClient())
+                {
+                    // If Client Connects ...
+                    Console.WriteLine("Connected!");
+                    NetworkStream stream = client.GetStream();
+
+                    // Send HTML Page to the Client
+                    StreamReader reader = new StreamReader("page.html");
+                    byte[] html = Encoding.Unicode.GetBytes(reader.ReadToEnd());
+                    stream.Write(html, 0, html.Length);
+
+                    // 500 ms wait
+                    Thread.Sleep(500);
+                }
+            }         
+        }
     }
 }
