e6afbf0100949cbe897f743d84a49c4590180d06
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-11-01 18:21:36 +0200

    Updates

--- 6. Databases/2018.11.01/25.sql
+++ 6. Databases/2018.11.01/25.sql
@@ -1,11 +1,16 @@
 /* 25. Основно избиране на няколко полета */
+USE soft_uni;
+
+/* Избор на колони */
 SELECT 
-towns.name, /* tows */
-departments.name, /* departments */
+towns.name as town, /* tows */
+departments.name as department, /* departments */
 first_name, last_name, job_title, salary /* employees */
 
+/* Таблици: градове, департаменти и работници */
 FROM towns,departments,employees,addresses
 
+/* Описване на връзките */
 WHERE  employees.address_id = addresses.address_id AND 
        addresses.town_id = towns.town_id AND
        employees.department_id = departments.department_id
