72b3322607927e98796f3332b2c768c6dee27978
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-11-10 16:07:35 +0200

    Part 1

---
+++ 6. Databases/2018.11.10/509.sql
@@ -0,0 +1,57 @@
+/* 509. Age Groups */
+USE gringotts;
+
+DROP TABLE IF EXISTS table509;
+
+CREATE TABLE IF NOT EXISTS table509
+(
+	age_group varchar(50),
+	wizzard_count int(2)
+);
+
+INSERT INTO table509 
+(
+		SELECT '[0-10]' AS age_group, count(ID) AS wizzard_count
+		FROM wizzard_deposits
+		GROUP BY age HAVING age <= 10
+);
+INSERT INTO table509  
+(
+		SELECT '[11-20]' AS age_group, count(ID) AS wizzard_count
+		FROM wizzard_deposits
+		GROUP BY age HAVING age >= 11 AND age <= 20
+);
+INSERT INTO table509 
+(
+		SELECT '[21-30]' AS age_group, count(ID) AS wizzard_count
+		FROM wizzard_deposits
+		GROUP BY age HAVING age >= 21 AND age <= 30
+);
+INSERT INTO table509 
+(
+		SELECT '[31-40]' AS age_group, count(ID) AS wizzard_count
+		FROM wizzard_deposits
+		GROUP BY age HAVING age >= 31 AND age <= 40
+);
+INSERT INTO table509 
+(
+		SELECT '[41-50]' AS age_group, count(ID) AS wizzard_count
+		FROM wizzard_deposits
+		GROUP BY age HAVING age >= 41 AND age <= 50
+);
+INSERT INTO table509 
+(
+		SELECT '[51-60]' AS age_group, count(ID) AS wizzard_count
+		FROM wizzard_deposits
+		GROUP BY age HAVING age >= 51 AND age <= 60
+);
+INSERT INTO table509 
+(
+		SELECT '[61+]' AS age_group, count(ID) AS wizzard_count
+		FROM wizzard_deposits
+		GROUP BY age HAVING age >= 61
+);
+
+SELECT age_group, SUM(wizzard_count) 
+FROM table509
+GROUP BY age_group;
---
+++ 6. Databases/2018.11.10/510.sql
@@ -0,0 +1,8 @@
+/* 510. First Letter */
+USE gringotts;
+
+SELECT SUBSTR(first_name,1,1) AS first_letter
+FROM wizzard_deposits
+WHERE deposit_group = "Troll Chest"
+GROUP BY first_letter
+ORDER BY first_letter;
