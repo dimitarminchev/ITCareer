c27c0f63f6e35f9d3de2b2097c63df797e7b6437
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-11-17 15:33:25 +0200

    Update

---
+++ 6. Databases/2018.11.17/06. Extract all military journeys.sql
@@ -0,0 +1,7 @@
+-- 06.	Extract all military journeys 
+-- https://judge.softuni.bg/Contests/Practice/Index/1265#6
+
+SELECT id, journey_start, journey_end
+FROM journeys
+WHERE purpose = 'Military'
+ORDER BY journey_start
