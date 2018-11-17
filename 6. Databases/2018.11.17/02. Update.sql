ecb875744081044f3387910bf528773198eea4c7
Dimitar Minchev <dimitar.minchev@gmail.com>
2018-11-17 15:14:53 +0200

    Update

---
+++ 6. Databases/2018.11.17/02. Update.sql
@@ -0,0 +1,16 @@
+-- 02. Update
+-- https://judge.softuni.bg/Contests/Practice/Index/1265#2
+
+UPDATE journeys
+   SET purpose = (
+		CASE
+			WHEN id % 2 = 0
+			THEN 'Medical'
+            WHEN id % 3 = 0
+            THEN 'Technical'
+            WHEN id % 5 = 0
+            THEN 'Educational'
+            WHEN id % 7 = 0
+            THEN 'Military'
+            ELSE purpose
+		END)
