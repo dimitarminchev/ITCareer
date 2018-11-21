2b7613aa042e9e2d90c010437c2ed400bd4a6d8b
destbg <35627380+destbg@users.noreply.github.com>
2018-11-21 19:08:10 +0200

    Update

--- 6. Databases/2018.11.18/107. Хранилища и потребители.sql
+++ 6. Databases/2018.11.18/107. Хранилища и потребители.sql
@@ -1,6 +1,10 @@
 -- 07. Хранилища и потребители
-SELECT r.id, r.name,COUNT(c.contributor_id) AS users
-FROM repositories AS r
-JOIN commits AS c ON r.id  = c.repository_id
-GROUP BY r.name
-ORDER BY 3 DESC, 1 ;
+SELECT r.id, name, COUNT(c.contributor_id) AS users
+FROM repositories r
+LEFT JOIN (
+	SELECT repository_id, contributor_id
+    FROM commits
+    GROUP BY repository_id, contributor_id
+) AS c ON c.repository_id = r.id
+GROUP BY name
+ORDER BY users DESC, r.id;
