4e79c792e96cf4913b838fd111c5aaebf1481f2f
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-02 17:14:13 +0200

    ConsoleMVC

---
+++ 7. Software Development/2019.02.02/ConsoleMVC/Model/Tip.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMVC.Model
+{
+    class Tip
+    {
+        private double amount;
+
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        private double percent;
+
+        public double Percent
+        {
+            get { return percent; }
+            set
+            {
+                if (value > 1)
+                {
+                    percent = value / 100.0;
+                }
+                else percent = value;
+            }
+        }
+
+        public Tip(double amount, double percent)
+        {
+            Amount = amount;
+            Percent = percent;
+        }
+
+        public Tip() : this(0, 0) { }
+
+        public double CalculateTip()
+        {
+            return Amount * Percent;
+        }
+
+        public double CalculateTotal()
+        {
+            return CalculateTip() + Amount;
+        }
+
+    }
+}
--- 7. Software Development/2019.02.02/ConsoleMVC/Program.cs
+++ 7. Software Development/2019.02.02/ConsoleMVC/Program.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ConsoleMVC.Controllers;
 namespace ConsoleMVC
 {
     class Program
     {
         static void Main(string[] args)
         {
+            TipCalculatorController tipCalculatorController = new TipCalculatorController();
         }
     }
 }
