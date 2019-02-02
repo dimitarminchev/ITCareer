4ed19bc1b6a1d3cda37efb2162ce386e1025e516
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-02 18:45:37 +0200

    ExamTime

---
+++ 7. Software Development/2019.02.02/OnTimeForExam/Controller/ExamTimeActionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnTimeForExam.Model;
+using OnTimeForExam.Views;
+namespace OnTimeForExam.Controller
+{
+    class ExamTimeActionController
+    {
+        ExamTime examTime;
+        Display display;
+
+        public ExamTimeActionController()
+        {
+            this.display = new Display();
+            this.examTime = new ExamTime
+            (
+                  display.hourOfExam,
+                  display.minuteOfExam, 
+                  display.hourOfArrival,
+                  display.minuteOfArrival
+            );
+
+            display.Result = examTime.GetResult();
+            display.PrintValues();
+        }
+    }
+}
---
+++ 7. Software Development/2019.02.02/OnTimeForExam/Views/Display.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnTimeForExam.Views
+{
+    class Display
+    {
+        public int hourOfExam { get; set; }
+        public int minuteOfExam { get; set; }
+        public int hourOfArrival { get; set; }
+        public int minuteOfArrival { get; set; }
+        public string Result { get; set; }
+
+        public Display()
+        {
+            this.hourOfExam = 0;
+            this.minuteOfExam = 0;
+            this.hourOfArrival = 0;
+            this.minuteOfArrival = 0;
+            this.Result = string.Empty;
+            GetValues();
+        }
+
+        private void GetValues()
+        {
+            hourOfExam = int.Parse(Console.ReadLine());
+            minuteOfExam = int.Parse(Console.ReadLine());
+            hourOfArrival = int.Parse(Console.ReadLine());
+            minuteOfArrival = int.Parse(Console.ReadLine());
+        }
+
+        public void PrintValues()
+        {
+            Console.WriteLine(Result);
+        }
+    }
+}
