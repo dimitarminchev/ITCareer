3b1f7da2035a1a1879cdcc0940ee072e102eefa6
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-03 17:13:19 +0200

    MagicNumbers

--- 7. Software Development/1. MVC/MagicNumbers/Model/MagicNumberGenerator.cs
+++ 7. Software Development/1. MVC/MagicNumbers/Model/MagicNumberGenerator.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MagicNumbers.Model
 {
     class MagicNumberGenerator
--- 7. Software Development/1. MVC/MagicNumbers/Program.cs
+++ 7. Software Development/1. MVC/MagicNumbers/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MagicNumbers.Controller;
+
 namespace MagicNumbers
 {
     class Program
