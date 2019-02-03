e78dd6aad4c924816fa8d2b7bb804660957bbb64
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-03 14:42:09 +0200

    Histogram

---
+++ 7. Software Development/1. MVC/Histogram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Histogram.Controller;
+namespace Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HistogramaActionController histogramaActionController = new HistogramaActionController();
+        }
+    }
+}
