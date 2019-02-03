4042df7f398afa6386ce430e7a28460f6b2efc7f
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-03 18:20:05 +0200

    PasswordGenerator

---
+++ 7. Software Development/1. MVC/PassGen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PassGen.Controller;
+
+namespace PassGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeneratorActionController generator = new GeneratorActionController();
+        }
+    }
+}
---
+++ 7. Software Development/1. MVC/PassGen/View/Display.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassGen.View
+{
+    class Display
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public List<string> Passwords { get; set; }
+
+        public Display()
+        {
+            this.FirstNumber = 0;
+            this.SecondNumber = 0;
+            this.Passwords = null;
+            GetValues();
+        }
+
+        private void GetValues()
+        {
+            this.FirstNumber = int.Parse(Console.ReadLine());
+            this.SecondNumber = int.Parse(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(string.Join(" ", Passwords));
+        }
+    }
+}
