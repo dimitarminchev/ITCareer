81ec86689f39ab15176d4375f38ebd9a758c0acb
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-03 17:11:02 +0200

    MagicNumbers

---
+++ 7. Software Development/1. MVC/MagicNumbers/Model/MagicNumberGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicNumbers.Model
+{
+    class MagicNumberGenerator
+    {
+        private int product;
+
+        public int Product
+        {
+            get { return product; }
+            set { product = value; }
+        }
+
+        public MagicNumberGenerator(int product)
+        {
+            this.Product = product;
+        }
+
+        public List<string> GenerateAllMagicNumbers()
+        {
+            var AllMagicNumbers = new List<string>();
+            for(int a = 1; a<=Math.Min(Product, 9); a++)
+            {
+                for (int b = 1; b <= Math.Min(Product, 9); b++)
+                {
+                    for (int c = 1; c <= Math.Min(Product, 9); c++)
+                    {
+                        for (int d = 1; d <= Math.Min(Product, 9); d++)
+                        {
+                            for (int e = 1; e <= Math.Min(Product, 9); e++)
+                            {
+                                for (int f = 1; f <= Math.Min(Product, 9); f++)
+                                {
+                                    if(a*b*c*d*e*f == this.Product)
+                                    {
+                                        AllMagicNumbers.Add($"{a}{b}{c}{d}{e}{f}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return AllMagicNumbers;
+        }
+    }
+}
---
+++ 7. Software Development/1. MVC/MagicNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MagicNumbers.Controller;
+namespace MagicNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var magicNumberGeneratorActionController =
+                new MagicNumberGeneratorActionController();
+        }
+    }
+}
