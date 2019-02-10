9f89432803f4c5ba656aaf11987c1c0fe1a8bd2b
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-10 14:13:41 +0200

    Debugging

---
+++ 7. Software Development/3. Debugging/6. Substring/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Substring
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+            int jump = int.Parse(Console.ReadLine());
+
+            const char Search = 'р';
+            bool hasMatch = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == Search)
+                {
+                    hasMatch = true;
+
+                    int endIndex = jump;
+
+                    if (endIndex > text.Length)
+                    {
+                        endIndex = text.Length;
+                    }
+
+                    string matchedString = text.Substring(i, endIndex);
+                    Console.WriteLine(matchedString);
+                    i += jump;
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
---
+++ 7. Software Development/3. Debugging/7. BitCarousel/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.BitCarousel
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte number = byte.Parse(Console.ReadLine());
+            byte rotations = byte.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotations; i++)
+            {
+                string direction = Console.ReadLine();
+
+                if (direction == "right")
+                {
+                    int rightMostBit = number & 1;
+                    number >>= 1;
+                    number |= (byte)(rightMostBit << 6);
+                }
+                else if (direction == "left")
+                {
+                    int leftMostBit = (number >> 6) & 1;
+                    number <<= 1;
+                    number |= (byte)leftMostBit;
+                }
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
