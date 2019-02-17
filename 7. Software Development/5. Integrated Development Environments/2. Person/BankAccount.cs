3689c98881d54ff072a3b36e8782a8568a11a846
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-17 18:53:28 +0200

    IDE and Text Editors

---
+++ 7. Software Development/5. Integrated Development Environments/2. Person/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Person
+{
+    public class BankAccount
+    {
+        private int id;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private double balance;
+
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public BankAccount(int id, double balance)
+        {
+            this.ID = id;
+            this.Balance = balance;
+        }
+
+    }
+}
---
+++ 7. Software Development/5. Integrated Development Environments/2. Person/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using _1
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Person
+{
+    public class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        private List<BankAccount> accounts;
+
+        public List<BankAccount> Accounts
+        {
+            get { return accounts; }
+            set { accounts = value; }
+        }
+
+        public Person(string name, int age, List<BankAccount> accounts)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Accounts = accounts;
+        }
+
+        public double GetBalance()
+        {
+            double balance = 0;
+            foreach(var account in accounts)
+            {
+                balance += account.Balance;
+            }
+            return balance;
+        }
+    }
+}
