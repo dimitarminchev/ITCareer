ff3aed8f469bfb7f6eacbf6b7d1b5fe5fd891e4a
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-17 15:28:46 +0200

    17.02.2019

---
+++ 7. Software Development/4. Refactoring/06. Abstraction/Figure.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Abstraction
+{
+    abstract class IFigure 
+    {
+
+        /// <summary>
+        /// Лице на фигура
+        /// </summary>
+        public abstract double Surface();
+
+        /// <summary>
+        /// Периметър на фигура
+        /// </summary>
+        public abstract double Perimeter();
+
+        /// <summary>
+        /// Печатаща функция
+        /// </summary>
+        public abstract void Print();
+
+    }
+}
