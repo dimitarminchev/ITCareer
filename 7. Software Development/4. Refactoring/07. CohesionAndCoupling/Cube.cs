3c62f0a2b9bad1c7afe06ee17f50bf481abdd40f
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-17 15:46:03 +0200

    17.02.2019

---
+++ 7. Software Development/4. Refactoring/07. CohesionAndCoupling/Cube.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure
+{
+    static class Cube
+    {
+
+        public static double Width { get; set; }
+        public static double Height { get; set; }
+        public static double Depth { get; set; }
+
+        public static double Distance(double x1, double y1, double x2, double y2)
+        {
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            return distance;
+        }
+
+        public static double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+        {
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+            return distance;
+        }
+
+        public static double CalcVolume()
+        {
+            double volume = Width * Height * Depth;
+            return volume;
+        }
+
+        public static double CalcDiagonalXYZ()
+        {
+            double distance = Distance(0, 0, 0, Width, Height, Depth);
+            return distance;
+        }
+
+        public static double CalcDiagonalXY()
+        {
+            double distance = Distance(0, 0, Width, Height);
+            return distance;
+        }
+
+        public static double CalcDiagonalXZ()
+        {
+            double distance = Distance(0, 0, Width, Depth);
+            return distance;
+        }
+
+        public static double CalcDiagonalYZ()
+        {
+            double distance = Distance(0, 0, Height, Depth);
+            return distance;
+        }
+    }
+}
---
+++ 7. Software Development/4. Refactoring/07. CohesionAndCoupling/File.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files
+{
+    static class File
+    {
+        public static string GetFileExtension(string fileName)
+        {
+            int indexOfLastDot = fileName.LastIndexOf(".");
+            if (indexOfLastDot == -1)
+            {
+                return "";
+            }
+
+            string extension = fileName.Substring(indexOfLastDot + 1);
+            return extension;
+        }
+
+        public static string GetFileNameWithoutExtension(string fileName)
+        {
+            int indexOfLastDot = fileName.LastIndexOf(".");
+            if (indexOfLastDot == -1)
+            {
+                return fileName;
+            }
+
+            string extension = fileName.Substring(0, indexOfLastDot);
+            return extension;
+        }
+    }
+}
