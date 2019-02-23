b16799c57dc79a58a230cdda675dfbcf7049c6e0
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-23 15:41:13 +0200

    Update

---
+++ 7. Software Development/6. NuGet/01. JsonApp/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.JsonApp
+{
+    public class Product
+    {
+        public int Id;
+
+        public string Name;
+
+        public decimal Price;
+
+        public int Stock;
+
+        public DateTime Expiry;
+    }
+}
--- 7. Software Development/6. NuGet/01. JsonApp/Program.cs
+++ 7. Software Development/6. NuGet/01. JsonApp/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,51 @@ namespace _01.JsonApp
     {
         static void Main(string[] args)
         {
+            // Part 1. Single Object
+            var product = new Product()
+            {
+                Id = 1,
+                Name = "Apple",
+                Price = 1.2M,
+                Stock = 10,
+                Expiry = new DateTime(2019,12,31)
+            };
+
+            // Serialize: OOP > JSON
+            string jsonObject = JsonConvert.SerializeObject(product);
+            Console.WriteLine(jsonObject);
+
+
+            // Part 2. List of Objects
+            List<Product> products = new List<Product>();
+            products.Add(new Product()
+            {
+                Id = 1,
+                Name = "Apple",
+                Price = 1.2M,
+                Stock = 10,
+                Expiry = new DateTime(2019, 12, 31)
+            });
+            products.Add(new Product()
+            {
+                Id = 2,
+                Name = "Tomato",
+                Price = 2.1M,
+                Stock = 23,
+                Expiry = new DateTime(2079, 11, 01)
+            });
+            products.Add(new Product()
+            {
+                Id = 3,
+                Name = "Lemon",
+                Price = 5.78M,
+                Stock = 173962,
+                Expiry = new DateTime(1991, 10, 12)
+            });
+
+            string jsonList = JsonConvert.SerializeObject(products);
+            Console.WriteLine(jsonList);
+
         }
     }
 }
