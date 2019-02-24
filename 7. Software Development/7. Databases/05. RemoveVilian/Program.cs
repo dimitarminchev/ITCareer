9fd0891160b81cb2982e58ac718076846f474e09
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-02-24 14:51:52 +0200

    Update

---
+++ 7. Software Development/7. Databases/05. RemoveVilian/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace _05.RemoveVilian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int villainId = int.Parse(Console.ReadLine());
+
+            string connectionString = "server=localhost;database=minions;port=3306;user=root;password=root";
+            MySqlConnection dbCon = new MySqlConnection(connectionString);
+            dbCon.Open();
+
+            using (dbCon)
+            {
+                MySqlCommand command = new MySqlCommand("SELECT Id FROM Villains WHERE Id = @Id", dbCon);
+                command.Parameters.AddWithValue("@Id", villainId);
+                int? result = (int?)command.ExecuteScalar();
+
+                if (result == null)
+                {
+                    Console.WriteLine("No such villain was found.");
+                    dbCon.Close();
+                    return;
+                }
+
+                command = new MySqlCommand("SELECT COUNT(*) FROM MinionsVillains WHERE VillainId = @Id", dbCon);
+                command.Parameters.AddWithValue("@Id", villainId);
+                int minionsCount = int.Parse(command.ExecuteScalar().ToString());
+
+                command = new MySqlCommand("DELETE FROM MinionsVillains WHERE VillainId = @Id", dbCon);
+                command.Parameters.AddWithValue("@Id", villainId);
+                command.ExecuteNonQuery();
+
+                command = new MySqlCommand("SELECT Name FROM Villains WHERE Id = @Id", dbCon);
+                command.Parameters.AddWithValue("@Id", villainId);
+                string villainName = (string)command.ExecuteScalar();
+
+                command = new MySqlCommand("DELETE FROM Villains WHERE Id = @Id", dbCon);
+                command.Parameters.AddWithValue("@Id", villainId);
+                command.ExecuteNonQuery();
+
+                Console.WriteLine($"{villainName} was deleted.");
+                Console.WriteLine($"{minionsCount} minions were released.");
+            }
+        }
+    }
+}
---
+++ 7. Software Development/7. Databases/06.AllMinionsNames/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace _06.AllMinionsNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "server=localhost;database=minions;port=3306;user=root;password=root";
+            MySqlConnection dbCon = new MySqlConnection(connectionString);
+            dbCon.Open();
+
+            List<string> minionsInitial = new List<string>();
+            List<string> minionsArranged = new List<string>();
+
+            using (dbCon)
+            {
+                MySqlCommand command = new MySqlCommand("SELECT Name FROM Minions", dbCon);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                using (reader)
+                {
+                    if (!reader.HasRows)
+                    {
+                        reader.Close();
+                        dbCon.Close();
+                        return;
+                    }
+
+                    while (reader.Read())
+                    {
+                        minionsInitial.Add((string)reader["Name"]);
+                    }
+                }
+            }
+
+            while (minionsInitial.Count > 0)
+            {
+                minionsArranged.Add(minionsInitial[0]);
+                minionsInitial.RemoveAt(0);
+
+                if (minionsInitial.Count > 0)
+                {
+                    minionsArranged.Add(minionsInitial[minionsInitial.Count - 1]);
+                    minionsInitial.RemoveAt(minionsInitial.Count - 1);
+                }
+            }
+
+            minionsArranged.ForEach(m => Console.WriteLine(m));
+        }
+    }
+}
