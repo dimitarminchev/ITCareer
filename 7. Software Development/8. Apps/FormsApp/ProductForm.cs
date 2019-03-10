586d0ec64183cea94c162db6bbc3487f67a5701c
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-03-10 13:54:26 +0200

    Update

---
+++ 7. Software Development/8. Apps/FormsApp/ProductForm.cs
@@ -0,0 +1,91 @@
+﻿using Business;
+using Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsApp
+{
+    public partial class ProductForm : Form
+    {
+        // Product
+        private ProductBusiness productBusiness;
+
+        // Constructor
+        public ProductForm()
+        {
+            InitializeComponent();
+            productBusiness = new ProductBusiness();
+            UpdateGrid();
+        }
+
+        // Update Grid
+        private void UpdateGrid()
+        {
+            dataGridView1.DataSource = productBusiness.GetAll();
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        // Insert
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            var product = new Product()
+            {
+                Name = textName.Text,
+                Price = decimal.Parse(textPrice.Text),
+                Stock = int.Parse(textStock.Text)
+            };
+            productBusiness.Add(product);
+            UpdateGrid();
+        }
+
+        // Update
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var item = dataGridView1.SelectedRows[0].Cells;
+                var id = int.Parse(item[0].Value.ToString());
+                Product update = productBusiness.Get(id);
+                textName.Text = update.Name;
+                textPrice.Text = update.Price.ToString();
+                textStock.Text = update.Stock.ToString();
+            }
+        }
+
+        // Save
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var item = dataGridView1.SelectedRows[0].Cells;
+                var id = int.Parse(item[0].Value.ToString());
+                Product save = productBusiness.Get(id);
+                save.Name = textName.Text;
+                save.Price = decimal.Parse(textPrice.Text);
+                save.Stock = int.Parse(textStock.Text);
+                productBusiness.Update(save);
+                UpdateGrid();
+            }
+        }
+
+        // Delete
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var item = dataGridView1.SelectedRows[0].Cells;
+                var id = int.Parse(item[0].Value.ToString());
+                productBusiness.Delete(id);
+                UpdateGrid();
+            }
+        }
+    }
+}
---
+++ 7. Software Development/8. Apps/readme.md
@@ -0,0 +1,29 @@
+# Проекти
+
+MySQL Sample Product Database
+```
+/* Промерна продуктова база данни */
+DROP DATABASE IF EXISTS products;
+CREATE DATABASE IF NOT EXISTS products;
+USE products;
+CREATE TABLE IF NOT EXISTS products (
+
+    `id` INT(10) AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `price` DECIMAL(4,2) NOT NULL,
+    `stock` INT(5) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO products (`name`,`price`,`stock`) VALUES 
+("Beer",2.20,100),
+("Caca",3.10,100),
+("Fries Potatoes",2.80,100),
+("Coca Cola",2.60,50),
+("Mineral Water",1.20,50);
+```
+NuGet Package Manager Console
+```
+Install-Package Microsoft.EntityFrameworkCore.Tools
+Install-Package MySql.Data.EntityFrameworkCore
+Scaffold-DbContext -Connection "server=localhost;port=3306;user=root;password=root;database=products" -Provider MySql.Data.EntityFrameworkCore -OutputDir Models
+```
