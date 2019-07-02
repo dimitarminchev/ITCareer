25f4a2f5c04b404124726cedeb541af1d4a83a0d
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-07-02 13:57:15 +0300

    02.07.2019

--- 4. Introduction to Algorithms and Data Structures/2019/2019.07.02/3214/Program.cs
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.02/3214/Program.cs
@@ -8,6 +8,13 @@ namespace _3214
 {
     class Program
     {
+/*      Problem 14. Мажорант на масив
+        ---
+        Мажорант на масив от N елемента е стойност, която се среща поне N/2+1 пъти. Напишете програма, която по даден масив от числа намира мажоранта на масива и го отпечатва. Ако мажоранта не съществува – отпечатва "The majorant does not exists!”.
+        Вход	                              Изход
+        2, 2, 3, 3, 2, 3, 4, 3, 3	          3
+        2, 3, 4, 5, 6, 7, 8, 3	              The majorant does not exists!
+*/
         static void Main(string[] args)
         {
             List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
