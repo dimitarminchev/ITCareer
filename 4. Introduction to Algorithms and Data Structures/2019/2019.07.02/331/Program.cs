079640050b22830b6727bc67021fe3331a7fd8a1
Tsvetilin <cvetilov6@abv.b>
2019-07-02 15:02:33 +0300

    Update

---
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.02/331/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _331
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(int.Parse(Console.ReadLine()));
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
