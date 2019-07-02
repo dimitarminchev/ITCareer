95e1e84f7edab4c9c964473a183f51f1c0ac7b42
Tsvetilin <cvetilov6@abv.b>
2019-07-02 13:49:19 +0300

    Update

---
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.02/3214/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3214
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int res = list.Where(x => list.Count(c => c == x) >= list.Count() / 2 + 1).ToList().Distinct().FirstOrDefault();
+            if (res != 0) Console.WriteLine(res);
+            else Console.WriteLine("The majorant does not exists!”.");
+        }
+    }
+}
