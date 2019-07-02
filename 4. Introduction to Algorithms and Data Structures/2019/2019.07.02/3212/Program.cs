c2ace275c1042ce2a85c883c27c05f6f3e775c7a
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-07-02 13:32:00 +0300

    02.07.2019

---
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.02/3212/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3212
+{
+    class Program
+    {
+        // Hard Drive Folders
+        private static Queue<string> folders = new Queue<string>();
+
+        // Process Folder
+        private static void ProcessFolder(string path)
+        {
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (var dir in dirs)
+            {
+                folders.Enqueue(dir);
+            }
+        }
+
+        public static void Main()
+        {
+            // root
+            ProcessFolder(@"C:\");            
+    
+            // level 2
+            while(folders.Count > 0)
+            {
+                try
+                {
+                    var folder = folders.Dequeue();
+                    Console.WriteLine(folder);
+                    ProcessFolder(folder);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("ERROR: {0}", ex.Message);
+                }
+            }
+        }
+    }
+}
