2c77f98919c8cf231c49fa7ca3cb35e5fe377baf
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-07-03 14:19:29 +0300

    03.07.2019

--- 4. Introduction to Algorithms and Data Structures/2019/2019.07.03/Tasks/434/Program.cs
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.03/Tasks/434/Program.cs
@@ -11,6 +11,16 @@ namespace _434
         // Автор: Димитър Митев
         static void Main(string[] args)
         {
+            int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (a[i] > a[j] && i < j) count++;
+                }
+            }
+            Console.WriteLine(count);
         }
     }
 }
