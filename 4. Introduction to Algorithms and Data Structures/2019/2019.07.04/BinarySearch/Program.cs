a553336f6b4dd315297cabe21066e6f83f75e495
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-07-04 10:12:36 +0300

    04.07.2019

---
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.04/BinarySearch/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data Structures
+            var numbers = new int[] { -5, 5, -4, 4, -3, 3, -2, 2, -1, 1 };
+            var alphas = new char[] { 'm', 'a', 'g', 'g', 'i', 'c', 'h', 'a', 'p', 'p', 'e', 'n', 'd', 's', 'h', 'e', 'r', 'e' };
+
+            // Pre-Processing ... (Sort)
+            numbers = numbers.OrderBy(item => item).ToArray();
+            alphas = alphas.OrderBy(item => item).ToArray();
+
+            // Search for Number
+            Console.WriteLine("Binary Search for Number [-3] ...");
+            if (Search.Binary(numbers, -3) == -1)
+            {
+                Console.WriteLine("Not Found");
+            }
+            else
+            {
+                Console.WriteLine("Found");
+            }
+
+            // Search for Char
+            Console.WriteLine("Binary Search for Char ['z'] ...");
+            if (Search.Binary(alphas, 'z') == -1)
+            {
+                Console.WriteLine("Not Found");
+            }
+            else
+            {
+                Console.WriteLine("Found");
+            }
+        }
+    }
+}
---
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.04/BinarySearch/Search.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class Search
+    {
+        // BinarySearch = O(log(n))
+        public static int Binary<T>(T[] elements, T key) where T : IComparable
+        {
+            int start = 0, end = elements.Count() - 1;
+            while (end >= start)
+            {
+                int mid = (start + end) / 2; // middle
+
+                // Comparing
+                if (elements[mid].CompareTo(key) < 0)
+                {
+                    start = mid + 1; // mid < key
+                }
+                else if (elements[mid].CompareTo(key) > 0)
+                {
+                    end = mid - 1; // mid > key
+                }
+                else
+                {
+                    return mid; // found
+                }
+            }
+            return -1; // not found
+        }
+    }
+}
