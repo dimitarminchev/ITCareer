7861b6d51b79e3699089c6322bf95fb4238151e1
Tsvetilin Tsvetilov <47277865+Tsvetilin@users.noreply.github.com>
2019-07-05 23:30:43 +0300

    Update

--- 4. Introduction to Algorithms and Data Structures/2019/2019.07.05/616/Program.cs
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.05/616/Program.cs
@@ -10,6 +10,15 @@ namespace _616
     {
         //Автор: Цветилин Цветилов
         // 616 = Arrays in Matrix
+        
+/*
+4
+9
+*--*---*-
+*--*---*-
+*--*---*-
+*---*-*--
+*/
         public static int row = int.Parse(Console.ReadLine());
         public static int col = int.Parse(Console.ReadLine());
         public static string[] table = new string[row];
@@ -36,52 +45,8 @@ namespace _616
                     }
                 }
             }
-
         }
-        public static void Check()
-        {
-            for (int ind = 0; ind < regions.Count; ind++)
-            {
-                int startX = regions[ind].X;
-                int startY = regions[ind].Y;
-                int area = 0;
-                //bool first = true;
-                for (; true; startY++)
-                {
 
-                    // TODO: Проверка за стени с разминаване на елемените >=1
-                    //     *----*         **----*----*
-                    //     *--*-*         *-*--*-----*
-                    //     *-**-*         *-----*----*
-                    //     ******         ************
-                    //      A=9           A1=12   A2=13
-
-                    while (startX > 0 && startY != 0)
-                    {
-                        if (table[startY][startX - 1] == '-') startX--;
-                        //else if(!first && table[startY-1][startX ] == '*'&& table[startY][startX] == '-'){startX--;}
-                        else if (startX - 2 >= 0)
-                        { if ((table[startY][startX - 1] == '*' && table[startY - 1][startX - 1] == '-' && table[startY - 1][startX] == '-' && table[startY - 1][startX - 2] == '-')) startX--; else break; }
-                        else break;
-                    }
-                    //first = false;
-                    for (; true; startX++)
-                    {
-                        if (startX == col) break;
-                        if (table[startY][startX] == '-')
-                        {
-                            var temp = table[startY].ToCharArray();
-                            temp[startX] = '*';
-                            table[startY] = string.Join("",temp);
-                            area++;
-                        }
-                        else break;
-                    }
-                    if (startY + 1 == row) break;
-                }
-                regions[ind].Area = area;
-            }
-        }
         public static void Print()
         {
             foreach (var item in regions)
@@ -93,9 +58,41 @@ namespace _616
         {
             Draw();
             FindStartingPoints();
-            Check();
-            regions = regions.Where(x=>x.Area!=0).OrderByDescending(x => x.Area).ToList();
+            for (int ind = 0; ind < regions.Count; ind++)
+            {
+                int startX = regions[ind].X;
+                int startY = regions[ind].Y;
+                Counter = 0;
+                Check(startX, startY);
+                regions[ind].Area = Counter;
+            }
+            regions = regions.Where(x => x.Area != 0).OrderByDescending(x => x.Area).ToList();
             Print();
+        }
+        public static int Counter = 0;
+        public static void Check(int startX, int startY)
+        {
+            if (IsInBounds(startX, startY))
+                if (table[startY][startX] != '*')
+                {
+                    Mark(startX, startY);
+                    Check(startX, startY + 1);
+                    Check(startX + 1, startY);
+                    Check(startX, startY - 1);
+                    Check(startX - 1, startY);
+                }
+        }
+        public static bool IsInBounds(int x, int y)
+        {
+            if (x < 0 || y >= row || y < 0 || x >= col) return false;
+            return true;
+        }
+        public static void Mark(int x, int y)
+        {
+            var temp = table[y].ToCharArray();
+            temp[x] = '*';
+            table[y] = string.Join("", temp);
+            Counter++;
         }
     }
     public class Region
@@ -109,6 +106,51 @@ namespace _616
             Y = y;
             Area = area;
         }
-
     }
 }
+
+//Нерекурсивен начин, неработещ за криви стени с отклонение > 1
+/*public static void Check()
+{
+    for (int ind = 0; ind < regions.Count; ind++)
+    {
+        int startX = regions[ind].X;
+        int startY = regions[ind].Y;
+        int area = 0;
+        //bool first = true;
+        for (; true; startY++)
+        {
+
+            // TODO: Проверка за стени с разминаване на елемените >=1
+            //     *----*         **----*----*
+            //     *--*-*         *-*--*-----*
+            //     *-**-*         *-----*----*
+            //     ******         ************
+            //      A=9           A1=12   A2=13
+
+            while (startX > 0 && startY != 0)
+            {
+                if (table[startY][startX - 1] == '-') startX--;
+                //else if(!first && table[startY-1][startX ] == '*'&& table[startY][startX] == '-'){startX--;}
+                else if (startX - 2 >= 0)
+                { if ((table[startY][startX - 1] == '*' && table[startY - 1][startX - 1] == '-' && table[startY - 1][startX] == '-' && table[startY - 1][startX - 2] == '-')) startX--; else break; }
+                else break;
+            }
+            //first = false;
+            for (; true; startX++)
+            {
+                if (startX == col) break;
+                if (table[startY][startX] == '-')
+                {
+                    var temp = table[startY].ToCharArray();
+                    temp[startX] = '*';
+                    table[startY] = string.Join("", temp);
+                    area++;
+                }
+                else break;
+            }
+            if (startY + 1 == row) break;
+        }
+        regions[ind].Area = area;
+    }
+}*/
