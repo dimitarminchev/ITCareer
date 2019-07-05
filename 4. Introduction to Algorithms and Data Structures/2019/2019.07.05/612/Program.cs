df4ab5bf88a75cf8797e5429f2a36adfc001ea76
Tony5768 <47269030+Tony5768@users.noreply.github.com>
2019-07-05 13:56:32 +0300

    Update

--- 4. Introduction to Algorithms and Data Structures/2019/2019.07.05/612/Program.cs
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.05/612/Program.cs
@@ -8,9 +8,31 @@ namespace _612
 {
     class Program
     {
+        //Автор: Стоян Златев
         // 612 = Iterations Variables
+         public static void Permutation(int index)
+        {
+            if (index == loops.Length)
+            {
+                foreach (var item in loops)
+                    Console.Write($"{item} ");
+                Console.WriteLine();
+                return;
+            }
+            else
+            {
+                for (int i = 1; i <=loops.Length; i++)
+                {
+                    loops[index] = i;
+                    Permutation(index + 1);
+                }
+            }
+        }
+        public static int n = int.Parse(Console.ReadLine());
+        static int[] loops = new int[n];
         static void Main(string[] args)
         {
+            Permutation(0);
         }
     }
 }
