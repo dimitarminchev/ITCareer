99051a1f59857955f018735f016d64e96e2f25b3
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-07-06 08:56:37 +0300

    06.07.2019

---
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.06/Product/Console.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Product
+{
+    public static class Console
+    {
+        /// <summary>
+        /// Отпечатване на текст на конзолата в зeлен цвят
+        /// </summary>
+        /// <param name="text">Текст за отпечатване</param>
+        public static void WriteLine(string text)
+        {
+            var color = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.WriteLine(text);
+            System.Console.ForegroundColor = color;
+        }
+    }
+}
