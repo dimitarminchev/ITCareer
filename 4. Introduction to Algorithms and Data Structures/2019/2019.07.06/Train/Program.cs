9128da9e4ce051e5136c69bbe586e5e41f136c91
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-07-06 13:40:01 +0300

    06.07.2019

--- 4. Introduction to Algorithms and Data Structures/2019/2019.07.06/Train/Program.cs
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.06/Train/Program.cs
@@ -1,17 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Train
 {
     class Program
     {
-        // Train
+        // Trains
+        private static Deque<Train> trains = new Deque<Train>();
+
+        private static void Add(int number, string name, string type, int cars)
+        {
+            if (type == "F")
+            {
+                //Add freight trains to back
+                trains.AddBack(new Train(number, name, type, cars));
+            }
+            else
+            {
+                trains.AddFront(new Train(number, name, type, cars));
+            }
+        }
+        private static void Travel()
+        {
+            if (trains.Count > 0)
+            {
+                Train frontTrain = trains.GetFront();
+                Train backTrain = trains.GetBack();
+                if (backTrain != null && backTrain.Type == "F" && backTrain.Cars > 15)
+                {
+                    Console.WriteLine(trains.RemoveBack());
+                }
+                else if (frontTrain != null && frontTrain.Type == "P")
+                {
+                    Console.WriteLine(trains.RemoveFront());
+                }
+                else if (backTrain != null && backTrain.Type == "F")
+                {
+                    Console.WriteLine(trains.RemoveBack());
+                }
+            }
+        }
+        private static void Next()
+        {
+            if (trains.Count > 0)
+            {
+                Train frontTrain = trains.GetFront();
+                Train backTrain = trains.GetBack();
+                if (backTrain != null && backTrain.Type == "F" && backTrain.Cars > 15)
+                {
+                    var item = backTrain.ToString();
+                    Console.WriteLine(item);
+                }
+                else if (frontTrain != null && frontTrain.Type == "P")
+                {
+                    var item = frontTrain.ToString();
+                    Console.WriteLine(item);
+                }
+                else if (backTrain != null && backTrain.Type == "F")
+                {
+                    var item = backTrain.ToString();
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        private static void History()
+        {
+            throw new NotImplementedException();
+        }
+
         static void Main(string[] args)
         {
-            
+            string[] command;
+            do
+            {
+                command = System.Console.ReadLine().Split(' ').ToArray();
+                switch (command[0])
+                {
+                    case "Add":
+                        Add(int.Parse(command[1]), command[2], command[3], int.Parse(command[4]));
+                        break;
+                    case "Travel":
+                        Travel();
+                        break;
+                    case "Next":
+                        Next();
+                        break;
+                    case "History":
+                        History();
+                        break;
+                }
+            } while (command[0] != "End");
+
         }
     }
 }
---
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.06/Train/Train.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train
+{
+    class Train
+    {
+        private int number;
+
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string type;
+
+        public string Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+
+        private int cars;
+
+        public int Cars
+        {
+            get { return cars; }
+            set { cars = value; }
+        }
+
+        public Train(int number, string name, string type, int cars)
+        {
+            this.Number = number;
+            this.Name = name;
+            this.Type = type;
+            this.Cars = cars;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Number} {this.Type} {this.Cars}";
+        }
+
+    }
+}
