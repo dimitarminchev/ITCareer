1e2d5697a62fcde0950c1f89521554850a116e43
Tony5768 <47269030+Tony5768@users.noreply.github.com>
2019-07-12 14:25:53 +0300

    Update Program.cs

--- 4. Introduction to Algorithms and Data Structures/2019/2019.07.05/616/Program.cs
+++ 4. Introduction to Algorithms and Data Structures/2019/2019.07.05/616/Program.cs
@@ -154,3 +154,102 @@ namespace _616
         regions[ind].Area = area;
     }
 }*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dinamit
+{
+    //Автор: Стоян Златев
+    //Малко по-различен начин :D
+    public class Region
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Area { get; set; }
+        public Region(int x, int y, int area = 0)
+        {
+            X = x;
+            Y = y;
+            Area = area;
+        }
+    }
+    class Program
+    {
+        public static void FindPath(int row, int col, char direction)
+        {
+            if (!IsInBounds(row, col))
+                return;
+            else if (!IsVisited(row, col))
+            {
+                Mark(row, col, direction);
+                FindPath(row, col + 1, 'R');
+                FindPath(row + 1, col, 'D');
+                FindPath(row, col - 1, 'L');
+                FindPath(row - 1, col, 'U');
+            }
+        }
+        public static bool IsInBounds(int row, int col)
+        {
+            if (row < 0 || col >= width || col < 0 || row >= height) return false;
+            return true;
+        }
+        public static void Mark(int row, int col, char direction)
+        {
+            var copy = matrix[row].ToCharArray();
+            copy[col] = '*';
+            matrix[row] = string.Join("", copy);
+            area++;
+        }
+        public static bool IsVisited(int row, int col)
+        {
+            if (matrix[row][col] == '*')
+                return true;
+            return false;
+        }
+        public static bool FindStartingCell()
+        {
+            for (int i = 0; i < height; i++)
+                for (int y = 0; y < width; y++)
+                    if (matrix[i][y] == '-')
+                    {
+                        dic.Add(new Region(y, i, 0));
+                        return true;
+                    }
+            return false;
+        }
+        public static void DrawMatrix()
+        {
+            for (int i = 0; i < height; i++)
+                matrix[i] = Console.ReadLine();
+        }
+        public static int height = int.Parse(Console.ReadLine());
+        public static int width = int.Parse(Console.ReadLine());
+        public static string[] matrix = new string[height];
+        public static int area = 0;
+        public static List<Region> dic = new List<Region>();
+        static void Main(string[] args)
+        {
+            DrawMatrix();
+            int counter = 0;
+            int row = 0, col = 0;
+            while (FindStartingCell() == true)
+            {
+                area = 0;
+                row = dic[counter].Y; col = dic[counter].X;
+                FindPath(row, col, '0');
+                dic[counter].Area = area;
+                counter++;
+            }
+            Console.WriteLine("Намерени области: {0}",dic.Count);
+            dic=dic.OrderByDescending(x => x.Area).ToList();
+            for (int i = 0; i < dic.Count; i++)
+            {
+                Console.WriteLine($"Област #{i+1} на ({dic[i].Y},{dic[i].X}) размер: {dic[i].Area}");
+            }
+        }
+    }
+}
