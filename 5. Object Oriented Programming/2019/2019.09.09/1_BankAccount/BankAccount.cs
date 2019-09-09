d834168c7452a9e06aa109a5383de2ebdd11a40f
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-09 10:12:52 +0300

    2019.09.09

---
+++ 5. Object Oriented Programming/2019/2019.09.09/1_BankAccount/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_BankAccount
+{
+    // Банкова сметка
+    public class BankAccount
+    {
+        // Сметка
+        private decimal balance;
+        public decimal Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+
+        // Конструктор
+        public BankAccount(decimal amount = 0)
+        {
+            this.balance = amount;
+        }
+
+        // Внасяне на пари по сметка
+        public void Deposit(decimal cash)
+        {
+            this.balance += cash;
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2019/2019.09.09/2_BankAccountTests/BankAccountTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using _1_BankAccount;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _2_BankAccountTests
+{
+    [TestClass] // Атрибут за клас за компонентно тестване 
+    public class BankAccountTests
+    {
+        [TestMethod] // Тест дали можем да създадем сметка и да я захраним
+        public void AccoutnInitilizeWithPositiveValue()
+        {
+            // Arrange
+            BankAccount account = new BankAccount(2000m);
+
+            // Act & Assert
+            Assert.AreEqual(2000m, account.Balance);
+        }
+
+        [TestMethod] // Тест за внасяне на пари по сметка
+        public void DespositShouldAddMoney()
+        {
+            // Arrange
+            BankAccount account = new BankAccount();
+
+            // Act
+            account.Deposit(50);
+
+            // Assert
+            Assert.IsTrue(account.Balance == 50);
+        }
+    }
+}
