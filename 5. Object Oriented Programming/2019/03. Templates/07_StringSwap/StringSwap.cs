2c45ac4acc385b1781e676ffdaa584840740a887
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-10 12:51:38 +0300

    10.09.2019

---
+++ 5. Object Oriented Programming/2019/03. Templates/07_StringSwap/StringSwap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_StringSwap
+{
+    public class StringSwap<T>
+    {
+        private T[] array;
+        private int index; 
+
+        public StringSwap(int capacity)
+        {
+            this.array = new T[capacity];
+            this.index = 0;
+        }
+
+        public void Add(T item)
+        {
+            this.array[index] = item;
+            this.index++;
+        }
+
+        public void Swap(int first, int second)
+        {
+            T temp = this.array[first];
+            this.array[first] = this.array[second];
+            this.array[second] = temp;
+        }
+
+        public void Print()
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2019/03. Templates/09_StringCounter/Box.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_StringCounter
+{
+    public class Box<T> where T : IComparable
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public int BiggerThan(T element)
+        {
+            int count = 0;
+            foreach (var item in this.items)
+            {
+                if (item.CompareTo(element) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public void Add(T element)
+        {
+            items.Add(element);
+        }
+
+        public T Remove()
+        {
+            T remove = items.Last();
+            items.Remove(items.Last());
+            return remove;
+        }
+
+        public void Print()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+
+        public override string ToString()
+        {
+            string temp = null, type = null;
+            foreach (var item in items)
+            {
+                type = item.GetType().ToString();
+                temp += item.ToString();
+            };
+            return $"{type}: {temp}";
+        }
+    }
+}
