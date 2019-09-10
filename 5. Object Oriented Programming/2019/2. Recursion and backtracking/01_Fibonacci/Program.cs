c92a019297542536fb9e74ab4957c8f9e6201b7f
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-10 14:39:38 +0300

    10.09.2019

---
+++ 5. Object Oriented Programming/2019/2. Recursion and backtracking/01_Fibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Fibonacci
+{
+    // Fibonacii
+    class Program
+    {
+        public static int Fib(int n)
+        {
+            if (n == 1 || n == 2) return 1;
+            return Fib(n - 1) + Fib(n - 2);
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int fibN = Fib(n);
+            Console.WriteLine(fibN);
+        }
+       
+    }
+}
