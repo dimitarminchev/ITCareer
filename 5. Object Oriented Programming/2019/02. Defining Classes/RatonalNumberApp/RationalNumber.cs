df3365af7d16dd4d615f3f15773095841d1016c3
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-10 09:17:24 +0300

    10.09.2019

---
+++ 5. Object Oriented Programming/2019/02. Defining Classes/RatonalNumberApp/RationalNumber.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatonalNumberApp
+{
+    public class RationalNumber
+    {
+        // Числител
+        private int number;
+
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        // Знаменател
+        private int denom;
+
+        public int Denom
+        {
+            get { return denom; }
+            set { denom = value; }
+        }
+
+        // Предефиниране на метод
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", this.number, this.denom);
+        }
+
+        // Най-големия общ делител
+        private int BigestDivider(int numerator, int denumerator)
+        {
+            return denumerator == 0 ? numerator : BigestDivider(denumerator, numerator % denumerator);
+        }
+
+        // Конструктор
+        public RationalNumber(int numerator = 0, int denumerator = 1)
+        {
+            int gcd = BigestDivider(numerator, denumerator);
+            this.Number = numerator / gcd;
+            this.Denom = denumerator / gcd;
+        }
+    }
+}
