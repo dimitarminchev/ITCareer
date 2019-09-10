625e1d53b0b9c66dc13a4312d5e7c370fdc2c02d
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-10 11:06:09 +0300

    10.09.2019

---
+++ 5. Object Oriented Programming/2019/02. Defining Classes/BufferedConsoleApp/BufferedConsole.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BufferedConsoleApp
+{
+    // Буферирана конзола
+    public class BufferedConsole : IDisposable
+    {
+        // Буфер
+        private char[] buffer;
+        private int pos;
+
+        // Конструктор
+        public BufferedConsole()
+        {
+            buffer = new char[50];
+            pos = 0;
+        }
+
+        // Добавяне към буфера
+        public void Write(string line)
+        {
+            bool BufferIsFull = false;
+
+            if (pos + line.Length >= 50)
+            {
+                line = line.Substring(0, 50 - pos);
+                BufferIsFull = true;
+            }
+
+            var source = line.ToArray<char>();
+            Array.Copy(source, 0, buffer, pos, line.Length);
+            pos += line.Length;
+
+            if (BufferIsFull) this.Dispose();
+        }
+
+        // Освобождаване на паметта
+        public void Dispose()
+        {
+            Console.WriteLine("> [{0}]", new String(buffer));
+            buffer = new char[50];
+            pos = 0;
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2019/02. Defining Classes/BufferedConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BufferedConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Буферирана конзола
+            BufferedConsole buf = new BufferedConsole();
+
+            // Безкарайно четене от конзолата
+            while (true)
+            {
+                buf.Write(Console.ReadLine());
+            }
+        }
+    }
+}
