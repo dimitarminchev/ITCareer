bd5bfb827a62b26886bae15416f4c481cf08e0b3
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-10 10:25:53 +0300

    10.09.2019

---
+++ 5. Object Oriented Programming/2019/02. Defining Classes/PersonCounter/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonCounter
+{
+    public class Person 
+    {
+        // Име
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // Възраст
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        // Конструктор
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            Person.count++;
+        }
+
+        // Брой създадени обекти 
+        private static int count = 0;
+
+        public static int Count
+        {  
+            // Статично свойство
+            get { return count; }
+        }
+
+    }
+}
