e3e8674725c8b4b27c52c865a712898e616660fb
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-10 11:02:27 +0300

    10.09.2019

---
+++ 5. Object Oriented Programming/2019/02. Defining Classes/MemoryApp/Animals.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MemoryApp
+{
+    // Animal
+    public class Animal
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public Animal(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    // Animals
+    public class Animals : IDisposable
+    {
+        // Animals List
+        public List<Animal> animals;
+
+        // Add Animal
+        public void Add(Animal animal)
+        {
+            this.animals.Add(animal);
+            Animals.count++;
+        }
+
+        // Counter
+        private static int count = 0;
+
+        public static int Count
+        {
+            get { return count; }
+        }
+
+        // Constructor
+        public Animals()
+        {
+            this.animals = new List<Animal>();            
+            Console.WriteLine("Constructor");
+        }
+
+        // Finalizer
+        ~Animals()
+        {
+            Console.WriteLine("Finalizer");
+        }
+
+        // Dispozer
+        public void Dispose()
+        {
+            Console.WriteLine("Dispozer");
+            this.animals = null;
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2019/02. Defining Classes/MemoryApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryApp
+{
+    class Program
+    {
+        // Memory App
+        static void Main(string[] args)
+        {
+            // Create Animals Instance
+            Animals animals = new Animals();
+
+            // Add Animals
+            animals.Add(new Animal("Krocodile", 70));
+            animals.Add(new Animal("Turle", 200));
+            animals.Add(new Animal("Pig", 12));
+            animals.Add(new Animal("Snake", 7));
+
+            // Animals Count
+            Console.WriteLine("Animals Count = {0}", Animals.Count);
+
+            // Dispose Animals Instance
+            animals.Dispose();
+        }
+    }
+}
