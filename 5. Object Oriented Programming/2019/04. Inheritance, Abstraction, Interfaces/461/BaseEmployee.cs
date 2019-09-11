20bab9030b4cbb9652f32fd7c5b4a6324e3451b9
Tony5768 <tonkatazl@gmail.com>
2019-09-11 12:40:28 +0300

    Up

--- 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/461/BaseEmployee.cs
+++ 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/461/BaseEmployee.cs
@@ -8,5 +8,38 @@ namespace _461
 {
     abstract class BaseEmployee
     {
+        private string id;
+
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string adress;
+
+        public string Adress
+        {
+            get { return adress; }
+            set { adress = value; }
+        }
+        public BaseEmployee(string name, string id, string adress)
+        {
+            this.adress = adress;
+            this.name = name;
+            this.id = id;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Employer {name} with {id} lives in {adress}");
+        }
+        public abstract double CalculateSalary(int workingHours);
+        public abstract string GetDepartment();
     }
 }
