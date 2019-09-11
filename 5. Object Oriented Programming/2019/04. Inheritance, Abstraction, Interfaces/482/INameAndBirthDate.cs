41d34de54149685347f313225147034ff8f4feac
Tony5768 <tonkatazl@gmail.com>
2019-09-11 13:40:33 +0300

    Push, Ceco

---
+++ 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/482/INameAndBirthDate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _482
+{
+    interface INameAndBirthDate
+    {
+        string Name { get; }
+        string BirthDate { get; }
+    }
+}
---
+++ 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/482/Robot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _482
+{
+    class Robot : Data
+    {
+        public Robot(string id,string name) : base(id, name)
+        {
+            //nope
+        }
+    }
+}
