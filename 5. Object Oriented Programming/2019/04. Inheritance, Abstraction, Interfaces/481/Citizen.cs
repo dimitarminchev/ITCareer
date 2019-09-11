6c69b3a33f17e9ddfa3dfb23780c423bfb42d7b9
Tsvetilin <cvetilov6@abv.b>
2019-09-11 13:12:35 +0300

    Update

---
+++ 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/481/Citizen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _481
+{
+    class Citizen : Data
+    {
+        private int age;
+        public Citizen(string id, string name, int age) : base(id, name)
+        {
+            this.age = age;
+        }
+    }
+}
