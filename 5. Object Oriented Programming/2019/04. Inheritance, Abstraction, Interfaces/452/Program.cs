2d4f8f42bf60d77bf887ed3e3308b131f06e790e
gancho19 <47268950+gancho19@users.noreply.github.com>
2019-09-11 13:20:54 +0300

    Update Program.cs

--- 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/452/Program.cs
+++ 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/452/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,34 @@ namespace _452
     {
         static void Main(string[] args)
         {
+            Type identifiableInterface = typeof(Citizen).GetInterface("IIdentifiable");
+
+            Type birthableInterface = typeof(Citizen).GetInterface("IBirthable");
+
+            PropertyInfo[] properties = identifiableInterface.GetProperties();
+
+            Console.WriteLine(properties.Length);
+
+            Console.WriteLine(properties[0].PropertyType.Name);
+
+            properties = birthableInterface.GetProperties();
+
+            Console.WriteLine(properties.Length);
+
+            Console.WriteLine(properties[0].PropertyType.Name);
+
+            string name = Console.ReadLine();
+
+            int age = int.Parse(Console.ReadLine());
+
+            string id = Console.ReadLine();
+
+            string birthdate = Console.ReadLine();
+
+            IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+
+            IBirthable birthable = new Citizen(name, age, id, birthdate);
         }
     }
 }
+
