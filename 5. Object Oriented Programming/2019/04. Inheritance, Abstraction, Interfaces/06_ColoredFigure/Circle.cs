76ab3deb8766b2f4ddd8ea2f6054507776d41c0f
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-11 10:46:36 +0300

    11.09.2019

---
+++ 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/06_ColoredFigure/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ColoredFigure
+{
+    // Derivative Class
+    public class Circle : ColoredFigure
+    {
+        public Circle(string color, double size) : base(color, size)
+        {
+            // nope
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Math.Pow(base.size, 2.0);
+        }
+
+        public override string GetName()
+        {
+            return "Circle";
+        }
+    }
+}
