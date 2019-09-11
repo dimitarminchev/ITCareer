0a60ffd0c1116378e34a81e0685e5cbf5a5b945a
VeselinInzov <47302313+VeselinInzov@users.noreply.github.com>
2019-09-11 12:35:04 +0300

    Update Program.cs

--- 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/451/Program.cs
+++ 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/451/Program.cs
@@ -10,6 +10,14 @@ namespace _451
     {
         static void Main(string[] args)
         {
+            Type personInterface = typeof(Citizen).GetInterface("IPerson");
+            PropertyInfo[] properties = personInterface.GetProperties();
+            Console.WriteLine(properties.Length);
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            IPerson person = new Citizen(name, age);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
         }
     }
 }
