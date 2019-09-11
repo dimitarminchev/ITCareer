0b803fa477c63553dcafbec9a5fa5c7660c453f2
hshirov <47268844+hshirov@users.noreply.github.com>
2019-09-11 12:38:50 +0300

    Update Program.cs

--- 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/471/Program.cs
+++ 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/471/Program.cs
@@ -1,15 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _471
+namespace AnimalKingdom
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Dog doge = new Dog("vanko", 2);
+
+            doge.MakeNoise();
+            doge.MakeTrick();
         }
     }
-}
