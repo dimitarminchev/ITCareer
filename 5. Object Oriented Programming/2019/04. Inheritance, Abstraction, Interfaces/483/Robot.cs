fa54b2b43a9e88560290227b98df21625444c68e
Tsvetilin <cvetilov6@abv.b>
2019-09-11 14:06:22 +0300

    Update

---
+++ 5. Object Oriented Programming/2019/04. Inheritance, Abstraction, Interfaces/483/Robot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _483
+{
+    class Robot : Data,INameAndBirthDate
+    {
+
+        public Robot(string id,string name,string b,int age) : base(id, name)
+        {
+            //nope
+            this.Age = age;
+            this.BirthDate = b;
+        }
+        public string BirthDate { get; set; }
+        public int Age { get; set; }
+
+        public string Name => throw new NotImplementedException();
+    }
+}
