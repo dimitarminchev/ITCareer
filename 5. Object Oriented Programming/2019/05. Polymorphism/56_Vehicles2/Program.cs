0ed07e95a78067e8970aa690cea3b47ef8520de2
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-12 11:21:21 +0300

    12.09.2019

---
+++ 5. Object Oriented Programming/2019/05. Polymorphism/56_Vehicles2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _56_Vehicles2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
