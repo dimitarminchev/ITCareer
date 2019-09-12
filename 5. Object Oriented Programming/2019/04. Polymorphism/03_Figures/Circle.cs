1c9e8f6c3d98a76e45084ce4b23f094b4ebf6fc0
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-12 09:13:06 +0300

    12.09.2019

---
+++ 5. Object Oriented Programming/2019/04. Polymorphism/03_Figures/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Figures
+{
+    // Derivative Class
+    public sealed class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double calculateArea()
+        {
+            return Math.PI * Math.Pow(this.radius,2.0);
+        }
+
+        public override double calculatePerimeter()
+        {
+            return 2.0 * Math.PI * this.radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + "Circle";
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2019/04. Polymorphism/03_Figures/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Figures
+{
+    class Program
+    {
+        // Task 3. Abstract & Sealed
+        static void Main(string[] args) 
+        {
+            // Circle
+            Shape circle = new Circle(3.0);
+            Console.WriteLine(circle.Draw());
+            Console.WriteLine("Area = {0:f2}", circle.calculateArea());
+            Console.WriteLine("Perimeter = {0:f2}", circle.calculatePerimeter());
+
+            // Rectangle
+            Shape rect = new Rectangle(2.3,3.0);
+            Console.WriteLine(rect.Draw()); 
+            Console.WriteLine("Area = {0:f2}", rect.calculateArea());
+            Console.WriteLine("Perimeter = {0:f2}", rect.calculatePerimeter());
+        }
+    }
+}
