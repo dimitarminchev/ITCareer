d08e55a209df77567ed81efb5689e542a5082b94
Hristo Shirov <hshirov@abv.bg>
2019-09-13 12:46:41 +0300

    Задача 6.3.1

---
+++ 5. Object Oriented Programming/2019/06. Working with Objects/631/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _631
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("_631.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
