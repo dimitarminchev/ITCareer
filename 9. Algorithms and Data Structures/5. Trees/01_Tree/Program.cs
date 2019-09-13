fc5dd526d4fb8700dbc04c8a7bf7f67d08749f25
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-13 18:16:14 +0300

    13.09.2019

---
+++ 9. Algorithms and Data Structures/5. Trees/01_Tree/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tree 
+            Tree<int> tree = 
+                new Tree<int>
+                (
+                    7, 
+                    new Tree<int>
+                    (
+                        19, 
+                        new Tree<int>(1), 
+                        new Tree<int>(12), 
+                        new Tree<int>(31) 
+                    ),
+                    new Tree<int>(21),
+                    new Tree<int>
+                    (
+                        14, 
+                        new Tree<int>(23), 
+                        new Tree<int>(6)
+                    )
+            );
+
+            // Print
+            tree.Print();
+        }
+    }
+}
---
+++ 9. Algorithms and Data Structures/5. Trees/02_DFS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_DFS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tree 
+            Tree<int> tree =
+                new Tree<int>
+                (
+                    7,
+                    new Tree<int>
+                    (
+                        19,
+                        new Tree<int>(1),
+                        new Tree<int>(12),
+                        new Tree<int>(31)
+                    ),
+                    new Tree<int>(21),
+                    new Tree<int>
+                    (
+                        14,
+                        new Tree<int>(23),
+                        new Tree<int>(6)
+                    )
+            );
+
+            // DFS
+            var ordered = tree.OrderDFS();
+            Console.WriteLine(string.Format("DFS: {0}", string.Join(", ",ordered)));
+            
+        }
+    }
+}
