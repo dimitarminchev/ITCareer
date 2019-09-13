f5c7c0f3a6b7ebd4d3287ea00bf3cc68b27b68ee
cakas <cakas@abv.bg>
2019-09-13 15:20:09 +0300

    Задача 1 - Алея

--- 9. Algorithms and Data Structures/4. Dynamic programming/03_Alley/Program.cs
+++ 9. Algorithms and Data Structures/4. Dynamic programming/03_Alley/Program.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
-namespace _03_Alley
+namespace Alley
 {
     class Program
     {
+        static int alleyLength;
+        static int[] allowedTileLengths;
+        static BigInteger counter;
+        static void SolveAlley(int remainingMeters)
+        {
+            if (remainingMeters == 0)
+            {
+                counter++;
+                return;
+            }
+            else if (remainingMeters > 0)
+            {
+                for (int i = 0; i < allowedTileLengths.Length; i++)
+                {
+                    if (remainingMeters - allowedTileLengths[i] >= 0)
+                    {
+                        SolveAlley(remainingMeters - allowedTileLengths[i]);
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
+            alleyLength = Console.ReadLine().Split().Select(int.Parse).ToArray()[0];
+            allowedTileLengths = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            counter = new BigInteger(0);
+            SolveAlley(alleyLength);
+            Console.WriteLine(counter);
         }
     }
 }
