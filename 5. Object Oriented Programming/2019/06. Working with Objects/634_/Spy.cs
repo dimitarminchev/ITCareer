8126efd02b82381d57a4826d600dd8d5f6600025
Hristo Shirov <hshirov@abv.bg>
2019-09-13 12:40:24 +0300

    update

---
+++ 5. Object Oriented Programming/2019/06. Working with Objects/634_/Spy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _634_
+{
+    class Spy
+    {
+        public string CollectGettersAndSetters(string investigatedClass)
+        {
+            Type classType = Type.GetType(investigatedClass);
+
+            MethodInfo[] classMethods =
+                classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("get")))
+            {
+                stringBuilder.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+
+            foreach (MethodInfo method in classMethods.Where(m => m.Name.StartsWith("set")))
+            {
+                stringBuilder.AppendLine($"{method.Name} will set field of {method.ReturnType}");
+            }
+
+            return stringBuilder.ToString().Trim();
+        }
+    }
+}
