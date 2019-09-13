9454ab5952aa8208915b13663f481c38bcc5ad62
Tony5768 <tonkatazl@gmail.com>
2019-09-13 18:54:07 +0300

    Up

---
+++ 5. Object Oriented Programming/2019/06. Working with Objects/632/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _632
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string newResult = spy.AnalyzeAcessModifiers("Hacker");
+            Console.WriteLine(newResult);
+        }
+    }
+}
