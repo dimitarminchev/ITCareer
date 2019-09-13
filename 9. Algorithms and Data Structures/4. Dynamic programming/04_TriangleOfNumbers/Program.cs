1c33e96f900bebc4044a885fe15f671ab33f44e7
cakas <cakas@abv.bg>
2019-09-13 15:55:52 +0300

    Задача 2 - Триъгълник от числа

--- 9. Algorithms and Data Structures/4. Dynamic programming/04_TriangleOfNumbers/Program.cs
+++ 9. Algorithms and Data Structures/4. Dynamic programming/04_TriangleOfNumbers/Program.cs
@@ -3,13 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
-namespace _04_TriangleOfNumbers
+namespace Alley
 {
     class Program
     {
+        static int[,] matrix;
+        static int[,] maxSums;
+
         static void Main(string[] args)
         {
+            // Input
+            int n = int.Parse(Console.ReadLine());
+            List<int[]> rowList = new List<int[]>();
+            for (int i = 0; i < n; i++)
+            {
+                rowList.Add(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            }
+
+            // Convert 
+            matrix = new int[n, n + 1];
+            maxSums = new int[n, n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                matrix[i, 0] = 0;
+                maxSums[i, 0] = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    try
+                    {
+                        matrix[i, j + 1] = rowList[i][j];
+                    }
+                    catch
+                    {
+                        matrix[i, j + 1] = 0;
+                    }
+                }
+            }
+
+            // Process
+            maxSums[0, 1] = matrix[0, 1];
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < n + 1; j++)
+                {
+                    maxSums[i, j] = matrix[i, j] + Math.Max(maxSums[i - 1, j - 1], maxSums[i - 1, j]);
+                }
+            }
+
+            // Display
+            int[] lastRow = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                lastRow[i] = maxSums[n - 1, i + 1];
+            }
+            Console.WriteLine(lastRow.Max());
         }
     }
 }
