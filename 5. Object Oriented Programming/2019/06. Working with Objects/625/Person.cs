af04f8f60fe34605f8b2f281d79df42f8cb808df
Tsvetilin <cvetilov6@abv.bg>
2019-09-14 08:41:04 +0300

    Update

---
+++ 5. Object Oriented Programming/2019/06. Working with Objects/625/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _625
+{
+    class Person:IComparable
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public override bool Equals(object obj) 
+        {
+            Person person = (Person)obj;
+            if (this.Age == person.age && this.name == person.name) return true;
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return this.name.GetHashCode()+age;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Person person = (Person)obj;
+            if (this.Age == person.age && this.name == person.name) return 0;
+            return 1;
+        }
+    }
+}
