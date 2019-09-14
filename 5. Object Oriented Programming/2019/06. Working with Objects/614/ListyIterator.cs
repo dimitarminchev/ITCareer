f79b3f48d57dc54f2402be19c0f676cf09b5f742
Tony5768 <tonkatazl@gmail.com>
2019-09-14 08:49:38 +0300

    Up

---
+++ 5. Object Oriented Programming/2019/06. Working with Objects/614/ListyIterator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _614
+{
+
+    public class ListyIterator<T>: IEnumerable<T> 
+    {
+        private int index = 0;
+        private List<T> list;
+
+        public ListyIterator(params T[] listy)
+        {
+            this.Create(listy);
+        }
+
+        public void Create(params T[] listy)
+        {
+            this.list = new List<T>(listy);
+        }
+
+        public bool Move()
+        {
+            if (!HasNext()) return false;
+            else
+            {
+                this.index = this.index + 1;
+                return true;
+            }
+        }
+
+        public string Print()
+        {
+            if (this.list.Count == 0)
+            {
+                throw new Exception("Invalid Operation");
+            }
+            return this.list[this.index].ToString();
+        }
+
+        public bool HasNext()
+        {
+            if (this.index + 1 > this.list.Count - 1) return false;
+            else return true;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count(); i++)
+            {
+                yield return this.list[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        public void PrintAll()
+        {
+            foreach (var item in list)
+            {
+                Console.Write(item+" ");
+            }
+        }
+    }
+}
