23ae242cfdedfd88281bf027e4c70845400fcc43
Hristo Shirov <hshirov@abv.bg>
2019-09-14 09:44:48 +0300

    Задача 6.1.5

---
+++ 5. Object Oriented Programming/2019/06. Working with Objects/615/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _615
+{
+    class Stack<T> : IEnumerable<T>
+    {
+        public T[] array;
+        public int count;
+
+        public Stack()
+        {
+            array = new T[5];
+            count = -1;
+        }
+
+        public void Push(T item)
+        {
+            count++;
+            if (count > array.Length)
+            {
+                Array.Resize<T>(ref array, count + 1);
+            }
+
+            array[count] = item;
+        }
+
+        public T Pop()
+        {
+            if (count > 0)
+            {
+                count--;
+                return array[count + 1];
+            }
+            else
+            {
+                return array[count];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new MyEnumerator<T>(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new MyEnumerator<T>(this);
+        }
+    }
+}
