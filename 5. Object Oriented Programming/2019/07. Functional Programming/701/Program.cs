6d0741effe594aa6eacc359e8ede80f294a37e5d
Hristo Shirov <hshirov@abv.bg>
2019-09-14 10:45:17 +0300

    Задача 701

---
+++ 5. Object Oriented Programming/2019/07. Functional Programming/701/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _701
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+
+            Action<string[]> print = x => {
+                foreach (string item in x)
+                    Console.WriteLine(item);
+            };
+
+            print(input);
+        }
+    }
+}
