2071373af7dbbd7da33c5df9dde71898c13f45a8
cakas <cakas@abv.bg>
2019-09-14 15:35:35 +0300

    Задача 1 - Въведение; Задача 2 - Корен на дървото

---
+++ 9. Algorithms and Data Structures/5. Trees/05_BasicTreeImplementation/Tree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_BasicTreeImplementation
+{
+    public class Tree<T>
+    {
+        public T Value { get; set; }
+        public Tree<T> Parent { get; private set; }
+        public List<Tree<T>> Children { get; private set; }
+
+        public Tree(T value, params Tree<T>[] children)
+        {
+            this.Value = value;
+            if (children == null)
+            {
+                this.Children = new List<Tree<T>>();
+            }
+            else
+            {
+                this.Children = children.ToList();
+                foreach (var child in children)
+                {
+                    child.Parent = this;
+                }
+            }
+        }
+
+        public void AddChild(Tree<T> newChild)
+        {
+            Children.Add(newChild);
+        }
+    }
+}
