09ec47172ee353d9e4654a3991b154f60482a73e
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-15 14:00:50 +0300

    15.09.2019

---
+++ 9. Algorithms and Data Structures/6. Hashing and hash tables/01_PointHashDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_PointHashDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Points
+            Point a = new Point(2, 3);
+            Point b = new Point(3, 2);
+            Point c = new Point(4, 4);
+            
+            // Hashes
+            Console.WriteLine("Point {0}, Hash = {1}", a, a.GetHashCode());
+            Console.WriteLine("Point {0}, Hash = {1}", b, b.GetHashCode());
+            Console.WriteLine("Point {0}, Hash = {1}", c, c.GetHashCode());
+        }
+    }
+}
