a9eadeed005223124f7bd301f7bf0700919a4b35
Tsvetilin <cvetilov6@abv.bg>
2019-09-15 11:20:55 +0300

    Update

---
+++ 5. Object Oriented Programming/2019/08. Communication Between Objects/55_KingsGambit2/King.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _55_KingsGambit2
+{
+    class King
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public King(string name)
+        {
+            this.name = name;
+        }
+        public void Attack(object sender, EventArgs e)
+        {
+            Console.WriteLine($"King {name} is under attack!");
+        }
+
+        public event EventHandler AttackedKing;
+
+        public void UnderAttack(EventArgs e)
+        {
+            AttackedKing(this, e);
+        }
+
+    }
+}
---
+++ 5. Object Oriented Programming/2019/08. Communication Between Objects/55_KingsGambit2/RoyalGuard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _55_KingsGambit2
+{
+    class RoyalGuard:EventArgs
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int health;
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        public RoyalGuard(string name)
+        {
+            this.name = name;
+            health = 3;
+        }
+        public void Attack(object sender, EventArgs e)
+        {
+            if (this.health == 0)
+            {
+                return;
+            }
+            health--;
+            Console.WriteLine($"Royal Guard {name} is defending!");
+            if (health == 0) Death(this,e);
+        }
+        public void Death(object sender, EventArgs e)
+        {
+            if (this.health == 0)
+            {
+                return;
+            }
+            health--;
+            Console.WriteLine($"Royal Guard {name} is defending!");
+        }
+    }
+}
