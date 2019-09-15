315681df41e60a549af0d16e76077deac22708e6
Hristo Shirov <hshirov@abv.bg>
2019-09-15 10:47:35 +0300

    51 Event Demo

---
+++ 5. Object Oriented Programming/2019/08. Communication Between Objects/51_EventsDemo/Handler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _51_EventsDemo
+{
+    class Handler
+    {
+        public void OnDispatcherNameChange(object sender, NameChangeEventArgs args)
+        {
+            Console.WriteLine("Dispatcher’s name changed to " + args.Name);
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2019/08. Communication Between Objects/51_EventsDemo/NameChangeEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _51_EventsDemo
+{
+    class NameChangeEventArgs : EventArgs
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public NameChangeEventArgs(string n)
+        {
+            name = n;
+        }
+    }
+}
