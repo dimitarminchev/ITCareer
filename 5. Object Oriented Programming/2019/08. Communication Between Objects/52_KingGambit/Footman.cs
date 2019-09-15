87d886c1d7ae524f6fa1b4df2c200d23b391f008
Tsvetilin <cvetilov6@abv.bg>
2019-09-15 10:53:58 +0300

    Update

---
+++ 5. Object Oriented Programming/2019/08. Communication Between Objects/52_KingGambit/Footman.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _52_KingGambit
+{
+    class Footman
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Footman(string name)
+        {
+            this.name = name;
+        }
+
+        public void Attack(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Footman {name} is panicking!");
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2019/08. Communication Between Objects/52_KingGambit/RoyalGuard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _52_KingGambit
+{
+    class RoyalGuard:EventArgs
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public RoyalGuard(string name)
+        {
+            this.name = name;
+        }
+        public void Attack(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Royal Guard {name} is defending!");
+        }
+        
+    }
+}
