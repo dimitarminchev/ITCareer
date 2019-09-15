5c73670e71bd9e68234c29b0e70da87f40171cf6
DimitarDimitrovMitev <miti_02@abv.bg>
2019-09-15 10:51:53 +0300

    8.0.3

---
+++ 5. Object Oriented Programming/2019/08. Communication Between Objects/51_Dependancies/IStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D
+{
+    interface IStrategy
+    {
+        int Calculate(int first, int second);
+    }
+}
---
+++ 5. Object Oriented Programming/2019/08. Communication Between Objects/51_Dependancies/Subtraction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D
+{
+    public class Subtraction : IStrategy
+    {
+        public int Calculate(int firstOperand, int secondOperand)
+        {
+            return firstOperand - secondOperand;
+        }
+    }
+}
