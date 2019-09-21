b1a981c07818977ef017114ebbb1af4deb7a2dd6
Tsvetilin <cvetilov6@abv.bg>
2019-09-21 09:29:49 +0300

    Update

--- 5. Object Oriented Programming/2019/09. Exceptions/03_SquareRoot/Program.cs
+++ 5. Object Oriented Programming/2019/09. Exceptions/03_SquareRoot/Program.cs
@@ -10,6 +10,21 @@ namespace _03_SquareRoot
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            try
+            {
+                if (n < 0) throw new Exception("Invalid number.");
+                double sqrt = Math.Sqrt(n);
+                Console.WriteLine(sqrt);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye.");
+            }
         }
     }
 }
