0bc403658800bb8c9181166883af38bf78d233e0
Tsvetilin <cvetilov6@abv.bg>
2019-09-21 09:30:41 +0300

    Update

--- 5. Object Oriented Programming/2019/09. Exceptions/04_EnterNumbers/Program.cs
+++ 5. Object Oriented Programming/2019/09. Exceptions/04_EnterNumbers/Program.cs
@@ -8,8 +8,26 @@ namespace _04_EnterNumbers
 {
     class Program
     {
+        public static void ReadNumber(int start, int end)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n < start || n > end) throw new InvalidOperationException($"Number must be in range {start} - {end}");
+        }
         static void Main(string[] args)
         {
+            int s = int.Parse(Console.ReadLine());
+            int e = int.Parse(Console.ReadLine());
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    ReadNumber(s, e);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
