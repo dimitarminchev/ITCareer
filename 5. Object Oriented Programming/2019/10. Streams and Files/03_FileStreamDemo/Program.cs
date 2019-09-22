4272cdedc1085c472a45cd9f2bcec18950c176e4
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-22 09:24:12 +0300

    22.09.2019

---
+++ 5. Object Oriented Programming/2019/10. Streams and Files/03_FileStreamDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_FileStreamDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Това е много хубава програма, която написахме в Сливен";
+
+            // v1
+            var fileStream = new FileStream("../../log.txt", FileMode.Create);            
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(text);
+                fileStream.Write(bytes, 0, bytes.Length);
+            }
+            finally
+            {
+                fileStream.Close(); 
+            }
+
+/*
+            // v2
+            using(var fileStream = new FileStream("../../log.txt", FileMode.Create))
+            {
+               byte[] bytes = Encoding.UTF8.GetBytes(text);
+               fileStream.Write(bytes, 0, bytes.Length);
+
+            } // filestream.Dispose();
+*/
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2019/10. Streams and Files/04_MemoryStreamDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_MemoryStreamDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Проблем ли има с кирилицата?
+            string text = "This mega amaizing multi app is written in Sliven.";
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            using (var memoryStream = new MemoryStream(bytes))
+            {
+                while (true)
+                {
+                    int readByte = memoryStream.ReadByte();
+                    if (readByte == -1) break;
+                    Console.Write((char)readByte);
+                }
+            }
+ 
+        }
+    }
+}
