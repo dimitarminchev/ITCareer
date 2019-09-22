b8f76b678c2365728af6147d9a8c267c1cd0ece0
Tsvetilin <cvetilov6@abv.bg>
2019-09-22 10:53:42 +0300

    Update

---
+++ 5. Object Oriented Programming/2019/10. Streams and Files/1024_FolderSearch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1024_FolderSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searched = Console.ReadLine();
+            //string[] files = Directory.GetFiles(Directory.GetCurrentDirectory().ToString());
+            DirectoryInfo d = new DirectoryInfo("../../"/*+Directory.GetCurrentDirectory()*/);
+            FileInfo[] fileInfo = d.GetFiles($"*{searched}*");
+            fileInfo = fileInfo.OrderBy(x => x.Extension).ThenBy(x => x.Length).ToArray();
+            using (StreamWriter writer = new StreamWriter("report.txt"))
+            {
+                string lastExt = string.Empty;
+                foreach (FileInfo file in fileInfo)
+                {
+                    if (lastExt != file.Extension)
+                    {
+                        lastExt = file.Extension;
+                        writer.WriteLine($".{lastExt}");
+                    }
+                    writer.WriteLine($"--{file.Name}.{file.Extension} - {file.Length/1000}kb");
+                }
+            }
+        }
+    }
+}
---
+++ 5. Object Oriented Programming/2019/10. Streams and Files/1025_FullFolderSearch/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1025_FullFolderSearch
+{
+    class Program
+    {
+        public static string searched;
+        static void Main(string[] args)
+        {
+            StreamWriter r = new StreamWriter("report.txt");
+            r.Close();
+            searched = Console.ReadLine();
+            DirectoryInfo dir = new DirectoryInfo("../../");
+            GetInfo(dir);
+            DirectoryInfo[] ad = dir.GetDirectories();
+            foreach (var item in ad)
+            {
+                GetInfo(item);
+            }
+            /*FileInfo[] fileInfo = dir.GetFiles($"*{searched}*");
+            fileInfo = fileInfo.OrderBy(x => x.Extension).ThenBy(x => x.Length).ToArray();
+            using (StreamWriter writer = new StreamWriter("report.txt"))
+            {
+                string lastExt = string.Empty;
+                foreach (FileInfo file in fileInfo)
+                {
+                    if (lastExt != file.Extension)
+                    {
+                        lastExt = file.Extension;
+                        writer.WriteLine($".{lastExt}");
+                    }
+                    writer.WriteLine($"--{file.Name}.{file.Extension} - {file.Length / 1000}kb");
+                }
+            }*/
+        }
+        static void GetInfo(DirectoryInfo dir)
+        {
+            DirectoryInfo[] ad = dir.GetDirectories();
+            if (ad.Count()!=0)
+            foreach (var item in ad)
+            {
+                GetInfo(item);
+            }
+            FileInfo[] fileInfo = dir.GetFiles($"*{searched}*");
+            fileInfo = fileInfo.OrderBy(x => x.Extension).ThenBy(x => x.Length).ToArray();
+            using (StreamWriter writer = new StreamWriter("report.txt",true))
+            {
+                string lastExt = string.Empty;
+                foreach (FileInfo file in fileInfo)
+                {
+                    if (lastExt != file.Extension)
+                    {
+                        lastExt = file.Extension;
+                        writer.WriteLine($".{lastExt}");
+                    }
+                    writer.WriteLine($"--{file.Name}.{file.Extension} - {file.Length / 1000}kb");
+                }
+            }
+        }
+    }
+}
