9d0a6b66b8a6dd5e16413a6c5af361abea1eece1
cakas <cakas@abv.bg>
2019-09-23 17:00:38 +0300

    Задача 4 - Good Omens

---
+++ 9. Algorithms and Data Structures/ExamPrep/GoodOmens/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoodOmens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
