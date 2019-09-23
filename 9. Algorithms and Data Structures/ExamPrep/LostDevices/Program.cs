dee01295fe726cc49f124d7c709b12cccdccc744
cakas <cakas@abv.bg>
2019-09-23 15:10:17 +0300

    Задача 2 - Lost Devices

---
+++ 9. Algorithms and Data Structures/ExamPrep/LostDevices/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LostDevices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> pigeons = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToList();
+            List<int> mail = Console.ReadLine().Split().Select(int.Parse).OrderBy(x => x).ToList();
+            List<int> positionDifferences = new List<int>();
+            for(int i = 0; i< pigeons.Count; i++)
+            {
+                positionDifferences.Add(Math.Abs(pigeons[i] - mail[i]));
+            }
+            Console.WriteLine("Job done in {0} hours", positionDifferences.Max());
+        }
+    }
+}
