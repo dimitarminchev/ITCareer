09b347a0a72234c5e6c5af0671086352af98ffb7
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-09-29 14:24:46 +0300

    29.09.2019

---
+++ 9. Algorithms and Data Structures/2019.09.29 - ExamPrep/1_Cinema/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
