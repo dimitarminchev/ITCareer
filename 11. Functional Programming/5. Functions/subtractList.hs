65291440ebaf2901e09d309340562906a4988bf8
Dimitar Minchev <dimitar.minchev@gmail.com>
2019-11-03 16:32:18 +0200

    Update

--- 11. Functional Programming/5. Functions/subtractList.hs
+++ 11. Functional Programming/5. Functions/subtractList.hs
@@ -5,4 +5,7 @@ subtractList' list = foldr (-) 0 list
 
 main = do
     putStrLn(show(subtractList [1,2,3,4,5] )) -- -15
+    -- (((((0-5)-4)-3)-2)-1) => -15
+
     putStrLn(show(subtractList' [1,2,3,4,5] )) -- 3
+    -- (5-(4-(3-(2-(1-0))))) => 3
