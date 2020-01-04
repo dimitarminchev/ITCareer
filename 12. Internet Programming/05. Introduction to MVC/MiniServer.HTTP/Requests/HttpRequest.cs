423d346f155855b4346898a01fcd87819011bf8a
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-01-04 15:11:14 +0200

    04.01.2020

--- 12. Internet Programming/05. Introduction to MVC/MiniServer.HTTP/Requests/HttpRequest.cs
+++ 12. Internet Programming/05. Introduction to MVC/MiniServer.HTTP/Requests/HttpRequest.cs
@@ -40,12 +40,6 @@ namespace MiniServer.HTTP.Requests
 
         private bool IsValidRequestQueryString(string queryString, string[] queryParameters)
         {
-            //if (!string.IsNullOrEmpty(queryString) && queryParameters.Length >= 1)
-            //{
-            //    return true;
-            //}
-            //else return false;
-
             CoreValidator.ThrowIfNullOrEmpty(queryString, nameof(queryString));
             return true; 
         }
@@ -69,29 +63,11 @@ namespace MiniServer.HTTP.Requests
 
         private void ParseRequestPath()
         {
-            // this.Path = this.Url.Split(new[] { '?', '#' }, StringSplitOptions.RemoveEmptyEntries)[0];
             this.Path = this.Url.Split('?')[0];
         }
 
         private void ParseHeaders(string[] requestContent)
         {
-            //for (int i = 0; i < requestContent.Length; i++)
-            //{
-            //    if (string.IsNullOrEmpty(requestContent[i].Trim()))
-            //    {
-            //        break;
-            //    }
-            //    string[] keyValueArr = requestContent[i].Split(new char[] { ':' });
-
-            //    HttpHeader headerToAdd = new HttpHeader(keyValueArr[0], keyValueArr[1].Trim());
-            //    this.Headers.AddHeader(headerToAdd);
-            //}
-
-            //if (!Headers.ContainsHeader("Host"))
-            //{
-            //    throw new BadRequestException();
-            //}
-
             requestContent.Select(plainHeader => plainHeader.Split(new[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries)).ToList().ForEach(headerKeyValuePair => this.Headers.AddHeader(new HttpHeader(headerKeyValuePair[0], headerKeyValuePair[1])));
         }
 
@@ -102,29 +78,6 @@ namespace MiniServer.HTTP.Requests
 
         private void ParseQueryParameters()
         {
-            //int indexOfQuestionMark = this.Url.IndexOf('?');
-            //if (indexOfQuestionMark == -1)
-            //{
-            //    return;
-            //}
-
-            //int indexOfAnchor = this.Url.IndexOf('#');
-            //if (indexOfAnchor == -1) indexOfAnchor = this.Url.Length - 1;
-
-            //string queryString = this.Url.Substring(indexOfQuestionMark, indexOfAnchor - indexOfQuestionMark + 1);
-            //string[] queryParameters = queryString.Split(new char[] { '&', '#', '?' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
-
-            //if (!IsValidRequestQueryString(queryString, queryParameters))
-            //{
-            //    throw new BadRequestException();
-            //}
-
-            //for (int i = 0; i < queryParameters.Length; i++)
-            //{
-            //    string[] keyValueArr = queryParameters[i].Split('=').ToArray();
-            //    QueryData.Add(keyValueArr[0], keyValueArr[1]);
-            //}
-
             if (!(this.Url.Split('?').Length > 1)) return;
             this.Url.Split('?', '#')[1].Split('&').Select(plainQueryParameter => plainQueryParameter.Split('=')).ToList().ForEach
             (
@@ -134,18 +87,6 @@ namespace MiniServer.HTTP.Requests
 
         private void ParseFormDataParameters(string formData)
         {
-            //formData.Trim();
-            //if (!string.IsNullOrEmpty(formData))
-            //{
-            //    string[] pairs = formData.Split('&').ToArray();
-            //    for (int i = 0; i < pairs.Length; i++)
-            //    {
-            //        string currentPair = pairs[i];
-            //        string[] pairArray = currentPair.Split('=').ToArray();
-            //        this.FormData.Add(pairArray[0], pairArray[1]);
-            //    }
-            //}
-
             if(!string.IsNullOrEmpty(formData))
             {
                 // TODO: Parse Multiple Parameters By Name
@@ -191,7 +132,6 @@ namespace MiniServer.HTTP.Requests
             this.ParseRequestPath();
 
             this.ParseHeaders(this.ParsePlainRequestHeaders(splitRequestContent).ToArray());
-            //this.ParseHeaders(splitRequestContent.Skip(1).ToArray());
             // this.ParseCookies();
 
             this.ParseRequestParameters(splitRequestContent[splitRequestContent.Length - 1]);
