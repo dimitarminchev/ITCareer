4c43ae6e16b2a18a5dfb7e9d4743a5e31a560a65
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-01-05 09:59:07 +0200

    05.01.2020

--- 12. Internet Programming/06. Introduction to MVC (part 2)/FDMC/Startup.cs
+++ 12. Internet Programming/06. Introduction to MVC (part 2)/FDMC/Startup.cs
@@ -14,12 +14,12 @@ namespace FDMC
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -40,17 +40,33 @@ namespace FDMC
             {
                 app.UseExceptionHandler("/Error");
             }
-
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
             });
+
+            UpdateDatabase(app);
+        }
+
+        // Ensure the database is created and adds two records.
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<CatContext>())
+                {
+                    context.Database.EnsureCreated();
+                    if (!context.Cats.Any())
+                    {
+                        context.Cats.Add(new Cat { Name = "Tom", Age = 5, Breed = "Black", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Close_up_of_a_black_domestic_cat.jpg/1200px-Close_up_of_a_black_domestic_cat.jpg" });
+                        context.Cats.Add(new Cat { Name = "Mary", Age = 7, Breed = "Gray", ImageUrl = "http://4.bp.blogspot.com/-ABNCNaeAWmU/VqkYRjTnbPI/AAAAAAABj_A/ef8aRYwWiJU/s1600/funny-cats-191-02.jpg" });
+                        context.SaveChanges();
+                    }
+                }
+            }
         }
     }
 }
