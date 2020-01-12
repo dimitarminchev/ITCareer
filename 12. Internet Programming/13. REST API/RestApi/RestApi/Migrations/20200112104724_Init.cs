289002edeca22ee6a772d14a139075d3dc8e2279
Nikolai <35627380+destbg@users.noreply.github.com>
2020-01-12 13:01:59 +0200

    12.01.2020

---
+++ 12. Internet Programming/13. REST API/RestApi/RestApi/Migrations/20200112104724_Init.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestApi.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Content = table.Column<string>(nullable: true),
+                    User = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+        }
+    }
+}
---
+++ 12. Internet Programming/13. REST API/RestApi/RestApi/ViewModel/MessageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RestApi.ViewModel
+{
+    public class MessageViewModel
+    {
+        public string Content { get; set; }
+        public string User { get; set; }
+    }
+}
