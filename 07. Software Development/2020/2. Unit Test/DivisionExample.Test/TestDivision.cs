783bac8316acb294b3c8254ee28807584a0b3c2d
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-04-12 10:48:46 +0300

    12.04.2020

---
+++ 07. Software Development/2020/2. Unit Test/DivisionExample.Test/TestDivision.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DivisionExample;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DivisionExample.Test
+{
+    [TestClass]
+    public class TestDivision
+    {
+        [TestMethod]
+        public void Dividing4By2Gives2()
+        {
+            // Arrange
+            Divider div = new Divider();
+
+            // Act
+            int result = div.Divide(4, 2);
+
+            // Assert
+            Assert.AreEqual(2, result, "Dividing 4 by 2 does not result in 2.");
+        }
+
+        [TestMethod]
+        public void TestDivideZero()
+        {
+            // Arrange
+            Divider div = new Divider();
+
+            // Act
+            int result = div.Divide(4, 0);
+
+            // Assert
+            Assert.AreEqual(0, result, "Dividing by zero fails");
+        }
+    }
+}
---
+++ 07. Software Development/2020/2. Unit Test/DivisionExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DivisionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Divider div = new Divider();
+            Console.WriteLine("4/2={0}", div.Divide(4, 2));
+            Console.WriteLine("4/0={0}", div.Divide(4, 0));
+        }
+    }
+}
