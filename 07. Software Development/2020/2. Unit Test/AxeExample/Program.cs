669afbc6fd7ec348322c4a463e9a3aa3b5cac72c
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-04-12 14:58:38 +0300

    12.04.2020

---
+++ 07. Software Development/2020/2. Unit Test/AxeExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AxeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hero = new Hero("Hero");
+            var dummy = new Dummy(50, 10);
+            while (!dummy.IsDead())
+            {
+                hero.Attack(dummy);
+            }
+        }
+    }
+}
