def266f371446bd7fcc90d36300f60346f9e5aa9
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-04-13 18:01:48 +0300

    13.04.2020

---
+++ 07. Software Development/2020/3. Debuging/Introduction/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Integer Array
+            int[] numbers = new int[] { 1,2,3 };
+
+            // Print the Array
+            Console.WriteLine(string.Join(", ", numbers));
+            
+        }
+    }
+}
