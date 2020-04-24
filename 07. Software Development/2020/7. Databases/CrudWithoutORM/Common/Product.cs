0da79c5212c0cc5f5ab4c25f6551f88a294e6b37
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-04-24 17:31:36 +0300

    24.04.2020

--- 07. Software Development/2020/7. Databases/CrudWithoutORM/Common/Product.cs
+++ 07. Software Development/2020/7. Databases/CrudWithoutORM/Common/Product.cs
@@ -4,9 +4,13 @@ namespace CrudWithoutORM.Common
     public class Product
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public decimal Price { get; set; }
+
         public int Stock { get; set; }
+
         public Product(int id, string name, decimal price, int stock)
         {
             this.Id = id;
