ff93973dd81941ddc81260b6bc582ed83c778a6f
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-09-12 16:17:10 +0300

    12.09.2020

--- 10. Algorithms and Data Structures/2020/2020.09.12/3. Backpack/Program.cs
+++ 10. Algorithms and Data Structures/2020/2020.09.12/3. Backpack/Program.cs
@@ -8,19 +8,59 @@ namespace _3._Backpack
     {
         static void Main(string[] args) 
         {
-            int[] w = { 7, 3, 4, 5 }; // Тегла на предметите в килограми
-            int[] v = { 42, 12, 40, 25 }; // Цени на предметите в килограми
+            List<int> w = new List<int> { 7, 3, 4, 5 }; // Тегла на предметите в килограми
+            List<int> v = new List<int> { 42, 12, 40, 25 }; // Цени на предметите в килограми
 
             int maxCapacity = 10;  // Максимален капацитет на раницата
-            int currCapacity = 0; // Текуша капацитет на раницата
-            int maxSum = 0; // Максимална сума
-            int currSum = 0; // Текуща сума
+            int maxValue = 0; // Максимална сума
 
-            // 1. Вариации на масива W, които се прилагат и на масива V
-            // 2. Обхождат се вариациите на W, взима се най-голяма сума от V
-            // 3. Извеждаме получената сума
+            // Намиране комбинациите на тегла и цени
+            var weights = GetCombination(w);
+            var prices =  GetCombination(v);
+            int vi = 0;
+
+            // Обхождаме получените комбинации от тегла
+            foreach (var item in weights)
+            {
+                // Суми на тегла и цени
+                int sumPrices = prices[vi].Sum();
+                int sumWeights = item.Sum();
+                vi++;
+
+                // Отпечатваме
+                Console.Write(string.Join(" + ", item));
+                Console.WriteLine(" = {0} kg / {1}$", sumWeights, sumPrices);
+
+                // Получаване на максималната сума
+                if (sumWeights <= maxCapacity && sumPrices > maxValue)
+                {
+                    maxValue = sumPrices;
+                }
+            }
+
+            // Отпечаване на максималната сума
+            Console.WriteLine("Maximum value: {0}$", maxValue);
         }
 
-
+        // Матод за намиране на комбинациите на елементите 
+        static List<List<int>> GetCombination(List<int> list)
+        {
+            var resultList = new List<List<int>>();
+            double count = Math.Pow(2, list.Count);
+            for (int i = 1; i <= count - 1; i++)
+            {
+                var nextList = new List<int>();
+                string str = Convert.ToString(i, 2).PadLeft(list.Count, '0');
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[j] == '1')
+                    {
+                        nextList.Add(list[j]);
+                    }
+                }
+                resultList.Add(nextList);
+            }
+            return resultList;
+        }
     }
 }
--- 10. Algorithms and Data Structures/2020/2020.09.12/4. Jobs/Program.cs
+++ 10. Algorithms and Data Structures/2020/2020.09.12/4. Jobs/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _4._Jobs
 {
@@ -6,6 +8,9 @@ namespace _4._Jobs
     {
         static void Main(string[] args)
         {
+            const int MAX = 4; // Брой елементи
+            int minSum = 1000000; // Минимална сума за свършване на работата
+
             // 4х4 = Цена за извършване на работа на работниците
             int [,] jobs =
             {
@@ -14,15 +19,34 @@ namespace _4._Jobs
                 { 5,8,1,8 },
                 { 7,6,9,4 }
             };
+
             // Пермутации на елементите
-            for (int j1 = 0; j1 < 4; j1++)
+            List<List<int>> permutations = new List<List<int>>();
+            for (int a = 0; a < MAX; a++)
+                for (int b = 0; b < MAX; b++)
+                    for (int c = 0; c < MAX; c++)
+                        for (int d = 0; d < MAX; d++)
+                            if (a != b && a != c && a != d && b != c && b != d && c != d)
+                            {
+                                var next = new List<int>();
+                                next.Add(jobs[a, 0]); // next.Add(a);
+                                next.Add(jobs[b, 1]); // next.Add(b);
+                                next.Add(jobs[c, 2]); // next.Add(c);
+                                next.Add(jobs[d, 3]); // next.Add(d);
+                                permutations.Add(next);
+                            }
+
+            // Отпечатване на пермутациите
+            foreach (var next in permutations)
             {
-                for (int w1 = 0; w1 < 4; w1++)
-                {
-                    Console.Write("{0} ", jobs[w1,j2]);
-                }
-                Console.WriteLine();
+                // НАмиране на сумата
+                int currentSum = next.Sum();
+                if (currentSum < minSum) minSum = currentSum;
+                // Отпечаване
+                Console.Write(string.Join(" + ", next));
+                Console.WriteLine(" = {0}$", currentSum);
             }
+            Console.WriteLine("Minimum value: {0}$", minSum);
         }
     }
 }
