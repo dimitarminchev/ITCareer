4f2bc13832f026feec9d59b63298c07e136e27b9
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-09-19 11:13:04 +0300

    19.09.2020

---
+++ 10. Algorithms and Data Structures/2020/3. Combinatorics/1. Combonatorics/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1._Combonatorics
+{
+    public static class Helper
+    {
+        /// <summary>
+        /// Разменяне на елементите в структура от данни
+        /// </summary>
+        /// <typeparam name="T">Тип на структурата данни</typeparam>
+        /// <param name="array">Структура данни</param>
+        /// <param name="first">Първи елемент</param>
+        /// <param name="second">Втори елемент</param>
+        public static void Swap<T>(T[] array, int first, int second)
+        {
+            var temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+    }
+}
---
+++ 10. Algorithms and Data Structures/2020/3. Combinatorics/1. Combonatorics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _1._Combonatorics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Структура данни
+            var data1 = new string[] { "A", "B", "C" };
+            var data2 = new int[] { 1, 2, 3 };
+
+            // Permutation = Пермутации = O(N!)
+            Console.WriteLine("1. Permutation without repetion ...");
+            Algo.PermuteNoRepeat(data1, 0); // index = 0
+            Algo.PermuteNoRepeat(data2, 0);
+
+            // Permutation with Repetition = Пермутации с повторения = O(N!)
+            Console.WriteLine("2. Permutation with Repetition ...");
+            Algo.PermuteRepeat(data1, 0); // index = 0
+            Algo.PermuteRepeat(data2, 0);
+
+            // Variation = Вариации = O(N! / (N - K)!)
+            Console.WriteLine("3. Variation ...");
+            Algo.Variations(data1, 2); // k = 2
+            Algo.Variations(data2, 2);
+
+            // Combination = Комбинации = O(N! / K!(N - K)!)
+            Console.WriteLine("4. Combination ...");
+            Algo.Combination(data1, 2); // k = 2
+            Algo.Combination(data2, 2);
+
+            // Combination with Repetition = Комбинации с повторения = O(N! / K!(N - K)!)
+
+            // Binomial Coefficients = Биномни коефициенти = C[n | k] = N! / (N - K)!K!
+
+        }
+    }
+}
