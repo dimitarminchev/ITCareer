01f3a14788dff4d72b46700fd535f1aa1cf87f68
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-09-19 16:21:00 +0300

    19.09.2020

---
+++ 10. Algorithms and Data Structures/2020/3. Combinatorics/2. ParmutationTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace _2._ParmutationTask
+{
+    class Program
+    {
+        /*
+        Изчислете броя на пермутациите за дадена стойност на N. 
+        Известно е, че броят на пермутациите е равен на факториела на N, 
+        т.е. N! = 1.2.3...N.  Рекурсивната дефиниция е N!=N*(N-1).
+         */
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // 7 -> 5040
+            BigInteger result = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 10. Algorithms and Data Structures/2020/3. Combinatorics/3. ZeroSum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._ZeroSum
+{
+    class Program
+    {
+        /*
+         Нека са дадени числата a1, a2, ..., an. 
+        Да се поставят операции "+" и "–" между числата ai и ai+1, 
+        за i = 1,2, …, n–1 така, че резултатът след пресмятане на 
+        получения израз да бъде равен на 0.
+        Например, ако са дадени естествените числа от 1 до 8, 
+        то няколко възможни решения на задачата са:
+        1 + 2 + 3 + 4 – 5 – 6 – 7 + 8 = 0
+        1 + 2 + 3 – 4 + 5 – 6 + 7 – 8 = 0
+        1 + 2 – 3 + 4 + 5 + 6 – 7 – 8 = 0
+        1 + 2 – 3 – 4 – 5 – 6 + 7 + 8 = 0
+        От клавиатурата се въвежда цяло число n,
+        броя на числата и след него n на брой числа.
+         */
+
+        // Применливи
+        static List<bool[]> sign = new List<bool[]>();
+        static bool[] currentSolution;
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            currentSolution = new bool[n];
+            Permute(n, 0);
+            foreach (var guess in sign)
+            {
+                var numGuess = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    numGuess[i] = (guess[i] ? -1 : 1) * numbers[i];
+                }
+                if (numGuess.Sum() == 0)
+                {
+                    Console.WriteLine($"{string.Join(" ", numGuess.Select(x => x > 0 ? $"+{x}" : $"{x}"))} = 0");
+                }
+            }
+        }
+
+        // Метод за генериране на пермутации
+        static void Permute(int n, int index)
+        {
+            if (n == index)
+            {
+                sign.Add(currentSolution.Clone() as bool[]);
+                return;
+            }
+
+            currentSolution[index] = true;
+            Permute(n, index + 1);
+            currentSolution[index] = false;
+            Permute(n, index + 1);
+        }
+    }
+}
