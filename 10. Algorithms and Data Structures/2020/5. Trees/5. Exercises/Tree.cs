2a97384560bd8e493b7d17b0419868d2406ef434
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-09-26 16:16:19 +0300

    29.09.2020

---
+++ 10. Algorithms and Data Structures/2020/5. Trees/5. Exercises/Tree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5._Exercises
+{
+    public class Tree
+    {
+        public int Value { get; private set; }
+        public Tree Parent { get; private set; }
+        public List<Tree> Children { get; private set; }
+
+        public Tree(int value, params Tree[] children)
+        {
+            this.Value = value;
+            if (children == null)
+            {
+                this.Children = new List<Tree>();
+            }
+            else
+            {
+                this.Children = children.ToList();
+                foreach (var child in children)
+                {
+                    child.Parent = this;
+                }
+            }
+        }
+
+        public void AddChild(Tree child)
+        {
+            Children.Add(child);
+        }
+
+        public void SetParent(Tree parent)
+        {
+            Parent = parent;
+        }
+
+        public static void Print(Tree node, int offset = 0)
+        {
+            Console.WriteLine($"{new string(' ', offset * 2)}{node.Value}");
+            foreach (var child in node.Children)
+            {
+                Print(child, offset + 1);
+            }
+        }
+    }
+}
---
+++ 10. Algorithms and Data Structures/2020/5. Trees/readme.md
@@ -0,0 +1,49 @@
+# 5. Excersises
+
+Input:
+```
+9
+7 19
+7 21
+7 14
+19 1
+19 12
+19 31
+14 23
+14 6
+27
+43
+```
+
+Output:
+```
+Root: 7
+
+Print tree:
+7
+  19
+    1
+    12
+    31
+  21
+  14
+    23
+    6
+
+Leaves: 1, 6, 12, 21, 23, 31
+
+Middle nodes: 14, 19
+
+Deepest node: 1
+
+Longest path: 7 19 1
+
+Paths with sum 27:
+7 19 1
+7 14 6
+
+SubTrees with sum 43:
+14
+  23
+  6
+```
