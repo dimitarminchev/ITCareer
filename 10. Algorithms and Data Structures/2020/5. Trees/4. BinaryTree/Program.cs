ee2e67e202a110f34ea527e26c5289a0737c0926
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-09-26 11:41:38 +0300

    26.09.2020

---
+++ 10. Algorithms and Data Structures/2020/5. Trees/4. BinaryTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4._BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Създаване на двоично дърво
+            BinaryTree tree = new BinaryTree();
+
+            // Добавяне на елементи към дървото
+            var items = new int[] { 17, 9, 6, 12, 19, 25 };
+            foreach (var item in items)
+            {
+                tree.Add(item);
+            }
+
+            // Отпечатване на елементите на вървото
+            BinaryTreePrinter.Print(tree.Root);
+        }
+    }
+}
