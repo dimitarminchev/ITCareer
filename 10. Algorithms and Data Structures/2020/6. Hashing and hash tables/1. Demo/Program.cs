51c663122f744e2aa832165998de5350ad68a3a0
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-09-27 10:26:20 +0300

    27.09.2020

---
+++ 10. Algorithms and Data Structures/2020/6. Hashing and hash tables/1. Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1._Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Създаваме три обекта инстанции на класа 
+            Point a = new Point(2, 3);
+            Point b = new Point(3, 2);
+            Point c = new Point(4, 4);
+
+            // Калкулираме и отпечатваме хеш на обектите
+            Console.WriteLine("Point {0}, Hash = {1}", a.ToString(), a.GetHashCode());
+            Console.WriteLine("Point {0}, Hash = {1}", b.ToString(), b.GetHashCode());
+            Console.WriteLine("Point {0}, Hash = {1}", c.ToString(), c.GetHashCode());
+        }
+    }
+}
