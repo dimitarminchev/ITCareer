616038f0b82ce7869ed7ab044b695f261a989f9b
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-09-27 11:31:42 +0300

    27.09.2020

--- 10. Algorithms and Data Structures/2020/6. Hashing and hash tables/HashTable/Program.cs
+++ 10. Algorithms and Data Structures/2020/6. Hashing and hash tables/HashTable/Program.cs
@@ -6,7 +6,27 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Task 13. Cointing Symbols
+
+            // Input
+            string symbols = Console.ReadLine();
+
+            // Process
+            var symbolCount = new HashTable<char, int>();
+            foreach (var symbol in symbols)
+            {
+                if (!symbolCount.ContainsKey(symbol))
+                {
+                    symbolCount.Add(symbol, 0);
+                }
+                symbolCount[symbol]++;
+            }
+
+            // Print
+            foreach (var item in symbolCount)
+            {
+                Console.WriteLine("{0}: {1} time/s", item.Key, item.Value);
+            }
         }
     }
 }
