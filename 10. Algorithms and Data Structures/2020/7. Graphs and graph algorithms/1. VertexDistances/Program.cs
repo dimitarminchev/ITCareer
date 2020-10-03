c5819321135cd90efec5057a87db232ce774ee0c
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-10-03 11:28:29 +0300

    03.10.2020

--- 10. Algorithms and Data Structures/2020/7. Graphs and graph algorithms/1. VertexDistances/Program.cs
+++ 10. Algorithms and Data Structures/2020/7. Graphs and graph algorithms/1. VertexDistances/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _1._VertexDistances
 {
     class Program
     {
+        static Dictionary<int, List<int>> nodes;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            nodes = new Dictionary<int, List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(new[] { ' ', ':' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                nodes[line[0]] = line.Skip(1).ToList();
+            }
+            var searchedPaths = new List<(int start, int finish)>();
+            for (int i = 0; i < m; i++)
+            {
+                var line = Console.ReadLine().Split(new[] { ' ', '-' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                searchedPaths.Add((line[0], line[1]));
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                currentSolution = new List<int>();
+                allSolutions = new List<List<int>>();
+                visited = new List<int>();
+                Solve(searchedPaths[i].start, searchedPaths[i].finish);
+                var min = (allSolutions.Count > 0) ? allSolutions.Select(x => x.Count).Min() : -1;
+                Console.WriteLine($"{{{searchedPaths[0].start},{searchedPaths[0].finish}}} -> {min}");
+            }
+
+        }
+
+        static List<int> currentSolution = new List<int>();
+        static List<List<int>> allSolutions = new List<List<int>>();
+        static List<int> visited = new List<int>();
+
+        public static void Solve(int start, int finish)
+        {
+            if (start == finish)
+            {
+                allSolutions.Add(currentSolution.ToList());
+                return;
+            }
+            var toVisit = nodes[start].Where(x => !visited.Contains(x));
+            foreach (var node in toVisit)
+            {
+                visited.Add(start);
+                currentSolution.Add(node);
+                Solve(node, finish);
+                currentSolution.Remove(node);
+                visited.Remove(start);
+            }
         }
     }
 }
