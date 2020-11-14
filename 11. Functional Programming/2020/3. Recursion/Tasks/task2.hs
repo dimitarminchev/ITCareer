394a3fd2e1c38243994957b357b4b80f2dc098b2
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-11-14 15:39:50 +0200

    2020

---
+++ 11. Functional Programming/2020/3. Recursion/Tasks/task2.hs
@@ -0,0 +1,16 @@
+-- Рекурсивна функция за намиране на Факториел
+findFactorial n initialValue index = 
+    if index > n
+    then initialValue
+    else findFactorial n (initialValue*index) (index+1)
+
+-- Помощна функция за намиране на Факториел
+factorial n = findFactorial n 1 1
+
+-- Главна функция
+main = do
+
+    -- Четем входни данни, обработваме ги и звеждаме резултата
+    line <- getLine
+    let n = read line :: Int
+    putStrLn(show(factorial n))
