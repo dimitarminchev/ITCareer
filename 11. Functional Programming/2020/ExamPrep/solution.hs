58967cfcd9881b89b1d00d71779df5bf02ad5d05
Dimitar Minchev <dimitar.minchev@gmail.com>
2020-11-21 10:27:44 +0200

    2020

--- 11. Functional Programming/2020/ExamPrep/solution.hs
+++ 11. Functional Programming/2020/ExamPrep/solution.hs
@@ -2,25 +2,25 @@ import System.Exit;
 
 -- Метод/функция за отпечатване на числата от 0 до 9
 printNumber number
-    | number == 0 do
+    | number == 0 = do
         putStrLn "Zero"
-    | number == 1 do
+    | number == 1 = do
         putStrLn "One"
-    | number == 2 do
+    | number == 2 = do
         putStrLn "Two"
-    | number == 3 do
+    | number == 3 = do
         putStrLn "Three"
-    | number == 4 do
+    | number == 4 = do
         putStrLn "Four"
-    | number == 5 do
+    | number == 5 = do
         putStrLn "Five"
-    | number == 6 do
+    | number == 6 = do
         putStrLn "Six"
-    | number == 7 do
+    | number == 7 = do
         putStrLn "Seven"
-    | number == 8 do
+    | number == 8 = do
         putStrLn "Eight"
-    | number == 9 do
+    | number == 9 = do
         putStrLn "Nine"
     | otherwise = do
         putStrLn "Please only enter single digit positive numbers"
