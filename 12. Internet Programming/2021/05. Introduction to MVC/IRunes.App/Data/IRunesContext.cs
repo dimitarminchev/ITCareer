d93c1add6e70c00a4adc3027514d2f7cefa7f1b8
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-01-23 18:27:51 +0200

    2021.01.23

---
+++ 12. Internet Programming/2021/05. Introduction to MVC/IRunes.App/Data/IRunesContext.cs
@@ -0,0 +1,20 @@
+﻿using IRunes.App.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRunes.App.Data
+{
+    public class IRunesContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string conn = @"Server=.\SQLEXPRESS; Databse=IRunnerDB; Trusted_Connection=True; Integrated Security=True;";
+            optionsBuilder.UseSqlServer(conn);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
