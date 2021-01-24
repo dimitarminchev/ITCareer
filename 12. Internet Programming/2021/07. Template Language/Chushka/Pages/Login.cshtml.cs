758b50a6a99059013161c397fe0f0541f46072e8
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-01-24 18:30:14 +0200

    2021.01.24

---
+++ 12. Internet Programming/2021/07. Template Language/Chushka/Pages/Login.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chushka.Pages
+{
+    public class LoginModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public LoginModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            // GET
+        }
+
+        public void OnPost()
+        {
+            // POST
+        }
+    }
+}
