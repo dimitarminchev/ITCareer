1e1aa5cae9e37f1d43bf2825d2cdf78bc4a18141
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-13 16:42:08 +0200

    13.02.2021

---
+++ 07. Software Development/2021/1. MVC/Task06/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task06.Controllers;
+
+namespace Task06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PoolController pool = new PoolController();
+        }
+    }
+}
---
+++ 07. Software Development/2021/1. MVC/Task06/Views/Display.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task06.Views
+{
+    public class Display
+    {
+        public int V { get; set; }
+        public int P1 { get; set; }
+        public int P2 { get; set; }
+        public double H { get; set; }
+        public string Response { get; set; }
+
+        public Display()
+        {
+            V = 0;
+            P1 = 0;
+            P2 = 0;
+            H = 0;
+        }
+
+        public void Input()
+        {
+            Console.Write("V = ");
+            V = int.Parse(Console.ReadLine());
+            Console.Write("P1 = ");
+            P1 = int.Parse(Console.ReadLine());
+            Console.Write("P2 = ");
+            P2 = int.Parse(Console.ReadLine());
+            Console.Write("H = ");
+            H = double.Parse(Console.ReadLine());
+        }
+
+        public void ShowResponse()
+        {
+            Console.WriteLine(Response);
+        }
+    }
+}
