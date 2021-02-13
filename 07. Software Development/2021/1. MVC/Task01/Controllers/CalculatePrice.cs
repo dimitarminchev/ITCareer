c23f7c86f00d3d3ae3e704c0a73703e18344319a
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-13 16:11:05 +0200

    14.02.2021

---
+++ 07. Software Development/2021/1. MVC/Task01/Controllers/CalculatePrice.cs
@@ -0,0 +1,19 @@
+﻿using Task01.Model;
+using Task01.Views;
+
+namespace Task01.Controllers
+{
+    public class CalculatePrice
+    {
+        private Transport transport;
+        private Display display;
+
+        public CalculatePrice()
+        {
+            display = new Display();
+            transport = new Transport(display.kilometers, display.time);
+            display.totalPrice = transport.CalculatePrice();
+            display.ShowCheapestWayToTravel();
+        }
+    }
+}
---
+++ 07. Software Development/2021/1. MVC/Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task01.Controllers;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatePrice cp = new CalculatePrice();
+        }
+    }
+}
