79e6efadf3a1fe4e0aea7e87351641c03c2df55f
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-13 16:47:12 +0200

    13.02.2021

---
+++ 07. Software Development/2021/1. MVC/Task07/Controllers/Controller.cs
@@ -0,0 +1,21 @@
+﻿using Task07.Views;
+using Task07.Models;
+
+namespace Task07.Controllers
+{
+    public class Controller
+    {
+        private Display display;
+        private Model model;
+
+        public Controller()
+        {
+            display = new Display();
+            model = new Model(display.budget, display.season);
+            display.place = model.DefinePlace();
+            display.expenses = model.CalculateExpenses();
+            display.facility = model.Facility;
+            display.ShowResult();
+        }
+    }
+}
---
+++ 07. Software Development/2021/1. MVC/Task07/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task07.Controllers;
+
+namespace Task07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controler = new Controller();
+        }
+    }
+}
