e61b46b52c75ce3edf01cd841a22ed1e5bc8f4c7
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-13 16:23:23 +0200

    13.02.2021

---
+++ 07. Software Development/2021/1. MVC/Task02/Controllers/ExamController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Task02.Model;
+using Task02.Views;
+
+namespace Task02.Controllers
+{
+    public class ExamController
+    {
+        private Display display;
+        private Exam exam;
+
+        public ExamController()
+        {
+            display = new Display();
+            while (exam is null)
+            {
+                try
+                {
+                    display.Input();
+                    exam = new Exam
+                    (
+                        display.StartHour,
+                        display.StartMinutes,
+                        display.ArrivalHour,
+                        display.ArrivalMinutes
+                    );
+                }
+                catch (Exception e)
+                {
+                    display.Response = e.Message;
+                    display.ShowResponse();
+                }
+            }
+            display.Response = exam.Calculate();
+            display.ShowResponse();
+        }
+    }
+}
---
+++ 07. Software Development/2021/1. MVC/Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task02.Controllers;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExamController ec = new ExamController();
+        }
+    }
+}
