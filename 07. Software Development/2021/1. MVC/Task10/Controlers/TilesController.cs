2d4e0d2af8b325fc43ec97a047cdc09ef4a58647
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-13 17:00:33 +0200

    13.02.2021

---
+++ 07. Software Development/2021/1. MVC/Task10/Controlers/TilesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Task10.Models;
+using Task10.Views;
+
+namespace Task10.Controlers
+{
+    public class TilesController
+    {
+        private Display display;
+        private Tiles tiles;
+
+        public TilesController()
+        {
+            display = new Display();
+            while (tiles is null)
+            {
+                try
+                {
+                    display.Input();
+                    tiles = new Tiles(display.N, display.W, display.L, display.M, display.O);
+                }
+                catch (Exception e)
+                {
+                    display.Response = e.Message;
+                    display.ShowResponse();
+                }
+            }
+            display.Response = tiles.Calculate();
+            display.ShowResponse();
+        }
+    }
+}
---
+++ 07. Software Development/2021/1. MVC/Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task10.Controlers;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TilesController tiles = new TilesController();
+        }
+    }
+}
