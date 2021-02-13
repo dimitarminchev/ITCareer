afa4c3f0be2768bee19a0402abd8e77c661018ce
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-13 16:38:30 +0200

    13.02.2021

---
+++ 07. Software Development/2021/1. MVC/Task05/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task05.Controllers;
+
+namespace Task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controler = new Controller();
+        }
+    }
+}
