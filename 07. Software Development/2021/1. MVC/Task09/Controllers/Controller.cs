35ec90fd47fa42bc5d18fb876896cba7e7dd2541
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-13 16:55:49 +0200

    13.02.2021

---
+++ 07. Software Development/2021/1. MVC/Task09/Controllers/Controller.cs
@@ -0,0 +1,19 @@
+﻿using Task09.Models;
+using Task09.Views;
+
+namespace Task09.Controllers
+{
+    public class Controller
+    {
+        public Display display;
+        public Model model;
+
+        public Controller()
+        {
+            display = new Display();
+            model = new Model(display.magicNumber);
+            display.magicNumbers = model.generateCombinations();
+            display.ShowResults();
+        }
+    }
+}
---
+++ 07. Software Development/2021/1. MVC/Task09/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task09.Controllers;
+
+namespace Task09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller magic = new Controller();
+        }
+    }
+}
