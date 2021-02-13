26215e1c971e2879d39734cbdda349ee62dae9de
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-13 11:48:56 +0200

    13.02.2021

---
+++ 07. Software Development/2021/1. MVC/ConsoleMVC/Controllers/TipCalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleMVC.Model;
+using ConsoleMVC.Views;
+
+namespace ConsoleMVC.Controllers
+{
+    public class TipCalculatorController
+    {
+        /// <summary>
+        /// Модел
+        /// </summary>
+        private Tip tip;
+
+        /// <summary>
+        /// Изглед
+        /// </summary>
+        private Display display;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public TipCalculatorController()
+        {
+            display = new Display();
+            tip = new Tip(display.Amount, display.Percent);
+            display.TipAmount = tip.CalculateTip();
+            display.Total = tip.CalculateTotal();
+            display.ShowTipAndTotal();
+        }
+    }
+}
---
+++ 07. Software Development/2021/1. MVC/ConsoleMVC/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleMVC.Controllers;
+
+namespace ConsoleMVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Контролер
+            TipCalculatorController t = new TipCalculatorController();
+        }
+    }
+}
