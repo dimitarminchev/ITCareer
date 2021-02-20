7245bfe57cc3915f165431c427618c15af71fcf1
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-20 16:16:30 +0200

    20.02.2021

---
+++ 07. Software Development/2021/3. Debugging/02_InstructionSet/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _02_InstructionSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+/* Input:
+INC 0
+ADD 1323134 421315521
+DEC 57314183
+MLA 252621 324532
+END
+*/
+            string opCode = System.Console.ReadLine();
+            while (opCode != "END")
+            {
+                string[] codeArgs = opCode.Split(' ');
+
+                long result = 0;
+                switch (codeArgs[0])
+                {
+                    case "INC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]); // fix: int => long
+                            result = operandOne + 1; // fix: operandOne++ => operandOne + 1
+                            break;
+                        }
+                    case "DEC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]); // fix: int => long
+                            result = operandOne - 1; // fix: operandOne-- -> operandOne - 1
+                            break;
+                        }
+                    case "ADD":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]); // fix: int => long
+                            long operandTwo = long.Parse(codeArgs[2]); // fix: int => long
+                            result = operandOne + operandTwo;
+                            break;
+                        }
+                    case "MLA":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]); // fix: int => long
+                            long operandTwo = long.Parse(codeArgs[2]); // fix: int => long
+                            result = (long)(operandOne * operandTwo);
+                            break;
+                        }
+                }
+
+                Console.WriteLine(result);
+                opCode = System.Console.ReadLine(); // fix: mising readline
+            }
+        }
+    }
+}
