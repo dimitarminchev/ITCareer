d6697827a862e9b29b488f3bb353f99eadd0163d
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-20 18:40:14 +0200

    20.02.2021

---
+++ 07. Software Development/2021/3. Debugging/06_BitCarousel/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _06_BitCarousel
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte number = byte.Parse(System.Console.ReadLine());
+            byte rotations = byte.Parse(System.Console.ReadLine());
+
+            for (int i = 0; i < rotations; i++)
+            {
+/* Input:
+45
+3
+left
+right
+left
+*/
+                string direction = System.Console.ReadLine();
+                if (direction == "right")
+                {
+                                                         // 00011011 = 27
+                    int rightMostBit = number & 1;       // 00000001 = 1
+                    number >>= 1;                        // 00001101 = 13
+                    number |= (byte)(rightMostBit << 5); // 00101101 = 45
+                }
+                else if (direction == "left")
+                {
+                                                         // 00101101 = 45
+                    int leftMostBit = (number >> 5) & 1; // 00000001 =  1
+                    number <<= 1;                        // 01011010 = 90 
+                    number |= (byte)leftMostBit;         // 01011011 = 91
+                    number ^= (byte)(leftMostBit << 6);  // 00011011 = 27
+
+                }
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
