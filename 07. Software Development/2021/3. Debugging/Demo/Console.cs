0f9d9bd537b12b3de262f259c3efa4c499bf82cc
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-20 11:40:28 +0200

    20.02.2021

---
+++ 07. Software Development/2021/3. Debugging/Demo/Console.cs
@@ -0,0 +1,37 @@
+﻿namespace Demo
+{
+    internal static class Console
+    {
+        /// <summary>
+        /// Метод за отпечатване
+        /// </summary>
+        /// <param name="message">Съобщение</param>
+        public static void WriteLine(object message)
+        {
+            // Отпечатваме съобщение
+            System.Console.WriteLine(message);
+        }
+
+        /// <summary>
+        /// Метод за негативно отпечатване
+        /// </summary>
+        /// <param name="message">Съобщение</param>
+        public static void WriteNegative(object message)
+        {
+            // Текущи стойности на цвета на конзолята
+            var bc = System.Console.BackgroundColor;
+            var fc = System.Console.ForegroundColor;
+
+            // Смяна на цвета на конзолата
+            System.Console.BackgroundColor = System.ConsoleColor.Green;
+            System.Console.ForegroundColor = System.ConsoleColor.Black;
+
+            // Отпечатваме съобщение
+            System.Console.WriteLine(message);
+
+            // Връщане на оригиналните настройки
+            System.Console.BackgroundColor = bc;
+            System.Console.ForegroundColor = fc;
+        }
+    }
+}
---
+++ 07. Software Development/2021/3. Debugging/Demo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Коректен начин да парснем число
+                // while (true)
+                {
+                    var fourtyTwo = int.Parse("42");
+                    Console.WriteLine(fourtyTwo);
+                }
+
+                // Некоректен начин да парснем число
+                var thirtyTwo = int.Parse("thirty two");
+                Console.WriteLine(thirtyTwo);
+
+                // Опит за хвърляне на изключение
+                throw new Exception("Exception test.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteNegative(ex.Message);
+            }
+            finally
+            {
+                System.Console.WriteLine("The End");
+            }
+        }
+    }
+}
