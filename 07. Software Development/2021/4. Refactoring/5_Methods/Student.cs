2c6b33e2c9f84e29bff2c036199a8996534b526c
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-21 17:19:40 +0200

    21.02.2021

---
+++ 07. Software Development/2021/4. Refactoring/5_Methods/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Methods
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string OtherInfo { get; set; }
+
+        public bool IsOlderThan(Student other)
+        {
+            DateTime firstDate =
+                DateTime.Parse(this.OtherInfo.Substring(this.OtherInfo.Length - 10));
+            DateTime secondDate =
+                DateTime.Parse(other.OtherInfo.Substring(other.OtherInfo.Length - 10));
+            return firstDate > secondDate;
+        }
+    }
+}
