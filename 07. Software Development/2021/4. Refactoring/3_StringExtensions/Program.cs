cc9ef88c060a24b2ea703df75f11fb76539e47c9
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-02-21 16:48:35 +0200

    21.02.2021

---
+++ 07. Software Development/2021/4. Refactoring/3_StringExtensions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3_StringExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            Console.WriteLine("Please Enter a String:");
+            var str = Console.ReadLine();
+
+            // StringExtention Demo
+            Console.WriteLine("MD5: {0}", StringExtensions.ToMd5Hash(str));
+            Console.WriteLine("Bool: {0}", StringExtensions.ToBoolean(str));
+            Console.WriteLine("Int: {0}", StringExtensions.ToInteger(str));
+            Console.WriteLine("Long: {0}", StringExtensions.ToLong(str));
+        }
+    }
+}
