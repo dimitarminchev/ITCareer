deca8f7b103780e32e46cf54b6f116edd4c4691b
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-03-06 18:49:35 +0200

    06.03.2021

---
+++ 07. Software Development/2021/7. Databases/05.ChangeRegister/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _05.ChangeRegister
+{
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Towns
+            var towns = new Dictionary<int, string>();
+
+            // Input
+            Console.Write("Enter country: ");
+            var countryName = Console.ReadLine();
+
+            // Process
+            using (var conn = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;Database=minions;Integrated Security=true"))
+            {
+                conn.Open();
+
+                // Find Country Id
+                SqlCommand sql = new SqlCommand
+                (
+                    "SELECT TOP 1 countries.id FROM countries " +
+                   $"WHERE countries.name = @countryName", conn
+                );
+                sql.Parameters.AddWithValue("@countryName", countryName);
+                int countryId = int.Parse(sql.ExecuteScalar().ToString());
+
+                // Find All Towns for selected Country
+                sql = new SqlCommand
+                (
+                    "SELECT towns.id, towns.name FROM towns" +
+                   $" WHERE towns.CountryCode = @countryId", conn
+                );
+                sql.Parameters.AddWithValue("@countryId", countryId);
+                var reader = sql.ExecuteReader();
+                while (reader.Read())
+                {
+                    towns.Add(int.Parse(reader[0].ToString()), reader[1].ToString().ToUpper());
+                }
+                reader.Close();
+
+                // Update the Namesof all Towns for selected Country
+                foreach (var town in towns)
+                {
+                    sql = new SqlCommand("UPDATE towns SET name = @name WHERE id=@id", conn);
+                    sql.Parameters.AddWithValue("@name", town.Value);
+                    sql.Parameters.AddWithValue("@id", town.Key);
+                    sql.ExecuteNonQuery();
+                }
+
+                // Finally print some information for the user
+                if (towns.Count() > 0)
+                {
+                    Console.WriteLine($"{towns.Count()} town names were affected.");
+                    Console.WriteLine("[" + string.Join(", ", towns.Values) + "]");
+                }
+                else
+                {
+                    Console.WriteLine("No town names were affected.");
+                }
+            }
+        }
+    }
+}
---
+++ 07. Software Development/2021/7. Databases/08.AgeIncrease/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _08.AgeIncrease
+{
+    using System.Data.SqlClient;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            Console.Write("Enter minions' id: ");
+            var minionsIds = Console.ReadLine().Split().ToArray();
+
+            using (var conn = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;Database=minions;Integrated Security=true"))
+            {
+                conn.Open();
+
+                // Process All Minnions
+                foreach(var minionId in minionsIds)
+                {
+                    // Get Minion Age
+                    var commandAge = new SqlCommand("SELECT age FROM minions WHERE minions.id = @id", conn);
+                    commandAge.Parameters.AddWithValue("@id", minionId);
+                    var minionAge = int.Parse(commandAge.ExecuteScalar().ToString());
+
+                    // Select Minion By Id
+                    var commandName = new SqlCommand("SELECT name FROM minions  where minions.id = @id", conn);
+                    commandName.Parameters.AddWithValue("@id", minionId);
+                    var name = commandName.ExecuteScalar().ToString();
+                    name = name.First().ToString().ToUpper() + name.Substring(1);
+
+                    // Update Minion
+                    var commandUpdate = new SqlCommand("UPDATE minions SET name = @name, age = @age+1 WHERE minions.id = @id", conn);
+                    commandUpdate.Parameters.AddWithValue("@id", minionId);
+                    commandUpdate.Parameters.AddWithValue("@age", minionAge);
+                    commandUpdate.Parameters.AddWithValue("@name", name);
+                    commandUpdate.ExecuteNonQuery();
+                }
+
+                // Print All Minions Names
+                SqlCommand commandPrint = new SqlCommand("SELECT name,age FROM minions", conn);
+                SqlDataReader reader = commandPrint.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("Name: {0}, Age: {1}", reader[0], reader[1]);
+                }
+
+                // CleanUp
+                reader.Close();
+                conn.Close();
+            }
+        }
+    }
+}
