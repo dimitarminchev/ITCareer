5f51001930a34c42502f41f6ee88602fc85e42c0
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-03-06 11:38:07 +0200

    06.03.2021

--- 07. Software Development/2021/7. Databases/MYSQL Demo/Program.cs
+++ 07. Software Development/2021/7. Databases/MYSQL Demo/Program.cs
@@ -1,47 +1,46 @@
-﻿using System;
-using System.Data.SqlClient;
+﻿using MySql.Data.MySqlClient;
+using System;
 
-namespace MSSQL_Demo
+namespace MYSQL_Demo
 {
     class Program
     {
         static void Main(string[] args)
         {
             // Заглавие на конзолното приложение
-            Console.WriteLine("MSSQL Demo 1.0");
-            Console.WriteLine(new string('-',25));
+            Console.WriteLine("MySQL Demo 1.0");
+            Console.WriteLine(new string('-', 25));
 
-            // 1. Отваряне на връзка към MSSQL база данни
-            SqlConnection dbConn = new SqlConnection
+            // 1. Отваряне на връзка към MySQL база данни
+            MySqlConnection dbConn = new MySqlConnection
             (
-                "Server=(localdb)\\ProjectsV13;" +
-                "Database=minions;Integrated Security=true"
-             );
+                "server=localhost;port=3306;user=root;password=root;database=minions"
+            );
             dbConn.Open();
             using (dbConn)
             {
                 // 2. Създаване на таблица 
-                //SqlCommand command = new SqlCommand
+                //MySqlCommand command = new MySqlCommand
                 //(
                 //    "CREATE TABLE minions (id INT, name VARCHAR(50), age INT)", dbConn
                 //);
                 //command.ExecuteNonQuery();
 
                 // 3. Добавяне на записи в таблицата
-                //SqlCommand command = new SqlCommand
+                //MySqlCommand command = new MySqlCommand
                 //(
                 //    "INSERT INTO minions (id, name, age) VALUES ('1', 'Kevin', '15');" +
                 //    "INSERT INTO minions (id, name, age) VALUES ('2', 'Bob', '22');" +
-                //    "INSERT INTO minions (id, name, age) VALUES ('3', 'Steward', '42');",dbConn
+                //    "INSERT INTO minions (id, name, age) VALUES ('3', 'Steward', '42');", dbConn
                 //);
                 //command.ExecuteNonQuery();
 
                 // 4. Извеждане на записите от таблицата
-                SqlCommand command = new SqlCommand
+                MySqlCommand command = new MySqlCommand
                 (
                     "SELECT name,age FROM minions;", dbConn
                 );
-                SqlDataReader reader = command.ExecuteReader();
+                MySqlDataReader reader = command.ExecuteReader();
                 while (reader.Read())
                 {
                     Console.WriteLine("Name: {0}, Age: {1}", reader[0], reader[1]);
---
+++ 07. Software Development/2021/7. Databases/MYSQL Demo/database.sql
@@ -0,0 +1,10 @@
+﻿/* 1. Създаване на таблица */
+CREATE TABLE minions (id INT, name VARCHAR(50), age INT);
+
+/* 2. Добавяне на записи в таблицата */
+INSERT INTO minions (id, name, age) VALUES ('1', 'Kevin', '15');
+INSERT INTO minions (id, name, age) VALUES ('2', 'Bob', '22')
+INSERT INTO minions (id, name, age) VALUES ('3', 'Steward', '42')
+
+/* 3. Извеждане на записите от таблицата */
+SELECT name,age FROM minions;
