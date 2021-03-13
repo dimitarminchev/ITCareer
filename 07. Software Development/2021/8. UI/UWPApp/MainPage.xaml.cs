7a82c630c7ca48484a8d0aaefa88ad3532d0484c
Dimitar Minchev <dimitar.minchev@gmail.com>
2021-03-13 18:47:45 +0200

    13.03.2021

---
+++ 07. Software Development/2021/8. UI/UWPApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Контекста на базата данни
+        private ProductBusiness productBusiness = new ProductBusiness();
+
+        // Конструктор
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // Запис
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Product product = new Product()
+            {
+                Name = name.Text,
+                Price = decimal.Parse(price.Text),
+                Stock = int.Parse(stock.Text)
+            };
+            productBusiness.Add(product);
+        }
+    }
+}
