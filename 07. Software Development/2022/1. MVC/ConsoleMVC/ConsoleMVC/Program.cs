a4d6a05b5378755aaaa172f48416772deb917219
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-01-31 10:47:25 +0200

    ConsoleMVC

---
+++ 07. Software Development/2022/1. MVC/ConsoleMVC/ConsoleMVC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleMVC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
