849d052fde11e63b068130ca1055718546079ceb
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-01-31 15:46:58 +0200

    4. PasswordGenerator

---
+++ 07. Software Development/2022/1. MVC/4. PasswordGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _4._PasswordGenerator
+{
+    using _4._PasswordGenerator.Controllers;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+        }
+    }
+}
---
+++ 07. Software Development/2022/1. MVC/4. PasswordGenerator/View/View.cs
@@ -0,0 +1,28 @@
+﻿namespace _4._PasswordGenerator.View
+{
+    internal class View
+    {
+        public int n { get; set; }
+        public int l { get; set; }
+        public string Response { get; set; }
+
+        public View()
+        {
+            n = 0;
+            l = 0;
+        }
+
+        public void Input()
+        {
+            Console.Write("n = ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("l = ");
+            l = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowResponse()
+        {
+            Console.WriteLine(Response);
+        }
+    }
+}
