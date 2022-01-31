3d02d26ba8036b6478fbe1de8ef3b91403153d1a
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-01-31 15:39:59 +0200

    3. Histogram

---
+++ 07. Software Development/2022/1. MVC/3. Histogram/Controllers/Controller.cs
@@ -0,0 +1,21 @@
+﻿namespace _3._Histogram.Controllers
+{
+    using _3._Histogram.Model;
+    using _3._Histogram.Display;
+
+    internal class Controller
+    {
+        private Display display;
+        private Model model;
+
+        public Controller()
+        {
+            display = new Display();
+            model = new Model(display.count, display.numbers);
+
+            display.percents = model.FindPercentages();
+
+            display.ShowResults();
+        }
+    }
+}
---
+++ 07. Software Development/2022/1. MVC/3. Histogram/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _3._Histogram
+{
+    using _3._Histogram.Controllers;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+        }
+    }
+}
