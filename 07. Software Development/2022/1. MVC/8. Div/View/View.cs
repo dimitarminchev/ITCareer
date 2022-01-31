a3434e1d6d1557a032998c1d68d666aea1b02a12
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-01-31 16:16:38 +0200

    8. Div

---
+++ 07. Software Development/2022/1. MVC/8. Div/View/View.cs
@@ -0,0 +1,37 @@
+﻿namespace _8._Div.View
+{
+    internal class View
+    {
+        public int n { get; set; }
+
+        public List<int> numbers { get; set; }
+
+        public string Response { get; set; }
+
+        public View()
+        {
+            n = 0;
+            numbers = new List<int>();
+        }
+
+        public void Input()
+        {
+            while (n < 1 || n > 1000)
+            {
+                Console.Write("n = ");
+                n = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"n[{i + 1}] = ");
+                var num = int.Parse(Console.ReadLine());
+                numbers.Add(num);
+            }
+        }
+
+        public void ShowResponse()
+        {
+            Console.WriteLine(Response);
+        }
+    }
+}
