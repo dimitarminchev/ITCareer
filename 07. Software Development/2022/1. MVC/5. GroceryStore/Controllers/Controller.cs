ad8b82a5426da3cca537be79a3bf321ec65186c8
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-01-31 15:55:20 +0200

    5. GroceryStore

---
+++ 07. Software Development/2022/1. MVC/5. GroceryStore/Controllers/Controller.cs
@@ -0,0 +1,19 @@
+﻿namespace _5._GroceryStore.Controllers
+{
+    using _5._GroceryStore.View;
+    using _5._GroceryStore.Model;
+
+    internal class Controller
+    {
+        private View view;
+        private Model model;
+
+        public Controller()
+        {
+            view = new View();
+            model = new Model(view.priceOfVegetables, view.priceOfFruit, view.kilosVegetables, view.kilosFruits);
+            view.total = model.Calculate();
+            view.ShowResult();
+        }
+    }
+}
---
+++ 07. Software Development/2022/1. MVC/5. GroceryStore/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _5._GroceryStore
+{
+    using _5._GroceryStore.Controllers;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+        }
+    }
+}
