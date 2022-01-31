fd5a0faff0627c6f4a63cc4f4e720acdc8f4d13b
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-01-31 16:02:13 +0200

    6. Pool

---
+++ 07. Software Development/2022/1. MVC/6. Pool/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _6._Pool
+{
+    using _6._Pool.Controllers;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+        }
+    }
+}
