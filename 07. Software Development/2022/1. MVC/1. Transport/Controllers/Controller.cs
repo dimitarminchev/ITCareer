a3b6cd619f9b3655652ba54f8be8706ffbe48b1a
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-01-31 15:16:21 +0200

    1. Transport

---
+++ 07. Software Development/2022/1. MVC/1. Transport/Controllers/Controller.cs
@@ -0,0 +1,19 @@
+﻿namespace _1._Transport.Controllers
+{
+    using _1._Transport.Model;
+    using _1._Transport.Display;
+
+    internal class Controller
+    {
+        private Display display;
+        private Model model;
+
+        public Controller()
+        {
+            display = new Display();
+            model = new Model(display.kilometers, display.time);
+            display.totalPrice = model.CalculatePrice();
+            display.ShowCheapestWayToTravel();
+        }
+    }
+}
---
+++ 07. Software Development/2022/1. MVC/1. Transport/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _1._Transport
+{
+    using _1._Transport.Controllers;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+        }
+    }
+}
