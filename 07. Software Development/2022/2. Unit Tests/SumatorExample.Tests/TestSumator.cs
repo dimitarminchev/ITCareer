e93a79e4468fb84835911e957a3a5e744b57b030
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-02-02 14:10:52 +0200

    Sumator Example

---
+++ 07. Software Development/2022/2. Unit Tests/SumatorExample.Tests/TestSumator.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SumatorExample.Tests
+{
+    [TestClass]
+    public class TestSumator
+    {
+        [TestMethod]
+        public void TestSumIfOnePlusTwoEqualsThree()
+        {
+            // Arrange & Act
+            var sum = Sumator.Sum(new int[] { 1, 2 });
+
+            // Assert
+            Assert.AreEqual(3, sum, "1+2 != 3");
+        }
+
+        [TestMethod]
+        public void TestSumIfMinus2isEqualMinus2()
+        {
+            // Arrange & Act
+            var sum = Sumator.Sum(new int[] { -2 });
+
+            // Assert
+            Assert.AreEqual(-2, sum, "-2 != -2");
+        }
+
+        [TestMethod]
+        public void TestSumWithEmptyArrayToGiveZero()
+        {
+            // Arrange & Act
+            var sum = Sumator.Sum(new int[] { });
+
+            // Assert
+            Assert.AreEqual(0, sum, "0 != 0");
+        }
+    }
+}
