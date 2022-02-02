018bb744ff4eff2da55a6d867d2297659cd8452e
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-02-02 09:42:35 +0200

    Division Example

---
+++ 07. Software Development/2022/2. Unit Tests/DivisionExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DivisionExample 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Divider div = new Divider();
+
+            // Ръчен тест
+            Console.WriteLine("4/2={0}", div.Divide(4,2));
+        }
+    }
+}
