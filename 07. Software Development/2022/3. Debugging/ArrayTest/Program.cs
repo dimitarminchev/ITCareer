0b984c94890ce5c3a84bb609dd4a563a3c832fd2
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-02-03 15:07:00 +0200

    ArrayTest

---
+++ 07. Software Development/2022/3. Debugging/ArrayTest/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace ArrayTest
+{
+    using System.Linq;
+
+    internal class Program
+    {
+        private const char ArgumentsDelimiter = ' ';
+
+        public static void Main()
+        {
+            int sizeOfArray = int.Parse(System.Console.ReadLine());
+            long[] array = new long[sizeOfArray];
+            array = System.Console.ReadLine()
+                .Split(ArgumentsDelimiter)
+                .Select(long.Parse)
+                .ToArray();
+
+            string[] command = System.Console.ReadLine().Split(' ');
+
+            while (!command[0].Equals("stop"))
+            {
+                int[] args = new int[2];
+                if (command[0].Equals("add") ||
+                    command[0].Equals("subtract") ||
+                    command[0].Equals("multiply"))
+                {
+                    args[0] = int.Parse(command[1]);
+                    args[1] = int.Parse(command[2]);
+
+                    array = PerformAction(array, command[0].ToString(), args);
+                }
+                else
+                {
+                    array = PerformAction(array, command[0], args);
+                }
+
+                PrintArray(array);
+
+                command = System.Console.ReadLine().Split(" ");
+            }
+        }
+
+        static long[] PerformAction(long[] arr, string action, int[] args)
+        {
+            long[] array = arr.Clone() as long[];
+            int pos = args[0] - 1;
+            int value = args[1];
+
+            switch (action)
+            {
+                case "multiply":
+                    array[pos] *= value;
+                    break;
+                case "add":
+                    array[pos] += value;
+                    break;
+                case "subtract":
+                    array[pos] -= value;
+                    break;
+                case "lshift":
+                    ArrayShiftLeft(array);
+                    break;
+                case "rshift":
+                    ArrayShiftRight(array);
+                    break;
+            }
+            return array;
+        }
+
+        private static void ArrayShiftRight(long[] array)
+        {
+            for (int i = array.Length - 1; i >= 1; i--)
+            {
+                array[i] = array[i - 1];
+            }
+        }
+
+        private static void ArrayShiftLeft(long[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+        }
+
+        private static void PrintArray(long[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
