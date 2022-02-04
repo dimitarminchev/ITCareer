4c618f718906072b0da35d304f9c2e6b09190c39
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-02-04 14:54:14 +0200

    StringExtentions

---
+++ 07. Software Development/2022/4. Refactoring/3_StringExtensions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _3_StringExtensions
+{
+    /// <summary>
+    /// String Extensions main program class.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// String Extensions main program method.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Input
+            Console.WriteLine("Please Enter a String:");
+            var str = Console.ReadLine();
+
+            // Demo
+            Console.WriteLine("MD5: {0}", StringExtensions.ToMd5Hash(str));
+            Console.WriteLine("Bool: {0}", StringExtensions.ToBoolean(str));
+            Console.WriteLine("Int: {0}", StringExtensions.ToInteger(str));
+            Console.WriteLine("Long: {0}", StringExtensions.ToLong(str));
+        }
+    }
+}
