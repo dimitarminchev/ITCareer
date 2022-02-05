835a230ec2cceb6f7676e21d511a12f348d2aaed
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-02-05 14:33:29 +0200

    InheritanceAndPolymorphism

---
+++ 07. Software Development/2022/4. Refactoring/8_InheritanceAndPolymorphism/Program.cs
@@ -0,0 +1,44 @@
+﻿/// <summary>
+/// Refactoring Task "InheritanceAndPolymorphism" Namespace.
+/// </summary>
+namespace InheritanceAndPolymorphism
+{
+    using InheritanceAndPolymorphism.Courses;
+
+    /// <summary>
+    /// Refactoring Task "InheritanceAndPolymorphism" program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Refactoring Task "InheritanceAndPolymorphism" main program method.
+        /// </summary>
+        /// <param name="args">Given arguments</param>
+        public static void Main(string[] args)
+        {
+            // LocalCourse
+            LocalCourse localCourse = new LocalCourse("Databases");
+            Console.WriteLine(localCourse);
+
+            localCourse.Lab = "Enterprise";
+            Console.WriteLine(localCourse);
+
+            localCourse.Students = new List<string>() { "Peter", "Maria" };
+            Console.WriteLine(localCourse);
+
+            localCourse.TeacherName = "Svetlin Nakov";
+            localCourse.Students.Add("Milena");
+            localCourse.Students.Add("Todor");
+            Console.WriteLine(localCourse);
+
+            // OffsiteCourse
+            OffsiteCourse offsiteCourse = new OffsiteCourse
+            (
+                "PHP and WordPress Development", // Course Name
+                "Mario Peshev", // Teacher name
+                new List<string>() { "Thomas", "Ani", "Steve" } // Students
+            );
+            Console.WriteLine(offsiteCourse);
+        }
+    }
+}
