11aa42c79ffc6a0a4126e7f3f515eda3c0daaa89
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-02-06 09:50:26 +0200

    IDE

---
+++ 07. Software Development/2022/5. IDE/Person/BankAccount.cs
@@ -0,0 +1,56 @@
+﻿/// <summary>
+/// Integrated Development Environment: "Person" Namespace
+/// </summary>
+namespace Person
+{
+    /// <summary>
+    /// Integrated Development Environment: "Bank Account" Class
+    /// </summary>
+    public class BankAccount
+    {
+        private int id;
+
+        /// <summary>
+        /// Bank Account Identification Property
+        /// </summary>
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Value must be positive number.");
+                }
+                id = value;
+            }
+        }
+
+        private double balance;
+
+        /// <summary>
+        /// Bank Account Balance
+        /// </summary>
+        public double Balance
+        {
+            get { return balance; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Value must be positive number.");
+                }
+                balance = value;
+            }
+        }
+
+        /// <summary>
+        /// Bank Account Constructor setting: Id and Balance
+        /// </summary>
+        public BankAccount(int id, double balance)
+        {
+            this.Id = id;
+            this.Balance = balance;
+        }
+    }
+}
