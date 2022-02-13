c1752eb301318eab8f0ffd848b7dbf2c2e203b50
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-02-13 09:28:29 +0200

    Optical Character Recognition

---
+++ 07. Software Development/2022/6. NuGet/OpticalCharacterRecognition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using Tesseract;
+
+namespace OpticalCharacterRecognition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Unicode encoding
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Load image to recognize
+            string filePath = AppContext.BaseDirectory;
+            string fileName = filePath + "tessdata\\picture.png";
+
+            // Process Optical Character Recognition (OCR) ...
+            using (var engine = new TesseractEngine(@"tessdata", "bul"))
+            {
+                using (var image = Pix.LoadFromFile(fileName))
+                {
+                    using (var page = engine.Process(image))
+                    {
+                        string text = page.GetText();
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+
+            // Pause
+            var key = Console.ReadKey();
+        }
+    }
+}
