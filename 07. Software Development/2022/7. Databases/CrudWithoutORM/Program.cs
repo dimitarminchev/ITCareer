1b6858ad0a9ed546a7879e61d348460225f136fa
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-02-13 14:56:41 +0200

    CrudWithoutORM

---
+++ 07. Software Development/2022/7. Databases/CrudWithoutORM/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using CrudWithoutORM.Presentation;
+
+namespace CrudWithoutORM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // UI
+            Display display = new Display();
+        }
+    }
+}
