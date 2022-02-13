93569ab75426da1bd4c459e38a2c59e771d0dcdb
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-02-13 15:22:35 +0200

    Crud With ORM

---
+++ 07. Software Development/2022/7. Databases/CrudWithORM/Data/ProductContext.cs
@@ -0,0 +1,35 @@
+﻿
+namespace CrudWithORM.Data
+{
+    using CrudWithORM.Data.Model;
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// Product Database Context
+    /// </summary>
+    public class ProductContext : DbContext
+    {
+        /// <summary>
+        /// Products Table
+        /// </summary>
+        public DbSet<Product> Products { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ProductContext()
+        {
+            // Create the database automaticly
+            Database.EnsureCreated();
+        }
+
+        /// <summary>
+        /// Connection string to Microsoft SQL Server
+        /// </summary>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Shop;Integrated Security=True;";
+            optionsBuilder.UseSqlServer(connString);
+        }
+    }
+}
---
+++ 07. Software Development/2022/7. Databases/CrudWithORM/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CrudWithORM.Presentation;
+
+namespace CrudWithORM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Display display = new Display();
+        }
+    }
+}
