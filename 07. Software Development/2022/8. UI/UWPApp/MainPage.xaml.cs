e7d26f61351f183700ae63c0ad56ccdeefd898cb
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-02-26 15:15:37 +0200

    UI

---
+++ 07. Software Development/2022/8. UI/UWPApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPApp
+{
+    // Namespace
+    using Model;
+
+    /// <summary>
+    /// Products 
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Конструктор
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // TODO: Connection String Fix
+            // ProductsList.ItemsSource = Products.GetAll();
+        }
+
+        // Запис
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Product product = new Product()
+            {
+                Name = name.Text,
+                Price = decimal.Parse(price.Text),
+                Stock = int.Parse(stock.Text)
+            };
+            Products.Add(product);
+
+            // TODO: Connection String Fix
+            // ProductsList.ItemsSource = Products.GetAll();
+        }
+    }
+}
