b8f8b77135a17bee5829ce90eadc929a7a2e7741
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-02-26 14:49:29 +0200

    UI

---
+++ 07. Software Development/2022/8. UI/WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(587, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(712, 309);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(191, 301);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 34);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Enabled = false;
+            this.btnUpdate.Location = new System.Drawing.Point(319, 301);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(112, 34);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(58, 64);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(501, 31);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(58, 150);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(501, 31);
+            this.txtPrice.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Price";
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(58, 222);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(501, 31);
+            this.txtStock.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Stock";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(447, 301);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(112, 34);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(58, 301);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(112, 34);
+            this.btnInsert.TabIndex = 9;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1331, 373);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Products 1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button btnSave;
+        private Button btnUpdate;
+        private Label label1;
+        private TextBox txtName;
+        private TextBox txtPrice;
+        private Label label2;
+        private TextBox txtStock;
+        private Label label3;
+        private Button btnDelete;
+        private Button btnInsert;
+    }
+}
---
+++ 07. Software Development/2022/8. UI/WinFormsApp/Form1.cs
@@ -0,0 +1,164 @@
+﻿using Business;
+using Data.Model;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        // Контекста на базата данни
+        private ProductBusiness productBusiness = new ProductBusiness();
+
+        // Идентификатор на продукта
+        private int editId = 0;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+
+            UpdateGrid();
+            ClearTextBoxes();
+
+            btnSave.Enabled = false;
+            btnUpdate.Enabled = true;
+        }
+
+        /// <summary>
+        /// Зарежда информацията от базата данни
+        /// </summary>
+        private void UpdateGrid()
+        {
+            dataGridView1.DataSource = productBusiness.GetAll();
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        /// <summary>
+        /// Изчистваме текстовите полета
+        /// </summary>
+        private void ClearTextBoxes()
+        {
+            txtName.Text = "";
+            txtPrice.Text = "";
+            txtStock.Text = "";
+        }
+
+        /// <summary>
+        /// Добави
+        /// </summary>
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            Product product = new Product()
+            {
+                Name = txtName.Text,
+                Price = decimal.Parse(txtPrice.Text),
+                Stock = int.Parse(txtStock.Text)
+            };
+            productBusiness.Add(product);
+
+            UpdateGrid();
+            ClearTextBoxes();
+        }
+
+        /// <summary>
+        /// Актуализация
+        /// </summary>
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var item = dataGridView1.SelectedRows[0].Cells;
+                var id = int.Parse(item[0].Value.ToString());
+                editId = id;
+                UpdateTextboxes(id);
+                ToggleSaveUpdate();
+                DisableSelect();
+            }
+        }
+
+        /// <summary>
+        /// Актуализация на текстовите кутийки
+        /// </summary>
+        private void UpdateTextboxes(int id)
+        {
+            txtName.Text = productBusiness.Get(id).Name;
+            txtPrice.Text = productBusiness.Get(id).Price.ToString();
+            txtStock.Text = productBusiness.Get(id).Stock.ToString();
+        }
+
+        /// <summary>
+        /// Смяна на бутоните Save и Update
+        /// </summary>
+        private void ToggleSaveUpdate()
+        {
+            if (btnUpdate.Enabled)
+            {
+                btnSave.Enabled = true;
+                btnUpdate.Enabled = false;
+            }
+            else
+            {
+                btnSave.Enabled = false;
+                btnUpdate.Enabled = true;
+            }
+        }
+
+        private void DisableSelect()
+        {
+            dataGridView1.Enabled = false;
+        }
+
+        /// <summary>
+        /// Изтриване
+        /// </summary>
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var item = dataGridView1.SelectedRows[0].Cells;
+                var id = int.Parse(item[0].Value.ToString());
+                productBusiness.Delete(id);
+                UpdateGrid();
+                ResetSelect();
+            }
+        }
+
+        private void ResetSelect()
+        {
+            dataGridView1.ClearSelection();
+            dataGridView1.Enabled = true;
+        }
+
+
+        /// <summary>
+        /// Запис
+        /// </summary>
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Product editedProduct = GetEditedProduct();
+            productBusiness.Update(editedProduct);
+            UpdateGrid();
+            ResetSelect();
+            ToggleSaveUpdate();
+        }
+
+        private Product GetEditedProduct()
+        {
+            Product product = new Product();
+            product.Id = editId;
+
+            var name = txtName.Text;
+            decimal price = 0;
+            decimal.TryParse(txtPrice.Text, out price);
+            int stock = 0;
+            int.TryParse(txtStock.Text, out stock);
+            product.Name = name;
+            product.Price = price;
+            product.Stock = stock;
+
+            return product;
+        }
+    }
+}
