5fe7abaf534fa02c53aa9d819d1372ae64ac9c44
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-06-25 13:30:08 +0300

    25.06.22

---
+++ 04. Introduction to Algorithms and Data Structures/2022/1. Data Structures/ArrayStack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ArrayStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Разтеглив масив с числа
+            ArrayStack<int> numbers = new ArrayStack<int>();
+            numbers.Push(112);
+            numbers.Push(911);
+            numbers.Push(166);
+            numbers.Push(150);
+            Console.WriteLine(string.Join(" ", numbers));
+
+            // Разтеглив масив с текст
+            ArrayStack<string> text = new ArrayStack<string>();
+            text.Push("Hello");
+            text.Push("World");
+            Console.WriteLine(string.Join(" ", text));
+
+        }
+    }
+}
