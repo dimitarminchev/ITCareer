92781aa9e4443738fadd7c6b80245d82ab352979
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-06-25 12:50:56 +0300

    25.06.22

---
+++ 04. Introduction to Algorithms and Data Structures/2022/1. Data Structures/CircularQueue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace CircularQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Кръгова опашка с числа
+            CircularQueue<int> numbers = new CircularQueue<int>();
+            numbers.Enqueue(112);
+            numbers.Enqueue(911);
+            numbers.Enqueue(166);
+            numbers.Enqueue(150);
+            Console.WriteLine(string.Join(" ", numbers));
+
+            // Кръгова опашка с текст
+            CircularQueue<string> text = new CircularQueue<string>();
+            text.Enqueue("Hello");
+            text.Enqueue("World");
+            Console.WriteLine(string.Join(" ", text));
+
+        }
+    }
+}
