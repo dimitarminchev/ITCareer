b5740404eb2fe5b4079e5891a661419b263faad4
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-06-25 12:22:50 +0300

    25.06.22

--- 04. Introduction to Algorithms and Data Structures/2022/1. Data Structures/ArrayList/Program.cs
+++ 04. Introduction to Algorithms and Data Structures/2022/1. Data Structures/ArrayList/Program.cs
@@ -5,26 +5,19 @@ namespace ArrayList
     {
         static void Main(string[] args)
         {
-            // Разтеглив масив с цели числа
-            ArrayList<int> arrayList = new ArrayList<int>();
-            arrayList.Add(1);
-            arrayList.Add(2);
-            arrayList.Add(3);
-            arrayList.Add(4);
-            arrayList.Add(5);
+            // Разтеглив масив с числа
+            ArrayList<int> numbers = new ArrayList<int>();
+            numbers.Add(112);
+            numbers.Add(911);
+            numbers.Add(166);
+            numbers.Add(150);
+            Console.WriteLine(string.Join(" ", numbers));
 
-            // Отпечатване
-            // foreach (var item in arraylist)
-            // {
-            //    console.writeline("{0} ", item);
-            // }
-            Console.WriteLine(string.Join(" ",arrayList));
-
-            // Разтеглив масив с низове
-            ArrayList<string> stringList = new ArrayList<string>();
-            stringList.Add("Hello");
-            stringList.Add("World");
-            Console.WriteLine(string.Join(" ", stringList));
+            // Разтеглив масив с текст
+            ArrayList<string> text = new ArrayList<string>();
+            text.Add("Hello");
+            text.Add("World");
+            Console.WriteLine(string.Join(" ", text));
 
         }
     }
---
+++ 04. Introduction to Algorithms and Data Structures/2022/1. Data Structures/DynamicList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace DynamicList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Динамичен списък с цисла
+            DynamicList numbers = new DynamicList();
+            numbers.Add(112);
+            numbers.Add(911);
+            numbers.Add(166);
+            numbers.Add(150);
+            foreach (var item in numbers)
+            {
+                Console.Write("{0} ",item);
+            }
+            // Console.WriteLine(string.Join(" ", numbers));
+
+            // Динамичен списък с тест
+            DynamicList text = new DynamicList();
+            text.Add("Hello");
+            text.Add("World");
+            foreach (var item in text)
+            {
+                Console.Write("{0} ", item);
+            }
+            // Console.WriteLine(string.Join(" ", text));
+
+        }
+    }
+}
