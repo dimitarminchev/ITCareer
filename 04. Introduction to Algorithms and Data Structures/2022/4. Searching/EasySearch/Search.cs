3c666132dd893fc27e58e8848fa7b9099ba87bad
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-06-26 12:49:53 +0300

    26.06.22

---
+++ 04. Introduction to Algorithms and Data Structures/2022/4. Searching/EasySearch/Search.cs
@@ -0,0 +1,22 @@
+﻿namespace EasySearch
+{
+    public class Search
+    {
+        /// <summary>
+        /// Лесно търсене
+        /// </summary>
+        public static int Easy<T>(T[] collection, T key) where T : IComparable
+        {
+            for (int index = collection.Count() - 1; index!=0; index--)
+            {
+                if (collection[index].Equals(key))
+                {
+                    // found
+                    return index;
+                }
+            }
+            // not found
+            return -1;
+        }
+    }
+}
