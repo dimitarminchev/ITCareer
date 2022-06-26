1f958da0d6b6fd5dc820c4aad7a3bd5c52a2f606
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-06-26 11:10:28 +0300

    26.06.22

---
+++ 04. Introduction to Algorithms and Data Structures/2022/3. Sorting/FisherYatesShuffle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FisherYatesShuffle
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Колекция за сортиране
+            int[] array = { -1, 9, 2, -8, 7, 6, -3, 5, 4, 1, 3 };
+
+            // Отпечаваме на колекцията
+            Console.WriteLine(String.Join(" ", array));
+
+            // Разбъркване на колекцията
+            Help.Shuffle(array);
+
+            // Отпечаваме на колекцията
+            Console.WriteLine(String.Join(" ", array));
+        }
+    }
+}
