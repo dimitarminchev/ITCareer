3a0db0b433297ca92d90e7e101fb60095ffed1a5
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-06-26 12:18:54 +0300

    26.06.22

---
+++ 04. Introduction to Algorithms and Data Structures/2022/4. Searching/LinearSearch/Search.cs
@@ -0,0 +1,20 @@
+﻿namespace LinearSearch
+{
+    public class Search
+    {
+        /// <summary>
+        /// Линейно търсене
+        /// </summary>
+        public static int Linear<T>(T[] collection, T key) where T : IComparable
+        {
+            for (int index = 0; index < collection.Count(); index++)
+            {
+                if (collection[index].CompareTo(key) == 0)
+                {
+                    return index; // Found
+                }
+            }
+            return -1; // Not Found
+        }
+    }
+}
