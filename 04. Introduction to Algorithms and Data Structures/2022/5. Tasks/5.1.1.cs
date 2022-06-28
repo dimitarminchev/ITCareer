8a91e9da614843274c15e22b996b3d90af00debd
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-06-28 21:15:00 +0300

    28.06.22

---
+++ 04. Introduction to Algorithms and Data Structures/2022/5. Tasks/5.1.1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _511
+{
+    public class Search
+    {        
+        public static int Linear<T>(T[] elements, T key) where T : IComparable
+        {
+            for (int index = 0; index < elements.Count(); index++)
+            {
+                if (elements[index].CompareTo(key) == 0)
+                {
+                    return index; // Found
+                }
+            }
+            return -1; // Not Found
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var n = int.Parse(Console.ReadLine());
+            index = Search.Linear(numbers, n);
+            if(index == -1)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(index);
+            }
+        }
+    }
+}
