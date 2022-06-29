bcc1bc416761cf7da41d985b3417ca7d98dcc380
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-06-29 20:38:44 +0300

    29.06.22

---
+++ 04. Introduction to Algorithms and Data Structures/2022/6. Additional Tasks/612/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _612
+{
+    public class Program
+    {
+        private static int n = int.Parse(Console.ReadLine());
+
+        private static int[] loops = new int[n];
+
+
+        private static void Permutation(int index)
+        {
+            // The End
+            if (index == loops.Length)
+            {
+                // Print
+                foreach (var item in loops)
+                {
+                    Console.Write($"{item} ");
+                }
+                Console.WriteLine();
+                return; // Exit from Permutation
+            }
+            // Process permutations
+            else 
+            {
+                for (int i = 1; i <= loops.Length; i++)
+                {
+                    loops[index] = i;
+                    Permutation(index + 1);
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Permutation(0);
+        }
+    }
+}
