e7a19a5177a0b14fac451c027a4ff4d27ecf3ea2
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-06-29 20:50:24 +0300

    613

---
+++ 04. Introduction to Algorithms and Data Structures/2022/6. Additional Tasks/613/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _613
+{
+    public class Program
+    {
+        private static void Combinations(int[] arr, int index, int start, int end)
+        {
+            if (index >= arr.Length)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write("{0} ", arr[i]);
+                }
+                Console.WriteLine();
+                return; // Exit
+            }
+            else 
+            {
+                for (int i = start; i <= end; i++)
+                {
+                    arr[index] = i;
+                    Combinations(arr, index + 1, i, end);
+                }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            // Input
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            // Output
+            int[] arr = new int[k];
+            Combinations(arr, 0, 1, n);
+        }
+    }
+}
