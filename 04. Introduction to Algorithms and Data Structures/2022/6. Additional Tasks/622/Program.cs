8219ef93f32c8ef4b5923a5b1bf03c9f967ddc30
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-06-30 21:05:42 +0300

    30.06.22

---
+++ 04. Introduction to Algorithms and Data Structures/2022/6. Additional Tasks/622/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _622
+{
+    public class Program
+    {
+        /// <summary>
+        /// Рекурсивен метод за намиране на факториел
+        /// </summary>
+        private static long Fak(int n)
+        {
+            if (n <= 0) return 1; // Exit Recurtion
+            else return n * Fak(n - 1);
+        }
+
+        /// <summary>
+        /// Главен метод
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fak(n));
+        }
+    }
+}
