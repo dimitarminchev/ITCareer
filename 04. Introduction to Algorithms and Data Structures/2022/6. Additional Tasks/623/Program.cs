f01665b533e0e5a8b4668efaa963c9b11b5db745
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-06-30 21:10:40 +0300

    30.06.22

---
+++ 04. Introduction to Algorithms and Data Structures/2022/6. Additional Tasks/623/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _623
+{
+    public class Program
+    {
+        /// <summary>
+        /// Рисуване на фигурата
+        /// </summary>
+        private static void Draw(int n)
+        {
+            int c = n;
+            if (n <= 1) Console.WriteLine(new string('*', n));
+            else
+            {
+                Console.WriteLine(new string('*', n));
+                Draw(n - 1);
+            }
+            if(c <= n) Console.WriteLine(new string('#', n));
+        }
+
+        /// <summary>
+        /// Главен метод
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Draw(n);
+        }
+    }
+}
