67d5eff655568c921d47270b48f097b5bf1ed52c
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-09-18 11:35:14 +0300

    18.09.2022

---
+++ 05. Object Oriented Programming/2022/02. Defining Classes/04. SqrtPrecalculator/SquareRootPrecalculator.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// Изчисляване на корен квадратен
+/// </summary>
+public static class SquareRootPrecalculator
+{
+    private const int MaxValue = 1000;
+    private static double[] sqrtValues;
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    static SquareRootPrecalculator()
+    {
+        sqrtValues = new double[MaxValue + 1];
+        for (int i = 1; i <= MaxValue; i++)
+            sqrtValues[i] = Math.Sqrt(i);
+    }
+
+    /// <summary>
+    /// Връща корен квадратен
+    /// </summary>
+    public static double GetSqrt(int value)
+    {
+        return sqrtValues[value];
+    }
+}
