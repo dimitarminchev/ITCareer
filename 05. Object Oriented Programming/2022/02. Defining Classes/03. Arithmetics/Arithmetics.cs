922d7efbe9cb21acc700883f5a6e7b96064a5800
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-09-18 11:26:22 +0300

    18.09.2022

---
+++ 05. Object Oriented Programming/2022/02. Defining Classes/03. Arithmetics/Arithmetics.cs
@@ -0,0 +1,25 @@
+﻿namespace _03._Arithmetics
+{
+    /// <summary>
+    /// Аритметични операции
+    /// </summary>
+    public static class Arithmetics
+    {
+        /// <summary>
+        /// Сумиране на две цели числа
+        /// </summary>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Умножение на две цели числа
+        /// </summary>
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
---
+++ 05. Object Oriented Programming/2022/02. Defining Classes/03. Arithmetics/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _03._Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Сумиране на две числа
+            var add = Arithmetics.Add(10, 15);
+            Console.WriteLine("Add = {0}", add);
+
+            // Умножение на две числа
+            var mul = Arithmetics.Multiply(10, 15);
+            Console.WriteLine("Multiply = {0}", mul);
+
+        }
+    }
+}
