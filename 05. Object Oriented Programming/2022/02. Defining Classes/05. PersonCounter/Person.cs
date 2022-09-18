7ad7f6f52d17a5b6a679af736a356d8e868f8317
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-09-18 11:47:28 +0300

    18.09.2022

---
+++ 05. Object Oriented Programming/2022/02. Defining Classes/05. PersonCounter/Person.cs
@@ -0,0 +1,55 @@
+﻿/// <summary>
+/// Клас човек
+/// </summary>
+public class Person
+{
+    private string name;
+
+    /// <summary>
+    /// Име на човека
+    /// </summary>
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private int age;
+
+    /// <summary>
+    /// Възраст на човека
+    /// </summary>
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    /// <summary>
+    /// Представяне
+    /// </summary>
+    public void IntroduceYourself()
+    {
+        Console.WriteLine("Hello! My name is {0} and I am {1} years old.", name, age);
+    }
+
+    private static int count = 0;
+
+    /// <summary>
+    /// Брой създадени обекти от този клас
+    /// </summary>
+    public static int Count 
+    {
+        get { return count; }
+    }
+
+    /// <summary>
+    /// Конструктор
+    /// </summary>
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+        Person.count++;
+    }
+}
---
+++ 05. Object Oriented Programming/2022/02. Defining Classes/05. PersonCounter/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _05._PersonCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Колекция с хора
+            List<Person> persons = new List<Person>();
+
+            // Добавя случаен брой хора към колецията
+            Random rand = new Random();
+            int random = rand.Next(1000);
+            for (int i = 0; i < random; i++)
+            {
+                var newPerson = new Person(i.ToString(), i);
+                persons.Add(newPerson);
+            }
+
+            // Колко човека сме добавили в колекцията?
+            Console.WriteLine("Persons Conter = {0}", Person.Count);
+        }
+    }
+}
