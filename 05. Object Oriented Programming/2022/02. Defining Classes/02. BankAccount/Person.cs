273e013d63148d56ebddda67e6b0750d4e7d5080
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-09-18 11:00:59 +0300

    18.09.2022

---
+++ 05. Object Oriented Programming/2022/02. Defining Classes/02. BankAccount/Person.cs
@@ -0,0 +1,72 @@
+﻿namespace _02._BankAccount
+{
+	/// <summary>
+	/// Човек
+	/// </summary>
+    public class Person
+    {
+		private string name;
+
+		/// <summary>
+		/// Име на човека
+		/// </summary>
+		public string Name
+		{
+			get { return name; }
+			set 
+			{
+				if (string.IsNullOrEmpty(value))
+				{
+					throw new ArgumentException("Name can not be empty!");
+				}
+				name = value; 
+			}
+		}
+
+		private int age;
+
+		/// <summary>
+		/// Възраст на човека
+		/// </summary>
+		public int Age
+		{
+			get { return age; }
+			set 
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException("Age must be positive value!");
+				}
+				age = value; 
+			}
+		}
+
+        private List<BankAccount> accounts = new List<BankAccount>();
+
+		/// <summary>
+		/// Банкови сметки
+		/// </summary>
+		public List<BankAccount> Accounts
+		{
+			get { return accounts; }
+			set { accounts = value; }
+		}
+
+		/// <summary>
+		/// Обща стойност на сумита по всички банкови сметки на човека
+		/// </summary>
+		/// <returns></returns>
+        public double GetBalance()
+        {
+            return accounts.Sum(element => element.Balance);
+        }
+
+		/// <summary>
+		/// Представяне на човека
+		/// </summary>
+        public void IntroduceYourself()
+		{
+			Console.WriteLine("Hello! My name is {0} and I am {1} years old.", name, age);
+		}
+	}
+}
---
+++ 05. Object Oriented Programming/2022/02. Defining Classes/02. BankAccount/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _02._BankAccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Създаване на три банкови сметки
+            List<BankAccount> accounts = new List<BankAccount>();
+            accounts.Add(new BankAccount() { ID = 1, Balance = 1000 });
+            accounts.Add(new BankAccount() { ID = 2, Balance = 2000 });
+            accounts.Add(new BankAccount() { ID = 3, Balance = 3000 });
+
+            // Създаваме обект person инстация на класа Person
+            Person person = new Person();
+            person.Name = "Peter";
+            person.Age = 18;
+            person.Accounts = accounts;
+
+            // Извеждане на инсформация за човека
+            person.IntroduceYourself();
+            foreach (var account in person.Accounts)
+            {
+                Console.WriteLine("Id = {0}, Balance {1}", account.ID, account.Balance);
+            }
+            Console.WriteLine("Total Bank Account Balance = {0}", person.GetBalance());
+        }
+    }
+}
