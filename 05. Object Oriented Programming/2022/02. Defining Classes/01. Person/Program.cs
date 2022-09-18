6a9d930f53e7de6a41817b856a630387fd7efedd
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-09-18 10:46:59 +0300

    18.09.2022

---
+++ 05. Object Oriented Programming/2022/02. Defining Classes/01. Person/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Създаване на обект firstPerson инстанция на класа Person
+            Person firstPerson = new Person();
+            firstPerson.Name = "George";
+            firstPerson.Age = 15;
+            firstPerson.IntroduceYourself();
+
+            // Създаване на обект secondPerson инстанция на класа Person
+            Person secondPerson = new Person();
+            secondPerson.Name = "Peter";
+            secondPerson.Age = 23;
+            secondPerson.IntroduceYourself();
+        }
+    }
+}
