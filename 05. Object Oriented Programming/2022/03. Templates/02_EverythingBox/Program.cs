32867f675cfbece6b8c2cb761e2c4afdfb397610
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-09-24 10:29:45 +0300

    24.09.2022

---
+++ 05. Object Oriented Programming/2022/03. Templates/02_EverythingBox/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02_EverythingBox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Integer Box
+            Box<int> box1 = new Box<int>();
+            box1.Add(123);
+            box1.Print();
+
+            // 2. String Box
+            Box<string> box2 = new Box<string>();
+            box2.Add("hello box");
+            box2.Print();
+
+            // 3. Floating point Box
+            Box<double> box3 = new Box<double>();
+            box3.Add(3.14);
+            box3.Print();
+        }
+    }
+}
