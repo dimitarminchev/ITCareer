52b6524524eed77bc0b4072002ba5db5dafb86b3
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-09-24 10:35:10 +0300

    24.09.2022

---
+++ 05. Object Oriented Programming/2022/03. Templates/04_IntegerBox/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _04_IntegerBox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+
+            // Input
+            int n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                box.Add(int.Parse(Console.ReadLine()));
+                n--;
+            }
+
+            // Ouput
+            box.Print();
+        }
+    }
+}
