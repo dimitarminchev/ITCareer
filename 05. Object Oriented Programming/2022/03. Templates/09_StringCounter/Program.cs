11fa1720559707a0444750344131b5b923f91359
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-09-24 11:19:22 +0300

    24.09.2022

---
+++ 05. Object Oriented Programming/2022/03. Templates/09_StringCounter/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _09_StringCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>();
+
+            // Input
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                box.Add(Console.ReadLine());
+            }
+            string element = Console.ReadLine();
+
+            // Output
+            Console.WriteLine(box.BiggerThan(element));
+        }
+    }
+}
