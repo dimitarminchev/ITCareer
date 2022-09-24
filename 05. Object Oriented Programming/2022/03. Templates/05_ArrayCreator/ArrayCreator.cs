d18363ce7be9711f232b1798c0597a5b6fa7cd08
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-09-24 10:49:23 +0300

    24.09.2022

---
+++ 05. Object Oriented Programming/2022/03. Templates/05_ArrayCreator/ArrayCreator.cs
@@ -0,0 +1,23 @@
+﻿/// <summary>
+/// Array Creator
+/// </summary>
+public static class ArrayCreator
+{
+    /// <summary>
+    /// Create Array of Same Kind of Elements
+    /// </summary>
+    public static T[] Create<T>(int length, T item)
+    {
+        // Create the Array
+        T[] array = new T[length];
+
+        // Initialize the Array
+        for (int i = 0; i < length; i++)
+        {
+            array[i] = item;
+        }
+
+        // Return the Array
+        return array;
+    }
+}
