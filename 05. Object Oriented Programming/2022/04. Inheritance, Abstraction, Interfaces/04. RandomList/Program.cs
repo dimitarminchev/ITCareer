74829d063375b177adba6e093fb652b612c07ec3
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-09-25 10:45:10 +0300

    25.09.2022

---
+++ 05. Object Oriented Programming/2022/04. Inheritance, Abstraction, Interfaces/04. RandomList/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _04._RandomList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create random list instance
+            RandomList list = new RandomList();
+
+            // Add some data to the list
+            list.Add(42); // int
+            list.Add("fourty two"); // string
+            list.Add(3.14); // float
+            list.Add(true);
+
+            // Print the content of the list
+            Console.WriteLine(String.Join(", ", list.ToArray()));
+
+            // Randomly remove element from the list
+            Console.WriteLine(list.RandomObject());
+            Console.WriteLine(list.RandomObject());
+        }
+    }
+}
