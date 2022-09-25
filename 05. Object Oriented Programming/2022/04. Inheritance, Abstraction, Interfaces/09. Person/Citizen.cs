f69157a36ebc8a2191de21823c899b286c02bf77
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-09-25 11:40:51 +0300

    25.09.2022

---
+++ 05. Object Oriented Programming/2022/04. Inheritance, Abstraction, Interfaces/09. Person/Citizen.cs
@@ -0,0 +1,12 @@
+﻿public class Citizen : IPerson
+{
+    public string Name { get; private set; }
+
+    public int Age { get; private set; }
+
+    public Citizen(string name, int age)
+    {
+        Name = name;
+        Age = age;  
+    }
+}
---
+++ 05. Object Oriented Programming/2022/04. Inheritance, Abstraction, Interfaces/09. Person/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace _09._Person
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type personInterface = typeof(Citizen).GetInterface("IPerson");
+            PropertyInfo[] properties = personInterface.GetProperties();
+            Console.WriteLine(properties.Length);
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            IPerson person = new Citizen(name, age);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+
+        }
+    }
+}
