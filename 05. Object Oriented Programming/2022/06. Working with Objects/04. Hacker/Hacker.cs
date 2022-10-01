8ab27a0ef5499f9dc3a0d7f61f5121c03a350884
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-01 15:48:59 +0300

    01.10.2022

---
+++ 05. Object Oriented Programming/2022/06. Working with Objects/04. Hacker/Hacker.cs
@@ -0,0 +1,38 @@
+﻿/// <summary>
+/// Хакер
+/// </summary>
+private class Hacker
+{
+    /// <summary>
+    /// Потребител
+    /// </summary>
+    public string username = "securityGod82";
+    
+    /// <summary>
+    /// Парола
+    /// </summary>
+    public string Password
+    {
+        get => this.password;
+        set => this.password = value;
+    }
+    private string password = "mySuperSecretPassw0rd";
+
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    private int Id { get; set; }
+
+    /// <summary>
+    /// Сметка
+    /// </summary>
+    public double BankAccountBalance { get; private set; }
+
+    /// <summary>
+    /// Изтегли всички пари на света
+    /// </summary>
+    public void DownloadAllBankAccountsInTheWorld()
+    {
+        // не знам как
+    }
+}
---
+++ 05. Object Oriented Programming/2022/06. Working with Objects/04. Hacker/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _04._Hacker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
