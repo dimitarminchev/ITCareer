16f5bd4eb0ce3a21351e9e3313c06838ae10b8bd
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-01 12:46:07 +0300

    01.10.2022

---
+++ 05. Object Oriented Programming/2022/05. Polymorphism/06. Vehicle/IVehicle.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// Интерфейс за превозните сртрдства
+/// </summary>
+public interface IVehicle
+{ 
+    /// <summary>
+    /// Гориво
+    /// </summary>
+    double Fuel { get; }
+
+    /// <summary>
+    /// Литри на километър
+    /// </summary>
+    double LitersPerKm { get; }
+
+    /// <summary>
+    /// Разстояние
+    /// </summary>
+    double Distance { get; }
+
+    /// <summary>
+    /// Карай
+    /// </summary>
+    void Drive(double km);
+
+    /// <summary>
+    /// Презареди
+    /// </summary>
+    void Refuel(double litres);
+}
