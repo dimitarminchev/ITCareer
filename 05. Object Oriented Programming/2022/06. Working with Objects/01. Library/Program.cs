209bec20d45546944145b3fdf4d9f26a9cd14339
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-01 14:39:13 +0300

    01.10.2022

---
+++ 05. Object Oriented Programming/2022/06. Working with Objects/01. Library/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01._Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Създаваме библиотека 
+            Library library = new Library
+            (
+                new Book("Animal Farm", 2003, "George Orwell"),
+                new Book("The document is the case", 2002, "Doroty Sayers", "Robert Eustace"),
+                new Book("Developing Cross Platform Apps", 2021, "Dimitar Minchev")
+                // Можем да добавим още книги
+            );
+
+            // Отпечаваме библитеката
+            foreach (var book in library)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
