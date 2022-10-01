b221e311f1d85772977f8ecbc666b266a22865b4
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-01 15:15:47 +0300

    01.10.2022

--- 05. Object Oriented Programming/2022/06. Working with Objects/03. BookComparator/Library.cs
+++ 05. Object Oriented Programming/2022/06. Working with Objects/03. BookComparator/Library.cs
@@ -13,23 +13,21 @@ public class Library : IEnumerable<Book>
     /// <summary>
     /// Конструктор
     /// </summary>
-    /// <param name="books"></param>
     public Library(params Book[] books)
     {
         this.books = new List<Book>(books);
 
         // Автоматично сортиране на книгите в библиотеката
-        // 1. Години [0..9]
-        // 2. Заглавия [A..Z]
+        // 1. Заглавия [A..Z]
+        // 2. Годнини [9..0]
         this.books = this.books
-            .OrderBy(book => book.Year)
-            .ThenBy(book => book.Title).ToList();
+                     .OrderBy(book => book.Title)
+                     .ThenByDescending(book => book.Year).ToList();
     }
 
     /// <summary>
     /// Обхождане на колекцията от книги
     /// </summary>
-    /// <returns></returns>
     public IEnumerator<Book> GetEnumerator()
     {
         foreach (Book book in books)
--- 05. Object Oriented Programming/2022/06. Working with Objects/03. BookComparator/Program.cs
+++ 05. Object Oriented Programming/2022/06. Working with Objects/03. BookComparator/Program.cs
@@ -1,19 +1,19 @@
-﻿namespace _02._ComparableBook
+﻿namespace _03._BookComparator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // Автоматично сортиране на книгите в библиотеката
-            // 1. Години [0..9]
-            // 2. Заглавия [A..Z]
+            // 1. Заглавия [A..Z]
+            // 2. Годнини [9..0]
 
             Library library = new Library
             (
                 new Book("Animal Farm", 2003, "George Orwell"),
                 new Book("The Documents in the Case", 2002, "Dorothy Sayers", "Robert Eustace"),
                 new Book("The Documents in the Case", 1930)
-            );
+            );          
 
             foreach (var book in library)
             {
