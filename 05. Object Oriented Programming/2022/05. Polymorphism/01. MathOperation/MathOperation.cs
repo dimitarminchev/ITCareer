e99d589c3787624bd5270ea6a97c2302292477ea
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-01 10:27:31 +0300

    01.10.2022

---
+++ 05. Object Oriented Programming/2022/05. Polymorphism/01. MathOperation/MathOperation.cs
@@ -0,0 +1,17 @@
+﻿public class MathOperation
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public double Add(double a, double b, double c)
+    {
+        return a + b + c;
+    }
+
+    public decimal Add(decimal a, decimal b, decimal c)
+    {
+        return a + b + c;
+    }
+}
---
+++ 05. Object Oriented Programming/2022/05. Polymorphism/01. MathOperation/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01._MathOperation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mo = new MathOperation();
+            Console.WriteLine(mo.Add(2,3)); // 5
+            Console.WriteLine(mo.Add(2.2, 3.3, 5.5)); // 11
+            Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m)); // 9.9
+        }
+    }
+}
