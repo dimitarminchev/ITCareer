1d12e22e01a0f02e2b210acf1cbbde7d0728b220
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-02 11:17:58 +0300

    02.10.2022

---
+++ 05. Object Oriented Programming/2022/07. Functional Programming/07.  CountUppercaseWords/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _07.__CountUppercaseWords
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Count Uppercase Words
+        /// https://judge.softuni.org/Contests/Practice/Index/597#2
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // input
+            var words = Console.ReadLine()
+                        .Split(new string[] { " " },
+                        StringSplitOptions.RemoveEmptyEntries);
+
+            // processing
+            Func<string, bool> checker = n => n[0] == n.ToUpper()[0];
+
+            // output
+            words.Where(checker)
+                 .ToList()
+                 .ForEach(n => Console.WriteLine(n));
+
+        }
+    }
+}
