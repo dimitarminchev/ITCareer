3e273733a3a7e6faccf5dac28dc05cddf3db17d4
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-02 10:55:07 +0300

    02.10.2022

---
+++ 05. Object Oriented Programming/2022/07. Functional Programming/04. ShortWordsSorted/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _04._ShortWordsSorted
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Short Words Sorter
+        /// https://judge.softuni.org/Contests/Practice/Index/174#4
+        /// </summary>
+        static void Main(string[] args)
+        {
+            char[] separators = new char[]{'.',',',':',';','(',')','[',']','\\','\"','\'','/','!','?',' '};
+
+            string sentence = Console.ReadLine().ToLower();
+
+            string[] words = sentence.Split(separators);
+
+            var result = words
+                      .Where(w => w != "")
+                      .Where(w => w.Count() < 5)
+                      .OrderBy(w => w).Distinct();
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
