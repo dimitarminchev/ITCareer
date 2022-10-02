20349b71d5d9a0556691f68489220264315794b5
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-02 10:29:15 +0300

    02.10.2022

---
+++ 05. Object Oriented Programming/2022/07. Functional Programming/02. MinMaxSumAverage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._MinMaxSumAverage
+{
+    internal class Program
+    {
+        // https://judge.softuni.bg/Contests/Practice/Index/174#2
+        static void Main(string[] args)
+        {
+            // input
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            // output
+            Console.WriteLine("Sum = {0}", nums.Sum());
+            Console.WriteLine("Min = {0}", nums.Min());
+            Console.WriteLine("Max = {0}", nums.Max());
+            Console.WriteLine("Average = {0}", nums.Average());
+        }
+    }
+}
