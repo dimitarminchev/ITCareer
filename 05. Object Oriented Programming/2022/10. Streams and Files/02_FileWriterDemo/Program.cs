46d20cbf4fb4b90b8fcc4f8dd2db91a28d812bac
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-09 10:56:32 +0300

    09.10.2022

--- 05. Object Oriented Programming/2022/10. Streams and Files/02_FileWriterDemo/Program.cs
+++ 05. Object Oriented Programming/2022/10. Streams and Files/02_FileWriterDemo/Program.cs
@@ -4,6 +4,25 @@
     {
         static void Main(string[] args)
         {
+            //// Short Version
+            //using (var reader = new StreamReader("Program.cs"))
+            //{
+            //    using (var writer = new StreamWriter("reversed.txt"))
+            //    {
+            //        string line = reader.ReadLine();
+            //        while (line != null)
+            //        {
+            //            for (int i = line.Length - 1; i >= 0; i--)
+            //            {
+            //                writer.Write(line[i]);
+            //            }
+            //            writer.WriteLine();
+            //            line = reader.ReadLine();
+            //        }
+            //    }
+            //}
+
+
             // Read file 'Program.cs'
             StreamReader reader = null;
             try
---
+++ 05. Object Oriented Programming/2022/10. Streams and Files/03_FileStreamDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace _03_FileStreamDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Text
+            string text = "Това е много хубава програма, която написахме в Сливен";
+
+            // Save text to file 'log.txt'
+            var fileStream = new FileStream("log.txt", FileMode.Create);
+            try
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(text);
+                fileStream.Write(bytes, 0, bytes.Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally
+            {
+                fileStream.Close();
+            }
+
+            //// v2
+            //using (fileStream)
+            //{
+            //    byte[] bytes = Encoding.UTF8.GetBytes(text);
+            //    fileStream.Write(bytes, 0, bytes.Length);
+            //} // fileStream.Dispose();
+
+            //// TODO Read the file and print it
+            //byte[] byteArray = File.ReadAllBytes("log.txt");
+            //string result = System.Text.Encoding.UTF8.GetString(byteArray);
+            //Console.WriteLine(result);
+
+        }
+    }
+}
