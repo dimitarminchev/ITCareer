189e26a826781108273b08d28efb89222ab64bb1
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-09 11:51:30 +0300

    09.10.2022

---
+++ 05. Object Oriented Programming/2022/10. Streams and Files/104_BinaryFileCopy/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _104_BinaryFileCopy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1. Source file
+            using (FileStream source = new FileStream("file.png", FileMode.Open))
+            {
+                // Step 2. Destination file
+                using (FileStream destination = new FileStream("newfile.png", FileMode.Create))
+                {
+                    byte[] bytes = new byte[4096]; // 4 KB
+                    while (true)
+                    {
+                        int data = source.Read(bytes, 0, bytes.Length);
+                        if (data == 0) break; // Exit
+                        destination.Write(bytes, 0, bytes.Count());
+                    }
+                } // destination.Dispose() // CleanUp
+            } // source.Dispose() // CleanUp
+        }
+    }
+}
