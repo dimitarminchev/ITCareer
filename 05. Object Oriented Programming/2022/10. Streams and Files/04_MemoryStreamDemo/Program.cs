da6835fcf2988296ef85eee17659b9ec590feba3
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-09 11:07:34 +0300

    09.10.2022

---
+++ 05. Object Oriented Programming/2022/10. Streams and Files/04_MemoryStreamDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Text;
+
+namespace _04_MemoryStreamDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "This is mega amazing super cool text witten in Sliven";
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            // Create memory stream (ms) from byte array (bytes)
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                while (true)
+                {
+                    // Read from the memory stream [ms]
+                    int readByte = ms.ReadByte();
+                    if (readByte == -1) break; // Exit
+                    Console.Write((char)readByte);
+                }
+
+            } // ms.Dispose(); // CleanUp
+        }
+    }
+}
