e28ec14131b7c0034d91b4eb9dc8f944a25c2a6e
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-09 11:31:41 +0300

    09.10.2022

---
+++ 05. Object Oriented Programming/2022/10. Streams and Files/101_OddStream/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _101_OddStream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("file.txt");
+
+            // v1
+            using (reader)
+            {
+                int lineNumber = 0;
+                string lineContent = reader.ReadLine();
+                while (lineContent != null) 
+                {
+                    if (lineNumber % 2 == 1)
+                    {
+                        Console.WriteLine(lineContent);
+                    }
+                    lineNumber++;
+                    lineContent = reader.ReadLine();
+                }
+
+            } // reader.Dispose(); // CleanUp
+
+            //// v2
+            //try
+            //{
+            //    int lineNumber = 0;
+            //    while (true)
+            //    {
+            //        string lineContent = reader.ReadLine();
+            //        if (lineNumber % 2 == 1)
+            //        {
+            //            Console.WriteLine(lineContent);
+            //        }
+            //        lineNumber++;
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine($"Error: {e.Message}");
+            //}
+            //finally
+            //{
+            //    reader.Close(); // CleanUp
+            //}
+        }
+    }
+}
