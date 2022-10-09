d2f969e5a8ba1b6f1c09c3bf85f09b607a63351f
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-10-09 10:19:45 +0300

    09.10.2022

---
+++ 05. Object Oriented Programming/2022/10. Streams and Files/01_FileReaderDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01_FileReaderDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader("Program.cs");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+                return; // Stop the program
+            }
+
+            using (reader)
+            {
+                int lineNumber = 1;
+                string lineContent = reader.ReadLine();
+                while(lineContent != null)
+                {
+                    Console.WriteLine($"Line {lineNumber}: {lineContent}");
+                    lineNumber++;
+                    lineContent = reader.ReadLine();
+                }
+            }
+            // reader.Dispose();
+        }
+    }
+}
