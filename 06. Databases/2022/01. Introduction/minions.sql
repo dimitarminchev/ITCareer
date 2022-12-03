2b8d1ed57546564730a3b0680c8149695229e21c
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-12-03 12:41:03 +0200

    03.12.2022

--- 06. Databases/2022/01. Introduction/minions.sql
+++ 06. Databases/2022/01. Introduction/minions.sql
@@ -1,47 +1,57 @@
-/* Създаване на база от данни */
-CREATE SCHEMA `minions` ;
+/* database minions */
+
+/* Създаване на нова база от данни */
+CREATE SCHEMA IF NOT EXISTS `minions`;
 
 /* Създаване на таблица */
-CREATE TABLE `minions`.`minions` (
-  `id` INT(11) NOT NULL,
-  `name` VARCHAR(50) NOT NULL,
-  `age` INT(3) NULL,
-  PRIMARY KEY (`id`));
-  
-/* Добавяне на запис */
-INSERT INTO `minions`.`minions` (`id`,`name`,`age`) VALUES ('1','Kevin','15');  
-INSERT INTO `minions`.`minions` (`id`,`name`,`age`) VALUES ('2','Bob','22');  
-INSERT INTO `minions`.`minions` (`id`,`name`) VALUES ('3','Steward');  
-  
-/* Извличане на записи */
-SELECT * FROM `minions`.`minions`;
-
-/* Актуализация на запис */ 
-UPDATE `minions`.`minions` SET `age`=10 WHERE `name`='Steward';
-
-/* Изтриване на запис */
-/* DELETE FROM `minions`.`minions` WHERE `id`=2; */
-
-/* Създаване на таблица towns */
-CREATE TABLE `minions`.`towns` 
+CREATE TABLE IF NOT EXISTS `minions`.`minions` 
 (
   `id` INT NOT NULL,
-  `name` VARCHAR(50) NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `age` INT(3) NULL,
+  PRIMARY KEY (`id`)
+);
+
+/* Вмъкване на данни в таблицата */
+INSERT INTO `minions`.`minions` (`id`, `name`, `age`) VALUES ('1', 'Kevin', '15');
+INSERT INTO `minions`.`minions` (`id`, `name`, `age`) VALUES ('2', 'Bob', '22');
+INSERT INTO `minions`.`minions` (`id`, `name`) VALUES ('3', 'Steward');
+
+/* Извеждане на всички записи от таблица */
+SELECT * FROM `minions`.`minions`;
+
+/* Извеждане на всички записи с въраст различна от NULL */
+SELECT * FROM `minions`.`minions` WHERE `age` IS NOT NULL;
+
+/* Aктуализиране на един запис */
+UPDATE `minions`.`minions` SET `age`='10' WHERE (`id` = '3');
+
+/* Актуализиране на всички записи */
+UPDATE `minions`.`minions` SET age = age + 1;
+
+/* Изтриване на запис */
+DELETE FROM `minions`.`minions` WHERE id=2;
+
+/* Създаване на нова таблица */
+CREATE TABLE IF NOT EXISTS `minions`.`towns`
+(
+   `id` INT NOT NULL,
+   `name` VARCHAR(40) NOT NULL,
    PRIMARY KEY (`id`)
 );
 
-/* Актуализиараме таблица minions */ 
-ALTER TABLE `minions`.`minions` ADD COLUMN `townid` INT NULL AFTER `age`, ADD INDEX `fk_minions_tows_idx` (`townid` ASC) VISIBLE;
-ALTER TABLE `minions`.`minions` ADD CONSTRAINT `fk_minions_tows`  FOREIGN KEY (`townid`)  REFERENCES `minions`.`towns` (`id`);
+/* Добавяне на данни в таблицата */
+INSERT INTO `minions`.`towns` (`id`,`name`) VALUES (1,"Burgas");
+INSERT INTO `minions`.`towns` (`id`,`name`) VALUES (2,"Sofia");
+INSERT INTO `minions`.`towns` (`id`,`name`) VALUES (3,"Varna");
 
-/* Изпразване на таблицата */
-TRUNCATE TABLE `minions`.`minions`;
-TRUNCATE TABLE `minions`.`towns`;
+/* Свързване на таблици */
+ALTER TABLE `minions`.`minions` 
+ADD COLUMN `townid` INT NULL AFTER `age`;
 
-/* Добавяне на записите */
-INSERT INTO `minions`.`towns`  (`id`,`name`) VALUES (1,'Sofia');
-INSERT INTO `minions`.`towns`  (`id`,`name`) VALUES (2,'Plovdiv'); 
-INSERT INTO `minions`.`towns`  (`id`,`name`) VALUES (3,'Varna');
-INSERT INTO `minions`.`minions` (`id`,`name`, `age`, `townid`) VALUES (1,'Kevin', 22, 1);
-INSERT INTO `minions`.`minions` (`id`,`name`, `age`, `townid`) VALUES (2,'Bob', 15, 3);
-INSERT INTO `minions`.`minions` (`id`,`name`, `age`, `townid`) VALUES (3,'Steward', NULL, 2);
+ALTER TABLE `minions`.`minions` 
+ADD CONSTRAINT `fk_minions_towns`
+  FOREIGN KEY (`id`)
+  REFERENCES `minions`.`towns` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
--- 06. Databases/2022/01. Introduction/school.sql
+++ 06. Databases/2022/01. Introduction/school.sql
@@ -1,34 +1,56 @@
-/* Създаване на нова база данни */
-CREATE SCHEMA `school`;
+/* database school */
+CREATE SCHEMA IF NOT EXISTS `school`;
 
-/* Създаване на таблица */
-CREATE TABLE `school`.`students` 
-(
-  `id` INT NOT NULL,
-  `name` VARCHAR(50) NULL,
-  `age` INT NULL,
-  `phone_number` VARCHAR(50) NULL,
-   PRIMARY KEY (`id`)
-);
-CREATE TABLE `school`.`classes` 
-(
-  `id` INT NOT NULL,
-  `name` VARCHAR(50) NULL,
-  `max_students` INT NULL,
-   PRIMARY KEY (`id`)
-);
-CREATE TABLE `school`.`teachers` 
-(
-  `id` INT NOT NULL,
-  `name` VARCHAR(50) NULL,
-  `class` INT NULL,
-   PRIMARY KEY (`id`)
-);
+CREATE TABLE IF NOT EXISTS `school`.`students` 
+( 
+	`id` INT(3) NOT NULL,
+	`name` VARCHAR(50) NOT NULL,
+    `age` INT(3) NULL,
+	`phone_number` VARCHAR(50)  NULL,
+    PRIMARY KEY (`id`)
+); 
+INSERT INTO `school`.`students` (`id`, `name`, `age`) VALUES ('1', 'Mitko', '42');
+INSERT INTO `school`.`students` (`id`, `name`, `age`) VALUES ('2', 'Ani', '36');
+INSERT INTO `school`.`students` (`id`, `name`, `age`) VALUES ('3', 'Peter', '4');
 
-/* Създаване на нова таблица */
-CREATE TABLE `school`.`towns` 
+CREATE TABLE IF NOT EXISTS `school`.`classes` 
 (
-  `id` INT NOT NULL,
-  `name` VARCHAR(50) NULL,
+	`id` INT(3) NOT NULL,
+	`name` VARCHAR(50) NOT NULL,
+	`max_students` INT(3) NULL,
+	PRIMARY KEY (`id`)
+);
+INSERT INTO `school`.`classes` (`id`, `name`, `max_students`) VALUES ('1', 'Introduction to programming', '15');
+INSERT INTO `school`.`classes` (`id`, `name`, `max_students`) VALUES ('2', 'Algorithms and data structure', '10');
+INSERT INTO `school`.`classes` (`id`, `name`, `max_students`) VALUES ('3', 'Databases', '2');
+
+CREATE TABLE IF NOT EXISTS `school`.`teachers`
+(
+	`id` INT(3) NOT NULL,
+	`name` VARCHAR(50) NOT NULL,
+	`class` TEXT NULL,
+	PRIMARY KEY (`id`)
+);
+INSERT INTO `school`.`teachers` (`id`, `name`, `class`) VALUES ('1', 'Dimitar Minchev', 'Introduction to programming');
+INSERT INTO `school`.`teachers` (`id`, `name`, `class`) VALUES ('2', 'Dimitar Minchev', 'Algorithms and data structure');
+INSERT INTO `school`.`teachers` (`id`, `name`, `class`) VALUES ('3', 'Dimitar Minchev', 'Databases');
+
+CREATE TABLE IF NOT EXISTS `school`.`towns`
+(
+   `id` INT(3) NOT NULL,
+   `name` VARCHAR(40) NOT NULL,
    PRIMARY KEY (`id`)
 );
+INSERT INTO `school`.`towns` (`id`,`name`) VALUES (1,"Burgas");
+INSERT INTO `school`.`towns` (`id`,`name`) VALUES (2,"Sofia");
+INSERT INTO `school`.`towns` (`id`,`name`) VALUES (3,"Varna");
+
+ALTER TABLE `school`.`students` 
+ADD COLUMN `townid` INT NULL AFTER `phone_number`;
+
+ALTER TABLE `school`.`students` 
+ADD CONSTRAINT `fk_students_towns`
+  FOREIGN KEY (`id`)
+  REFERENCES `school`.`towns` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
