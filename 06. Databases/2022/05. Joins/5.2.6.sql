580b4e01d613d94a079bb5169c714fe1d054306a
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-12-17 13:45:11 +0200

    17.12.2022

---
+++ 06. Databases/2022/05. Joins/5.2.6.sql
@@ -0,0 +1,12 @@
+/* 5.2.6. Планински вериги */
+USE `geography`;
+
+SELECT `mc`.`country_code`, `c`.`country_name`,	`m`.`mountain_range`
+FROM `mountains_countries` AS `mc` 
+INNER JOIN `countries` AS `c`  ON 
+(
+	`mc`.`country_code` = `c`.`country_code` AND
+	`mc`.`country_code` IN ("BG", "RU", "US")
+)
+INNER JOIN `mountains` AS `m` ON `mc`.`mountain_id` = `m`.`id` 
+ORDER BY `mc`.`country_code`, `m`.`mountain_range`;
