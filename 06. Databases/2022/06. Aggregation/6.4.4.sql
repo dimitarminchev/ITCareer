d185a141d1209f06193351fcc7a604f400db8b7f
Dimitar Minchev <dimitar.minchev@gmail.com>
2022-12-18 12:08:38 +0200

    18.12.2022

---
+++ 06. Databases/2022/06. Aggregation/6.4.4.sql
@@ -0,0 +1,9 @@
+/* 6.4.4. Средна лихва  */
+USE `gringotts`;
+
+SELECT `deposit_group`,	`is_deposit_expired`, 
+AVG(`deposit_interest`) AS `average_interest`
+FROM `wizzard_deposits`
+WHERE `deposit_start_date` >= 01/01/1985
+GROUP BY `deposit_group`, `is_deposit_expired`
+ORDER BY `deposit_group` DESC, `is_deposit_expired` ASC;
