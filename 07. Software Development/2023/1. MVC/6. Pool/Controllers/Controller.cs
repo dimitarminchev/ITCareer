33e70bfab2d386fda0ff92c7fd78a94f9081f99e
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-02-25 14:03:25 +0200

    25.02.2023

---
+++ 07. Software Development/2023/1. MVC/6. Pool/Controllers/Controller.cs
@@ -0,0 +1,29 @@
+﻿using _6._Pool.Models;
+using _6._Pool.Views;
+
+namespace _6._Pool.Controllers
+{
+    public class Controller
+    {
+        public Model model { get; set; }
+
+        public View view { get; set; }
+
+        public Controller()
+        {
+            view = new View();
+
+            model = new Model
+            (
+                view.v, 
+                view.p1, 
+                view.p2, 
+                view.h
+            );
+
+            view.Response = model.CalculateResponse();
+
+            view.ShowResponse();
+        }
+    }
+}
