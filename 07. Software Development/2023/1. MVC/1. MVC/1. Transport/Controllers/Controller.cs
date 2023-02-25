14bf578be9bb4bbdb71c9ecc69d7804f6d5f3908
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-02-25 12:50:28 +0200

    25.02.2023

---
+++ 07. Software Development/2023/1. MVC/1. MVC/1. Transport/Controllers/Controller.cs
@@ -0,0 +1,21 @@
+﻿using _1._Transport.Views;
+using _1._Transport.Models;
+
+namespace _1._Transport.Controllers
+{
+    public class Controller
+    {
+        private View view;
+        private Model model;
+
+        public Controller()
+        {
+            view = new View();
+            model = new Model(view.kilometers, view.time);
+
+            view.totalPrice = model.CalculatePrice();
+
+            view.ShowCheapestWayToTravel();
+        }
+    }
+}
---
+++ 07. Software Development/2023/1. MVC/1. MVC/1. Transport/Program.cs
@@ -0,0 +1,12 @@
+﻿using _1._Transport.Controllers;
+
+namespace _1._Transport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new Controller();
+        }
+    }
+}
