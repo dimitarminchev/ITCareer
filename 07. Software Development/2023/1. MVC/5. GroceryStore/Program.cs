98a315ceb566f4eb123f61e241504e177bc3cf9e
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-02-25 13:50:27 +0200

    25.02.2023

---
+++ 07. Software Development/2023/1. MVC/5. GroceryStore/Program.cs
@@ -0,0 +1,12 @@
+﻿using _5._GroceryStore.Controllers;
+
+namespace _5._GroceryStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new Controller();
+        }
+    }
+}
