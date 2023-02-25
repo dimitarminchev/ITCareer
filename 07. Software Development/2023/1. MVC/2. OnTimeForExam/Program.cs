c54d4df328e801a6346de91ab8a7d3d87ac408bd
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-02-25 13:05:40 +0200

    25.02.2023

---
+++ 07. Software Development/2023/1. MVC/2. OnTimeForExam/Program.cs
@@ -0,0 +1,12 @@
+﻿using _2._OnTimeForExam.Controllers;
+
+namespace _2._OnTimeForExam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new Controller();
+        }
+    }
+}
