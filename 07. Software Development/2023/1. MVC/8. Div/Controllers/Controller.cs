6ab3d81e785f34a39892d3439ebd33d7660fe978
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-02-25 14:28:06 +0200

    25.02.2023

---
+++ 07. Software Development/2023/1. MVC/8. Div/Controllers/Controller.cs
@@ -0,0 +1,25 @@
+﻿using _8._Div.Models;
+using _8._Div.Views;
+namespace _8._Div.Controllers
+{
+    public class Controller
+    {
+        public View view { get; set; }
+
+        public Model model { get; set; }
+
+        public Controller()
+        {
+            view = new View();
+
+            model = new Model
+            (
+                view.numbers
+            );
+
+            view.Response = model.CalculateResponse();
+
+            view.ShowResponse();
+        }
+    }
+}
---
+++ 07. Software Development/2023/1. MVC/8. Div/Program.cs
@@ -0,0 +1,12 @@
+﻿using _8._Div.Controllers;
+
+namespace _8._Div
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new Controller();
+        }
+    }
+}
