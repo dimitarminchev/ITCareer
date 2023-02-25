e968827cedb5a083cffb2c87c957b76bc8e8ab49
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-02-25 13:18:09 +0200

    25.02.2023

---
+++ 07. Software Development/2023/1. MVC/3. Histogram/Controllers/Controller.cs
@@ -0,0 +1,21 @@
+﻿using _3._Histogram.Models;
+using _3._Histogram.Views;
+
+namespace _3._Histogram.Controllers
+{
+    public class Controller
+    {
+        private View view;
+        private Model model;
+
+        public Controller()
+        {
+            view = new View();
+            model = new Model(view.count, view.numbers);
+
+            view.percents = model.FindPercentages();
+
+            view.Output();
+        }
+    }
+}
---
+++ 07. Software Development/2023/1. MVC/3. Histogram/Views/View.cs
@@ -0,0 +1,35 @@
+﻿
+namespace _3._Histogram.Views
+{
+    public class View
+    {
+        public double count { get; set; }
+        public List<int> numbers { get; set; }
+        public string percents { get; set; }
+
+        public View()
+        {
+            count = 0;
+            numbers = new List<int>();
+            percents = String.Empty;
+            Input();
+        }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter how many numbers you want: ");
+            count = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the numbers on new lines:");
+            for (int i = 0; i < count; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(percents);
+        }
+    }
+}
