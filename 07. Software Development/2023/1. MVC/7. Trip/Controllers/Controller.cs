92cebb9adef2e9a9eaac13de4e2509a481e05212
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-02-25 14:12:21 +0200

    25.02.2023

---
+++ 07. Software Development/2023/1. MVC/7. Trip/Controllers/Controller.cs
@@ -0,0 +1,29 @@
+﻿using _7._Trip.Models;
+using _7._Trip.Views;
+
+namespace _7._Trip.Controllers
+{
+    public class Controller
+    {
+        public Model model { get; set; }
+
+        public View view { get; set; }
+
+        public Controller()
+        {
+            view = new View();
+
+            model = new Model   
+            (
+                view.budget,
+                view.season
+            );
+
+            view.place = model.DefinePlace();
+            view.expenses = model.CalculateExpenses();
+            view.facility = model.Facility;
+
+            view.ShowResult();
+        }
+    }
+}
---
+++ 07. Software Development/2023/1. MVC/7. Trip/Program.cs
@@ -0,0 +1,12 @@
+﻿using _7._Trip.Controllers;
+
+namespace _7._Trip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var controller = new Controller();
+        }
+    }
+}
