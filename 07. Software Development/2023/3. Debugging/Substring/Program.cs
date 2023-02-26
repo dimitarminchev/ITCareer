3ec097e7ed853f2aeab7c4f4d3fb3787b67e2eeb
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-02-26 13:53:54 +0200

    26.02.2023

---
+++ 07. Software Development/2023/3. Debugging/Substring/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Substring
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine();
+            int jump = int.Parse(Console.ReadLine());
+
+            // fix: р (bg) => p (en)
+            const char Search = 'p'; 
+            bool hasMatch = false;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == Search)
+                {
+                    hasMatch = true;
+
+                    // fix: string len fixes
+                    int endIndex = jump + 1;
+                    if (i + endIndex >= text.Length)
+                    {
+                        endIndex = text.Length - i;
+                    }
+
+                    string matchedString = text.Substring(i, endIndex);
+                    Console.WriteLine(matchedString);
+                    i += jump;
+                }
+            }
+
+            if (!hasMatch)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
