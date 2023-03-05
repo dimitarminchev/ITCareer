f2dc3d65f61e5e5fb63dcde38ade5fc8c84d4775
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-03-05 13:44:28 +0200

    05.03.2023

---
+++ 07. Software Development/2023/4. Refactoring/Rotating Walk in Matrix/Matrix.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+/// <summary>
+/// Refactoring Task "Rotating Walk in Matrix" namespace.
+/// </summary>
+namespace Rotating_Walk_in_Matrix
+{
+    /// <summary>
+    ///  Refactoring Task "Rotating Walk in Matrix" class MAtrix
+    /// </summary>
+    public static class Matrix
+    {
+        public static void Change(ref int dX, ref int dY)
+        {
+            int[] directionsX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+            int[] directionsY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+            int cd = 0;
+
+            for (int count = 0; count < 8; count++)
+            {
+                if (directionsX[count] == dX && directionsY[count] == dY)
+                {
+                    cd = count;
+                    break;
+                }
+            }
+
+            if (cd == 7)
+            {
+                dX = directionsX[0];
+                dY = directionsY[0];
+                return;
+            }
+
+            dX = directionsX[cd + 1];
+            dY = directionsY[cd + 1];
+        }
+
+        public static bool CheckCell(int[,] arr, int x, int y)
+        {
+            int[] dirX = { 1, 1, 1, 0, -1, -1, -1, 0 };
+            int[] dirY = { 1, 0, -1, -1, -1, 0, 1, 1 };
+          
+            for (int i = 0; i < 8; i++)
+            {
+                if (x + dirX[i] >= arr.GetLength(0) || x + dirX[i] < 0)
+                {
+                    dirX[i] = 0;
+                }
+
+                if (y + dirY[i] >= arr.GetLength(0) || y + dirY[i] < 0)
+                {
+                    dirY[i] = 0;
+                }
+            }
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (arr[x + dirX[i], y + dirY[i]] == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void FindCell(int[,] arr, out int x, out int y)
+        {
+            x = 0;
+            y = 0;
+            for (int row = 0; row < arr.GetLength(0); row++)
+            {
+                for (int col = 0; col < arr.GetLength(0); col++)
+                {
+                    if (arr[row, col] == 0)
+                    {
+                        x = row;
+                        y = col;
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+}
