09a3ad5941ee1ff97b36e365e23796dbc8daa376
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-03-11 12:11:59 +0200

    11.02.2023

---
+++ 07. Software Development/2023/6. NuGet/OpticalCharacterRecognition/Program.cs
@@ -0,0 +1,27 @@
+﻿using Tesseract;
+
+namespace OpticalCharacterRecognition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // File Path
+            string filePath = AppContext.BaseDirectory;
+            string fileName = filePath + "tessdata\\test.png";
+
+            // Process Optical Character Recognition (OCR) ...
+            using (var engine = new TesseractEngine(@"tessdata", "eng"))
+            {
+                using (var image = Pix.LoadFromFile(fileName))
+                {
+                    using (var page = engine.Process(image))
+                    {
+                        string text = page.GetText();
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+    }
+}
