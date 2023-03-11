8ee11208eb5b93e77328022b068ac86a717c2bdb
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-03-11 11:58:07 +0200

    10.03.2023

---
+++ 07. Software Development/2023/6. NuGet/ChuckNorrisJokes/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+/// <summary>
+/// Task "Chuck Norris Jokes" namespace.
+/// </summary>
+namespace ChuckNorrisJokes
+{
+    /// <summary>
+    /// Task "Chuck Norris Jokes" main program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Task "Chuck Norris Jokes" main program method.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Step 1. Get Chuck Norris Joke from Internet
+            string json = GetJoke().Result;
+
+            // Step 2. Deserilize JSON to Object
+            Joke joke = JsonConvert.DeserializeObject<Joke>(json);
+
+            // Step 3. Print Joke
+            Console.WriteLine(joke.value);
+        }
+
+        /// <summary>
+        /// Get New Chuck Norris Joke from Internet
+        /// </summary>
+        /// <returns>json</returns>
+        private static async Task<string> GetJoke()
+        {
+            string url = "https://api.chucknorris.io/jokes/random";
+            HttpClient client = new HttpClient();
+            return await client.GetStringAsync(url);
+        }
+    }
+}
---
+++ 07. Software Development/2023/6. NuGet/Products/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+/// <summary>
+/// Task "Products" namespace.
+/// </summary>
+namespace Products
+{
+    /// <summary>
+    /// Task "Products" main program class.
+    /// </summary>
+    public class Program
+    {
+
+        /// <summary>
+        /// Task "Products" main program method.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Step 1. Serialize Object to JSON
+            Product product = new Product(1,"Coca Cola", 2.4m, 100, new DateTime(2023,09,10));
+            string json = JsonConvert.SerializeObject(product);
+
+            Console.WriteLine("Step 1. Serialize Object to JSON");
+            Console.WriteLine(json);
+            Console.WriteLine(Environment.NewLine);
+
+            // Step 2. Deserialize JSON to Object
+            Product second = JsonConvert.DeserializeObject<Product>(json);
+
+            Console.WriteLine("Step 2. Deserialize JSON to Object");
+            Console.WriteLine(second);
+            Console.WriteLine(Environment.NewLine);
+        }
+    }
+}
