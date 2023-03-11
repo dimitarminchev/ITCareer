bc92efe66aab926913d3b9b39da6494f2b8fe6b5
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-03-11 11:03:37 +0200

    11.03.2023

---
+++ 07. Software Development/2023/5. IDE/Person/Person.cs
@@ -0,0 +1,106 @@
+﻿using System.Security.Principal;
+using System.Text;
+/// <summary>
+/// Task "Person" namespace.
+/// </summary>
+namespace Person
+{
+    /// <summary>
+    /// Task "Person" BankAccount class.
+    /// </summary>
+    public class Person
+    {
+        private string name;
+
+        /// <summary>
+        /// Person Name
+        /// Note: Name can not be null or empty.
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Name can not be null or empty.");
+                }
+                name = value; 
+            }
+        }
+
+        private int age;
+
+        /// <summary>
+        /// Person Age
+        /// Note: Age must be from 18 to 100.
+        /// </summary>
+        public int Age
+        {
+            get { return age; }
+            set 
+            {
+                if (age >= 18 && age <= 100)
+                {
+                    throw new ArgumentException("Age must be from 18 to 100");
+                }
+                age = value; 
+            }
+        }
+
+        private List<BankAccount> accounts;
+
+        /// <summary>
+        /// Person Bank Accounts
+        /// Note: Collection must not be empty.
+        /// </summary>
+        public List<BankAccount> Accounts
+        {
+            get { return accounts; }
+            set 
+            {
+                if (value == null && value.Count() > 0)
+                {
+                    throw new ArgumentException("Collection must not be empty.");
+                }
+                accounts = value; 
+            }
+        }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Person() : this("Noname", 18, new List<BankAccount>())
+        {
+            // empty
+        }
+
+        /// <summary>
+        /// Overloaded Constructor
+        /// </summary>
+        /// <param name="name">Person name</param>
+        /// <param name="age">Person Age</param>
+        /// <param name="accounts">Person Bank Accounts</param>
+        public Person(string name, int age, List<BankAccount> accounts)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Accounts = accounts;
+        }
+
+        /// <summary>
+        /// ToString
+        /// </summary>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(string.Format($"Name: {this.Name}") + Environment.NewLine);
+            sb.Append(string.Format($"Age: {this.Age}") + Environment.NewLine);
+            sb.Append(string.Format($"Bank Accounts:") + Environment.NewLine);
+            Accounts.ForEach(account => sb.Append(string.Format($"\t{account}") + Environment.NewLine));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 07. Software Development/2023/5. IDE/Person/Program.cs
@@ -0,0 +1,30 @@
+﻿/// <summary>
+/// Task "Person" namespace.
+/// </summary>
+namespace Person
+{
+    /// <summary>
+    /// Task "Person" main prorgam class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Task "Person" main prorgam method.
+        /// </summary>
+        /// <param name="args">no arguments needed</param>
+        public static void Main(string[] args)
+        {
+            // Create Bank Accounts
+            List<BankAccount> bankAccounts = new List<BankAccount>();
+            bankAccounts.Add(new BankAccount(1, 100));
+            bankAccounts.Add(new BankAccount(2, 200));
+            bankAccounts.Add(new BankAccount(3, 300));
+
+            // Create Person
+            Person person = new Person("Ivan Ivanov", 22, bankAccounts);
+
+            // Print
+            Console.WriteLine(person);
+        }
+    }
+}
