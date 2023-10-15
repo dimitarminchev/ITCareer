d1e64725ff856de7f5b54f9fb60dcc63af1e1647
Dimitar Minchev <dimitar.minchev@gmail.com>
2023-10-15 14:54:16 +0300

    Актуализация на модул 10

---
+++ 10. Алгоритми и структури от данни/08. Exam Preparation/2023.10.15/CityTrade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    private static int FindFuel(int[] cities)
+    {
+        int n = cities.Length;
+        int[] fuel = new int[n];
+        fuel[n - 1] = cities[n - 1];
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            fuel[i] = cities[i] + Math.Min(fuel[i + 1], i + 2 < n ? fuel[i + 2] : 0);
+        }
+
+        return Math.Min(fuel[0], fuel[1]);
+    }
+
+    public static void Main()
+    {
+        int[] cities = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        Console.WriteLine(FindFuel(cities));
+    }
+}
---
+++ 10. Алгоритми и структури от данни/08. Exam Preparation/2023.10.15/Elements/Program.cs
@@ -0,0 +1,40 @@
+﻿﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    private static IEnumerable<IEnumerable<T>> Combinations<T>(IEnumerable<T> elements, int a)
+    {
+        return a == 0 ? new[] { new T[0] } : elements.SelectMany((x, y) => Combinations(elements.Skip(y + 1), a - 1).Select(c => (new[] { x }).Concat(c)));
+    }
+
+    public static void Main()
+    {
+        List<string> elementsList = Console.ReadLine().Split().ToList();
+        int N = int.Parse(Console.ReadLine());
+        List<string> priorityElements = Console.ReadLine().Split().ToList();
+
+        List<List<string>> experiments = new List<List<string>>();
+        var combinations = Combinations(elementsList, N);
+
+        foreach (var combo in combinations)
+        {
+            List<string> experiment = combo.ToList();
+
+            bool hasPriority = priorityElements.Any(experiment.Contains);
+
+            if (hasPriority)
+            {
+                experiment = experiment.OrderBy(e => priorityElements.IndexOf(e) < 0 ? int.MaxValue : priorityElements.IndexOf(e)).ToList();
+            }
+
+            experiments.Add(experiment);
+        }
+
+        foreach (var experiment in experiments)
+        {
+            Console.WriteLine(string.Join(" ", experiment));
+        }
+    }
+}
